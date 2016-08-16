db0bacf51c071f6218688b38644f6b1b00b6b42d
U-HQ\zhangs <HQ+zhangs@ZHANGS-PC.opentext.net>
2016-08-16 16:06:56 -0700

    initial implementation for vector 2D

---
+++ src/design/Vector2D.java
@@ -0,0 +1,39 @@
+package design;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class Vector2D implements Iterator<Integer> 
+{
+	private Iterator<List<Integer>> rowIterator;
+	private Iterator<Integer> colIterator;
+	
+    public Vector2D(List<List<Integer>> vec2d) 
+    {
+    	rowIterator = vec2d.iterator( );
+    }
+
+    @Override
+    public Integer next() 
+    {
+    	return colIterator.next( );
+    }
+
+    @Override
+    public boolean hasNext() 
+    {
+		while ( rowIterator.hasNext( ) 
+				&& !colIterator.hasNext( ) )
+		{
+			colIterator = rowIterator.next( ).iterator( );
+		}
+		
+		return colIterator.hasNext( );
+    }
+}
+
+/**
+ * Your Vector2D object will be instantiated and called as such:
+ * Vector2D i = new Vector2D(vec2d);
+ * while (i.hasNext()) v[f()] = i.next();
+ */
