ea3c4056914955e4cadc6abba61deeb891ea990b
FreemanZhang <sjzhang1901@yahoo.com>
2016-08-17 08:18:53 -0700

    two sum with map<Value, Freq>

--- src/design/TwoSum.java
+++ src/design/TwoSum.java
@@ -1,15 +1,40 @@
 package design;
 
-public class TwoSum {
+import java.util.HashMap;
+import java.util.Map;
 
+public class TwoSum 
+{
+	private Map<Integer, Integer> numToFreqMap = new HashMap<>();
+	
     // Add the number to an internal data structure.
-	public void add(int number) {
-	    
+	public void add( int number )
+	{
+		numToFreqMap.put( number, numToFreqMap.getOrDefault( number, 1 )  );
 	}
 
     // Find if there exists any pair of numbers which sum is equal to the value.
-	public boolean find(int value) {
-	    
+	public boolean find( int value ) 
+	{
+		for ( Map.Entry<Integer, Integer> entry : numToFreqMap.entrySet() )
+		{
+			int key = entry.getKey();
+			int freq = entry.getValue();
+			
+			if ( ( value == key + key )
+				&& ( freq > 1 ) ) 
+			{
+				return true;
+			}
+			
+			if ( numToFreqMap.containsKey( value - key ) )
+			{
+				return true;
+			}			
+			
+		}
+		
+		return false;
 	}
 }
 
