4055c402e49167927a3b4aa125a297243f08e00a
FreemanZhang <sjzhang1901@yahoo.com>
2016-08-17 08:31:30 -0700

    init impl with set

--- src/design/ValidWordAbbr.java
+++ src/design/ValidWordAbbr.java
@@ -1,17 +1,39 @@
-
 package design;
+
+import java.util.Set;
 
 public class ValidWordAbbr
 {
-
+	private Set<String> dict;
+	
 	public ValidWordAbbr( String[] dictionary )
 	{
-
+		for ( String word : dictionary )
+		{
+			dict.add( getAbbre( word ) );
+		}
 	}
 
 	public boolean isUnique( String word )
 	{
-
+		String abbre = getAbbre( word );
+		return !dict.contains( abbre );
+	}
+	
+	private String getAbbre( String word )
+	{
+		if ( word.length() <= 2 )
+		{
+			return word;
+		}
+		else
+		{
+			StringBuilder abbre = new StringBuilder();
+			abbre.append( word.charAt( 0 ) );
+			abbre.append( word.length() - 2 );
+			abbre.append( word.charAt( word.length() - 1 ) );
+			return abbre.toString();
+		}
 	}
 }
 
