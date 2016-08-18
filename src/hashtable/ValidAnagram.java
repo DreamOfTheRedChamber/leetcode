a1309d51b9a0753b4ed10f3a0cd960928c7eba5a
Shijie Zhang <szhang@opentext.com>
2016-08-18 15:55:31 -0700

    what about for unicode characters

--- src/hashtable/ValidAnagram.java
+++ src/hashtable/ValidAnagram.java
@@ -1,13 +1,43 @@
 package hashtable;
 
 /**
- * 
+Given two strings s and t, write a function to determine if t is an anagram of s.
+
+For example,
+s = "anagram", t = "nagaram", return true.
+s = "rat", t = "car", return false.
+
+Note:
+You may assume the string contains only lowercase alphabets.
+
+Follow up:
+What if the inputs contain unicode characters? How would you adapt your solution to such case?
  */
 
 public class ValidAnagram
 {
+	private final static int CHARSET_SIZE = 26;
     public boolean isAnagram(String s, String t) 
     {
-        
+    	int[] sHistogram = new int[CHARSET_SIZE];
+    	for ( char ch : s.toCharArray( ) )
+    	{
+    		sHistogram[ch - 'a'] = sHistogram[ch - 'a'] + 1;
+    	}
+    	
+    	int[] tHistogram = new int[CHARSET_SIZE];
+    	for ( char ch : t.toCharArray( ) )
+    	{
+    		tHistogram[ch - 'a'] = tHistogram[ch - 'a'] + 1;
+    	}
+    	
+    	for ( int i = 0; i < CHARSET_SIZE; i++ )
+    	{
+    		if ( sHistogram[i] != tHistogram[i] )
+    		{
+    			return false;
+    		}
+    	}
+    	return true;
     }
 }
