80541378fb6ccc82424a05ca30fc49a917584fe2
FreemanZhang <sjzhang1901@yahoo.com>
2016-08-17 21:52:29 -0700

    test chinese

--- src/clouderaInterview/ClouderaOAQuestion1.java
+++ src/clouderaInterview/ClouderaOAQuestion1.java
@@ -1,9 +1,9 @@
 package clouderaInterview;
 
 /**
- * 第一题，把一个integer的数字顺�?打乱�?�得到的新的integer�?��?�该integer的sibling   比如 123和231，132，213，321都是sibling，但和222就�?是。
-写个程�?找一个integer的所有sibling里�?�最大的。比如上�?�那个例�?返回321.
-我的�?路就是把integer弄�?string �?转�?char array，然�?�sort一下，�?转回integer。注�?最�?�的integer�?�能会溢出，所以查看edge case
+ * 第一题，把一个integer的数字顺序打乱后得到的新的integer叫做该integer的sibling   比如 123和231，132，213，321都是sibling，但和222就不是。
+写个程序找一个integer的所有sibling里面最大的。比如上面那个例子返回321.
+我的思路就是把integer弄成string 再转成char array，然后sort一下，再转回integer。注意最后的integer可能会溢出，所以查看edge case
 
 public static int solution(int N){
                 long result=0;
@@ -15,10 +15,11 @@ public static int solution(int N){
                 }
                 if(result>Integer.MAX_VALUE){
                         return Integer.MAX_VALUE;
-                }else{
+                }else{. 1point3acres.com/bbs
                         return (int)result;
-                }                
+                }                .1point3acres缃�
         }
+
  */
 
 public class ClouderaOAQuestion1
