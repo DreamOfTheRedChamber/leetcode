7a7e4ab124f3450e2f60d48d382c991ab4539a9c
Shijie Zhang <szhang@opentext.com>
2016-08-18 13:00:10 -0700

    bottom up level order traversal

--- src/bfs/BinaryTreeLevelOrderTraversalII.java
+++ src/bfs/BinaryTreeLevelOrderTraversalII.java
@@ -1,5 +1,11 @@
 package bfs;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 /**
  * 
  */
@@ -9,7 +15,41 @@ public class BinaryTreeLevelOrderTraversalII
 
     public List<List<Integer>> levelOrderBottom(TreeNode root) 
     {
+        List<List<Integer>> allLevelNums = new ArrayList<>();
+        List<Integer> currLevelNums = new ArrayList<>();
+
+    	Queue<TreeNode> currLevelNodes = new LinkedList<>();
+        Queue<TreeNode> nextLevelNodes = new LinkedList<>();
+
+        currLevelNodes.add( root );        
         
+        while ( !currLevelNodes.isEmpty( ) )
+        {
+        	// dequeue curr level elem, add to result and enqueue its children
+        	TreeNode queueHead = currLevelNodes.remove( );
+        	currLevelNums.add( queueHead.val );
+        	if ( queueHead.left != null )
+        	{
+        		nextLevelNodes.add( queueHead.left );
+        	}
+        	if ( queueHead.right != null )
+        	{
+        		nextLevelNodes.add( queueHead.right );
+        	}
+        	
+        	// currLevel used up
+        	if ( currLevelNodes.isEmpty( ) )
+        	{
+        		allLevelNums.add( currLevelNums );
+        		currLevelNums = new ArrayList<>();
+        		
+        		currLevelNodes.addAll( nextLevelNodes );
+        		nextLevelNodes = new LinkedList<>();
+        	}
+        }
+        
+        Collections.reverse( allLevelNums );
+        return allLevelNums;
     }
 
 }
