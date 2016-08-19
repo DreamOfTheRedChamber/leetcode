06e04ff9c67c1e8675b7df5442776f6e3f7dc61c
Shijie Zhang <szhang@opentext.com>
2016-08-18 17:18:31 -0700

    init impl

--- src/hashtable/ContainsDuplicate.java
+++ src/hashtable/ContainsDuplicate.java
@@ -1,13 +1,27 @@
 package hashtable;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
- * 
+ * Given an array of integers, find if the array contains any duplicates. 
+ * Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
  */
 
 public class ContainsDuplicate
 {
     public boolean containsDuplicate(int[] nums) 
     {
-        
+    	Set<Integer> appearedNums = new HashSet<>();
+    	for ( int num : nums )
+    	{
+    		if ( appearedNums.contains( num ) )
+    		{
+    			return true;
+    		}
+    		
+    		appearedNums.add( num );
+    	}
+    	return false;
     }
 }
