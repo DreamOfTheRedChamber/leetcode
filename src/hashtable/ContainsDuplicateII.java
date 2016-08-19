28f031e08f4dda5be0b33f73329df3dccf3a3604
Shijie Zhang <szhang@opentext.com>
2016-08-18 17:24:36 -0700

    init impl.

--- src/hashtable/ContainsDuplicateII.java
+++ src/hashtable/ContainsDuplicateII.java
@@ -1,7 +1,12 @@
 package hashtable;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
- * 
+ * Given an array of integers and an integer k, 
+ * find out whether there are two distinct indices i and j in the array such that nums[i] = nums[j] 
+ * and the difference between i and j is at most k.
  */
 
 public class ContainsDuplicateII
@@ -9,7 +14,19 @@ public class ContainsDuplicateII
 
     public boolean containsNearbyDuplicate(int[] nums, int k) 
     {
-        
+    	Map<Integer, Integer> numToLatestPos = new HashMap<>();
+    	for ( int i = 0; i < nums.length; i++ )
+    	{
+    		int num = nums[i];
+    		if ( numToLatestPos.containsKey( num ) 
+    				&& i - numToLatestPos.get( num ) <= k)
+    		{
+    			return true;
+    		}
+    		
+    		numToLatestPos.put( num, i );
+    	}
+    	return false;
     }
 
 }
