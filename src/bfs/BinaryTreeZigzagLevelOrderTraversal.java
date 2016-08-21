f14c4e91a5953f0777f6413ab1a11ce4c98a2081
FreemanZhang <sjzhang1901@yahoo.com>
2016-08-20 20:59:13 -0700

    init impl

--- src/bfs/BinaryTreeZigzagLevelOrderTraversal.java
+++ src/bfs/BinaryTreeZigzagLevelOrderTraversal.java
@@ -1,5 +1,12 @@
 package bfs;
 
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.junit.Test;
+
 /**
  * 
  */
@@ -9,7 +16,46 @@ public class BinaryTreeZigzagLevelOrderTraversal
 
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) 
     {
-        
+    	List<List<Integer>> result = new ArrayList<>();
+    	if ( root == null )
+    	{
+    		return result;
+    	}
+    	
+    	Deque<TreeNode> bfsQueue = new LinkedList<>();
+    	bfsQueue.addFirst( root );
+    	boolean isOddLevel = true;
+    	while ( !bfsQueue.isEmpty() )
+    	{
+    		int levelSize = bfsQueue.size();
+    		List<Integer> levelNodes = new ArrayList<>();
+    		for ( int i = 0; i < levelSize; i++ )
+    		{
+    			TreeNode head = null;
+    			if ( isOddLevel )
+    			{
+    				head = bfsQueue.removeLast();
+    			}
+    			else
+    			{
+    				head = bfsQueue.removeFirst();
+    			}
+    			levelNodes.add( head.val );
+    			
+    			if ( head.right != null )
+    			{
+    				bfsQueue.addLast( head.right );
+    			}
+    			if ( head.left != null )
+    			{
+    				bfsQueue.addFirst( head.left );
+    			}
+    		}
+    		isOddLevel = !isOddLevel;
+    		result.add( levelNodes );
+    	}
+    	    	
+    	return result;
     }
 
 }
