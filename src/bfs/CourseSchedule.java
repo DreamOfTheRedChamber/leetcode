75ed54919b130170bc66ea90e0e2de709d202b9e
FreemanZhang <sjzhang1901@yahoo.com>
2016-08-21 07:57:17 -0700

    init impl

--- src/bfs/CourseSchedule.java
+++ src/bfs/CourseSchedule.java
@@ -1,14 +1,75 @@
 package bfs;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * 
  */
 
 public class CourseSchedule
 {
-
     public boolean canFinish(int numCourses, int[][] prerequisites) 
     {
-        
+    	if ( numCourses < 0 
+    			|| prerequisites.length == 0 
+    			|| prerequisites[0].length != 2 )
+    	{
+    		throw new IllegalArgumentException("");
+    	}
+    	
+    	// build graph
+    	Map<Integer, Set<Integer>> graph = new HashMap<>();
+    	for ( int i = 0; i < numCourses; i++ )
+    	{
+    		graph.put( i, new HashSet<>() );
+    	}
+    	for ( int[] edge : prerequisites )
+    	{
+    		graph.get( edge[1] ).add( edge[0] );
+    	}
+    	
+    	Set<Integer> discovered = new HashSet<>();
+    	Set<Integer> visited = new HashSet<>();
+    	for ( Integer vertex : graph.keySet() )
+    	{
+    		if ( !visited.contains( vertex ) )
+    		{
+    			if ( judgeCycleExist( graph, vertex, discovered, visited ) )
+    			{
+    				return false;
+    			}
+    		}
+    	}
+    	return true;
+    }
+    
+    private boolean judgeCycleExist( Map<Integer, Set<Integer>> graph, Integer startNode, Set<Integer> discovered, Set<Integer> visited )
+    {
+    	discovered.add( startNode );
+    	for ( Integer neighbor : graph.get( startNode ) )
+    	{
+    		if ( !discovered.contains( neighbor ) )
+    		{
+    			if ( judgeCycleExist( graph, neighbor, discovered, visited ) )
+    			{
+    				return true;
+    			}
+    		}
+    		else if ( discovered.contains( neighbor ) 
+    				&& !visited.contains( neighbor ) )
+    		{
+    			return true;
+    		}
+    		else
+    		{
+    			// already visited, do nothing
+    			;
+    		}
+    	}
+    	visited.add( startNode );
+    	return false;
     }
 }
