0d0887b172500e9fea701d8f1376a9d36154c1c5
FreemanZhang <sjzhang1901@yahoo.com>
2016-08-21 07:14:53 -0700

    init impl. dfs with a hashmap instead of a set

--- src/bfs/CloneGraph.java
+++ src/bfs/CloneGraph.java
@@ -1,13 +1,41 @@
 package bfs;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import utility.UndirectedGraphNode;
+
 /**
  * 
  */
 
-public class CloneGraph {
+public class CloneGraph 
+{
 
-	public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
-
+	public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) 
+	{
+		return cloneGraphRecurse( node, new HashMap<Integer, UndirectedGraphNode>() );
 	}
 
+	private UndirectedGraphNode cloneGraphRecurse( UndirectedGraphNode node, Map<Integer, UndirectedGraphNode> labelToVertexMap )
+	{
+		UndirectedGraphNode clonedNode = new UndirectedGraphNode( node.label );
+		labelToVertexMap.put( node.label, clonedNode );
+		for ( UndirectedGraphNode neighbor : node.neighbors )
+		{
+			// if clonedNeighbor exists
+			if ( !labelToVertexMap.containsKey( neighbor.label ) )
+			{
+				UndirectedGraphNode clonedNeighbor = cloneGraphRecurse( neighbor, labelToVertexMap );
+				clonedNode.neighbors.add( clonedNeighbor );
+			}
+			else
+			{
+				clonedNode.neighbors.add( labelToVertexMap.get( neighbor.label ) );
+			}			
+		}
+		return clonedNode;
+	}
 }
