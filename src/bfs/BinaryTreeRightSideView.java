9aa0c528bc25c8abdaafef3cc3232dd2b40e6477
FreemanZhang <sjzhang1901@yahoo.com>
2016-08-20 20:58:48 -0700

    init impl

--- src/bfs/BinaryTreeRightSideView.java
+++ src/bfs/BinaryTreeRightSideView.java
@@ -1,13 +1,43 @@
 package bfs;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 /**
  * 
  */
 
 public class BinaryTreeRightSideView
 {
-    public List<Integer> rightSideView(TreeNode root) 
+    public List<Integer> rightSideView( TreeNode root ) 
     {
-        
+        List<Integer> rightMostNodes = new ArrayList<>();
+        Queue<TreeNode> bfsQueue = new LinkedList<>();
+        bfsQueue.add( root );
+        while ( !bfsQueue.isEmpty() )
+        {
+        	int levelSize = bfsQueue.size();
+        	for ( int i = 0; i < levelSize; i++ )
+        	{
+        		TreeNode head = bfsQueue.remove();
+        		if ( head.left != null )
+        		{
+        			bfsQueue.add( head.left );
+        		}
+        		if ( head.right != null )
+        		{
+        			bfsQueue.add( head.right );
+        		}
+        		
+        		if ( i == levelSize - 1 )
+        		{        			
+        			rightMostNodes.add( head.val );
+        		}
+        	}
+        	
+        }
+        return rightMostNodes;
     }
 }
