84f6646f312ed3247a6cf3740134f8f6d3d9a797
FreemanZhang <sjzhang1901@yahoo.com>
2016-08-20 21:18:03 -0700

    init impl. Iterative with bfs, need to put in null. better space compl?

--- src/bfs/SymmetricTree.java
+++ src/bfs/SymmetricTree.java
@@ -8,6 +8,30 @@ public class SymmetricTree
 {
     public boolean isSymmetric(TreeNode root) 
     {
-        
+    	if ( root == null )
+    	{
+    		return true;
+    	}
+    	else if ( root.left == null || root.right == null )
+    	{
+    		return true;
+    	}
+    	else if ( root.left != null && root.right != null )
+    	{
+    		if ( root.left.val == root.right.val 
+    				&& isSymmetric( root.left )
+    				&& isSymmetric( root.right ) )
+    		{
+    			return true;
+    		}
+    		else
+    		{
+    			return false;
+    		}
+    	}
+    	else
+    	{
+    		return false;
+    	}    	    	
     }
 }
