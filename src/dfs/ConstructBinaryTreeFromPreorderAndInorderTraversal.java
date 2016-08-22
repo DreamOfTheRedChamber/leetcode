d1bbc72ed5a54a2eb4ef7003d6fa089d244dc72a
FreemanZhang <szhang@opentext.com>
2016-08-22 06:28:32 -0700

    init impl

--- src/dfs/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
+++ src/dfs/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
@@ -1,5 +1,7 @@
 package dfs;
 
+import utility.TreeNode;
+
 /**
  * 
  */
@@ -7,9 +9,39 @@ package dfs;
 public class ConstructBinaryTreeFromPreorderAndInorderTraversal
 {
 
-    public TreeNode buildTree(int[] preorder, int[] inorder) 
+    public TreeNode buildTree( int[] preorder, int[] inorder )
     {
-        
+        return buildTreeRecurse( preorder, inorder, 0, preorder.length - 1, 0, inorder.length - 1 );
     }
 
+    private TreeNode buildTreeRecurse( int[] preorder, int[] inorder, int preorderStart, int preorderEnd, int inorderStart, int inorderEnd )
+    {
+    	if ( preorderStart > preorderEnd )
+    	{
+    		return null;
+    	}
+    	
+    	// build root
+    	TreeNode root = new TreeNode( preorder[preorderStart] );
+    	
+    	// find root index inside inorder
+    	int rootInorderIndex = inorderStart;
+    	for ( int i = inorderStart; i <= inorderEnd; i++ )
+    	{
+    		if ( inorder[i] == preorder[preorderStart] )
+    		{
+    			rootInorderIndex = i;
+    			break;
+    		}
+    	}
+    	
+    	// build child
+    	int leftSubtreeSize = rootInorderIndex - inorderStart;
+    	TreeNode leftChild = buildTreeRecurse( preorder, inorder, preorderStart + 1, preorderStart + leftSubtreeSize, inorderStart, inorderStart + leftSubtreeSize - 1 );
+    	root.left = leftChild;
+    	int rightSubtreeSize = inorderEnd - rootInorderIndex;
+    	TreeNode rightChild = buildTreeRecurse( preorder, inorder, preorderEnd - rightSubtreeSize + 1, preorderEnd, inorderEnd - rightSubtreeSize + 1, inorderEnd );
+    	root.right = rightChild;
+    	return root;
+    }
 }
