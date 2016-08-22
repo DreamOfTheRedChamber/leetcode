ee39b02d7145d9a6882efe26f09104c9b4a62c25
FreemanZhang <szhang@opentext.com>
2016-08-22 07:32:41 -0700

    init impl

--- src/dfs/FlattenBinaryTreeToLinkedList.java
+++ src/dfs/FlattenBinaryTreeToLinkedList.java
@@ -1,13 +1,59 @@
 package dfs;
 
+import utility.ListNode;
+import utility.TreeNode;
+
 /**
  * 
  */
 
+class ListHeadTail
+{
+	public final ListNode head;
+	public final ListNode tail;
+	public ListHeadTail( ListNode head, ListNode tail )
+	{
+		this.head = head;
+		this.tail = tail;
+	}
+}
+
 public class FlattenBinaryTreeToLinkedList
 {
-    public void flatten(TreeNode root) 
+    public void flatten( TreeNode root )
     {
-        
+    	if ( root == null )
+    	{
+    		return;
+    	}
+    	flattenRecurse( root );
+    }
+    
+    private ListHeadTail flattenRecurse( TreeNode root )
+    {
+    	if ( root.left == null 
+    			&& root.right == null )
+    	{
+    		ListNode node = new ListNode( root.val );
+    		return new ListHeadTail( node, node );
+    	}
+    	else
+    	{
+    		ListNode head = new ListNode( root.val );
+    		ListNode tail = head;
+    		if ( root.left != null )
+    		{
+    			ListHeadTail leftChildList = flattenRecurse( root.left );
+    			tail.next = leftChildList.head;
+    			tail = leftChildList.tail;
+    		}
+    		if ( root.right != null )
+    		{
+    			ListHeadTail rightChildList = flattenRecurse( root.right );
+    			tail.next = rightChildList.head;
+    			tail = rightChildList.tail;
+    		}
+    		return new ListHeadTail( head, tail );
+    	}
     }
 }
