bc567c46b0447f693d0e45f078de9569b1bfea57
FreemanZhang <szhang@opentext.com>
2016-08-22 06:36:31 -0700

    init impl.

--- src/dfs/ConvertSortedArrayToBinarySearchTree.java
+++ src/dfs/ConvertSortedArrayToBinarySearchTree.java
@@ -1,5 +1,7 @@
 package dfs;
 
+import utility.TreeNode;
+
 /**
  * 
  */
@@ -7,9 +9,22 @@ package dfs;
 public class ConvertSortedArrayToBinarySearchTree
 {
 
-    public TreeNode sortedArrayToBST(int[] nums) 
+    public TreeNode sortedArrayToBST( int[] nums )
     {
-        
+        return buildTreeRecurse( nums, 0, nums.length - 1 );
     }
 
+    private TreeNode buildTreeRecurse( int[] nums, int start, int end )
+    {
+    	if ( start > end )
+    	{
+    		return null;
+    	}
+    	
+    	int rootIndex = ( end - start ) / 2 + start;
+    	TreeNode root = new TreeNode( nums[rootIndex] );
+    	root.left = buildTreeRecurse( nums, start, rootIndex - 1 );
+    	root.right = buildTreeRecurse( nums, rootIndex + 1, end );
+    	return root;
+    }
 }
