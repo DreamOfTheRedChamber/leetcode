38dd5f138c90aefa0861fabc679768e6c78923ba
FreemanZhang <szhang@opentext.com>
2016-08-22 21:42:10 -0700

    init impl

--- src/heap/FindKPairsWithSmallestSums.java
+++ src/heap/FindKPairsWithSmallestSums.java
@@ -1,5 +1,9 @@
 package heap;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+
 /*
 You are given two integer arrays nums1 and nums2 sorted in ascending order and an integer k.
 
@@ -33,7 +37,45 @@ All possible pairs are returned from the sequence:
 public class FindKPairsWithSmallestSums 
 {
     public List<int[]> kSmallestPairs(int[] nums1, int[] nums2, int k) 
-    {
+    {    	
+        List<int[]> result = new ArrayList<>();
+        if ( nums1.length == 0 
+        		|| nums2.length == 0 )
+        {
+        	return result;
+        }
         
+        PriorityQueue<Pair> pairSumQueue = new PriorityQueue<>( ( o1, o2 ) -> o2.sum - o1.sum );
+        pairSumQueue.offer( new Pair( 0, 0, nums1[0] + nums2[0] ) );
+        int numPairs = 0;
+        while ( numPairs < k
+        		&& !pairSumQueue.isEmpty() )
+        {
+        	Pair qHead = pairSumQueue.remove();
+        	result.add( new int[]{ nums1[qHead.num1Index], nums2[qHead.num2Index ] } );
+        	
+        	if ( qHead.num2Index + 1 < nums2.length )
+        	{
+        		pairSumQueue.offer( new Pair( qHead.num1Index, qHead.num2Index + 1, nums1[qHead.num1Index] + nums2[qHead.num2Index] ) );
+        	}        	
+        	if ( qHead.num1Index + 1 < nums1.length )
+        	{
+        		pairSumQueue.offer( new Pair( qHead.num1Index + 1, qHead.num2Index, nums1[qHead.num1Index+1] + nums2[qHead.num2Index] ) );        		
+        	}
+        }
+        return result;
     }
+}
+
+class Pair
+{
+	public final int num1Index;
+	public final int num2Index;
+	public final int sum;
+	public Pair( int num1Index, int num2Index, int sum )
+	{
+		this.num1Index = num1Index;
+		this.num2Index = num2Index;
+		this.sum = sum;
+	}
 }
