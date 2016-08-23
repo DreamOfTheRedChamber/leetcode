f3d6820bda9f0a5612e2d3ba302ac6c9ecd24d66
zhangs <zhangs@ZHANGS-PC.opentext.net>
2016-08-23 10:45:20 -0700

    init impl

--- src/binarySearch/SearchInsertPosition.java
+++ src/binarySearch/SearchInsertPosition.java
@@ -12,8 +12,42 @@ Here are few examples.
 [1,3,5,6], 0 → 0
  * */
 
-public class SearchInsertPosition {
-    public int searchInsert(int[] nums, int target) {
-        
+public class SearchInsertPosition 
+{
+	// TODO: question array duplicates exist?
+    public int searchInsert( int[] nums, int target )
+    {
+    	if ( nums.length == 0 )
+    	{
+    		return 0;
+    	}
+    	
+    	int start = 0;
+    	int end = nums.length - 1;
+    	while ( start + 1 < end )
+    	{
+    		int mid = ( end - start ) / 2 + start;
+    		if ( nums[mid] < target )
+    		{
+    			start = mid;
+    		}
+    		else
+    		{
+    			end = mid;
+    		}
+    	}
+    	
+    	if ( nums[end] < target )
+    	{
+    		return end;
+    	}
+    	else if ( nums[start] < target )
+    	{
+    		return start;
+    	}
+    	else
+    	{
+    		return nums.length;
+    	}
     }
 }
