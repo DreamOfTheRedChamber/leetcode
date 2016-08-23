082f4d3ea7bd6af54cd72985df361844eff3530c
FreemanZhang <szhang@opentext.com>
2016-08-23 07:13:02 -0700

    init impl

--- src/binarySearch/SearchForARange.java
+++ src/binarySearch/SearchForARange.java
@@ -12,9 +12,87 @@ Given [5, 7, 7, 8, 8, 10] and target value 8,
 return [3, 4].
  */
 
-public class SearchForARange {
-    public int[] searchRange(int[] nums, int target) {
-        
+public class SearchForARange 
+{
+
+	public int[] searchRange( int[] nums, int target )
+    {
+		if ( nums.length == 0 )
+		{
+			return new int[]{ -1, -1 };
+		}
+		int firstAppearPos = findFirstAppear( nums, target );
+		int lastAppearPos = ( firstAppearPos == -1 ) ? -1 : findLastAppear( nums, target );
+		return new int[]{ firstAppearPos, lastAppearPos };
     }
 
+	private int findFirstAppear( int[] nums, int target )
+	{
+		int start = 0;
+		int end = nums.length - 1;
+		while ( start + 1 < end )
+		{
+			int mid = ( end - start ) / 2 + start;
+			if ( nums[mid] == target )
+			{
+				end = mid;
+			}
+			else if ( nums[mid] < target )
+			{
+				start = mid + 1;
+			}
+			else
+			{
+				end = mid - 1;
+			}
+		}
+		
+		if ( nums[start] == target )
+		{
+			return start;
+		}
+		else if ( nums[end] == target )
+		{
+			return end;
+		}
+		else
+		{
+			return -1;
+		}
+	}
+	
+	private int findLastAppear( int[] nums, int target )
+	{
+		int start = 0;
+		int end = nums.length - 1;
+		while ( start + 1 < end )
+		{
+			int mid = ( end - start ) / 2 + start;
+			if ( nums[mid] == target )
+			{
+				start = mid;
+			}
+			else if ( nums[mid] < target )
+			{
+				start = mid + 1;
+			}
+			else
+			{
+				end = mid - 1;
+			}
+		}
+		
+		if ( nums[end] == target )
+		{
+			return end;
+		}
+		else if ( nums[start] == target )
+		{
+			return start;
+		}
+		else
+		{
+			return -1;
+		}		
+	}
 }
