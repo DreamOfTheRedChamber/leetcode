0b920c88f9edfa78ecc418f772ebdecef9fd929d
FreemanZhang <szhang@opentext.com>
2016-08-23 07:51:52 -0700

    init impl

--- src/binarySearch/CountCompleteTreeNodes.java
+++ src/binarySearch/CountCompleteTreeNodes.java
@@ -1,5 +1,9 @@
 package binarySearch;
 
+import java.util.Stack;
+
+import utility.TreeNode;
+
 /*
  * Given a complete binary tree, count the number of nodes.
 
@@ -10,8 +14,93 @@ It can have between 1 and 2h nodes inclusive at the last level h.*/
 
 public class CountCompleteTreeNodes 
 {
-    public int countNodes(TreeNode root) 
+    public int countNodes( TreeNode root )
     {
+    	if ( root == null )
+    	{
+    		return 0;
+    	}
+    	
+    	// calc tree depth
+        int treeDepth = 0;
+        TreeNode currNode = root;
+        while ( currNode != null )
+        {
+        	treeDepth++;
+        	currNode = currNode.left;
+        }
         
+        // binary search
+        int start = 1;
+        int end = 2 * treeDepth;
+        while ( start + 1 < end )
+        {
+        	int mid = ( end - start ) / 2 + start;
+        	if ( doesLeafExist( root, mid, treeDepth ) )
+        	{
+        		start = mid;
+        	}
+        	else
+        	{
+        		end = mid;
+        	}
+        }
+
+        // TODO: int pow
+        int numNonLeafNodes = ( int )( Math.pow( 2, treeDepth) ) - 1;
+        if ( doesLeafExist( root, end, treeDepth ) )
+        {
+        	return numNonLeafNodes + end;
+        }
+        else
+        {
+        	return numNonLeafNodes + start;
+        }
+    }
+    
+    private boolean doesLeafExist( TreeNode root, int kthLeaf, int treeDepth )
+    {
+    	Stack<Boolean> rootToKthLeafPath = new Stack<>(); // false -> left, true -> right
+    	int numNonLeafNodes = ( int )( Math.pow( 2, treeDepth) ) - 1;
+    	
+    	// go from leaf to root, put path in a stack
+    	int currNodeOrder = numNonLeafNodes + kthLeaf;
+    	while ( currNodeOrder != 1 )
+    	{
+    		if ( currNodeOrder % 2 == 1 )
+    		{
+    			rootToKthLeafPath.push( true );
+    		}
+    		else
+    		{
+    			rootToKthLeafPath.push( false );
+    		}
+    		currNodeOrder /= 2;
+    	}
+    	
+    	// go from root to leaf, check node existence
+    	TreeNode currNode = root;
+    	while ( !rootToKthLeafPath.isEmpty() )
+    	{
+    		boolean shouldGoRight = rootToKthLeafPath.pop();
+    		if ( shouldGoRight )
+    		{
+    			currNode = currNode.right;
+    		}
+    		else
+    		{
+    			currNode = currNode.left;
+    		}
+    	}
+    	
+    	// whether specific leaf exists
+    	if ( currNode == null )
+    	{
+    		return false;
+    	}
+    	else
+    	{
+    		return true;
+    	}
     }
 }
