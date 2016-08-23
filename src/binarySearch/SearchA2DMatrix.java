bd84d42e79fd423305c11bd60908d384ca375e64
FreemanZhang <szhang@opentext.com>
2016-08-23 08:47:36 -0700

    init impl

--- src/binarySearch/SearchA2DMatrix.java
+++ src/binarySearch/SearchA2DMatrix.java
@@ -21,6 +21,95 @@ public class SearchA2DMatrix
 {
     public boolean searchMatrix(int[][] matrix, int target) 
     {
-        
+    	if ( matrix.length == 0 
+    			|| matrix[0].length == 0 )
+    	{
+    		return false;
+    	}
+    	
+    	int rowIndex = findLastSmallerRowHead( matrix, target );
+    	if ( rowIndex == -1 )
+    	{
+    		return false;
+    	}
+    	int colIndex = binarySearch( matrix, rowIndex, target );
+    	if ( colIndex == -1 )
+    	{
+    		return false;
+    	}
+    	else
+    	{
+    		return true;
+    	}
     }
+    
+    private int binarySearch( int[][] matrix, int rowIndex, int target )
+    {
+    	int start = 0;
+    	int end = matrix[0].length - 1;
+    	while ( start + 1 < end )
+    	{
+    		int mid = ( end - start ) / 2 + start;
+    		if ( matrix[rowIndex][mid] < target )
+    		{
+    			start = mid + 1;
+    		}
+    		else if ( matrix[rowIndex][mid] > target )
+    		{
+    			end = mid - 1;
+    		}
+    		else
+    		{
+    			return mid;
+    		}
+    	}
+    	
+    	if ( matrix[rowIndex][start] == target )
+    	{
+    		return start;
+    	}
+    	else if ( matrix[rowIndex][end] == target )
+    	{
+    		return end;
+    	}
+    	else
+    	{
+    		return -1;
+    	}
+    }
+    
+    /**
+     * @return  index of last smaller row head, -1 for nonexistence
+     */
+    private int findLastSmallerRowHead( int[][] matrix, int target )
+    {
+    	int start = 0;
+    	int end = matrix.length - 1;
+    	while ( start + 1 < end )
+    	{
+    		int mid = ( end - start ) / 2 + start;
+    		if ( matrix[mid][0] <= target )
+    		{
+    			start = mid;
+    		}
+    		else //if ( matrix[mid][0] > target )
+    		{
+    			// TODO: binary search conditions
+    			end = mid - 1;
+    		}
+    	}
+    	
+    	if ( matrix[end][0] <= target )
+    	{
+    		return end;
+    	}
+    	else if ( matrix[start][0] <= target )
+    	{
+    		return start;
+    	}
+    	else
+    	{
+    		return -1;
+    	}
+    }   
 }
