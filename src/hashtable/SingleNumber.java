f058761e4afaa3e5b76eea94e126648082491e60
FreemanZhang <szhang@opentext.com>
2016-08-22 21:13:27 -0700

    init impl

--- src/hashtable/SingleNumber.java
+++ src/hashtable/SingleNumber.java
@@ -12,7 +12,12 @@ public class SingleNumber
 	
     public int singleNumber(int[] nums) 
     {
-        
+    	int result = 0;
+    	for ( int num : nums )
+    	{
+    		result ^= num;
+    	}
+    	return result;
     }
 
 }
