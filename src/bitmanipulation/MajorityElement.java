cccf3148bf1cff514e0fbafd59382e5ad2a930de
zhangs <szhang@opentext.com>
2016-08-24 12:55:30 -0700

    init impl

--- src/bitmanipulation/MajorityElement.java
+++ src/bitmanipulation/MajorityElement.java
@@ -4,10 +4,37 @@ package bitmanipulation;
  * 
  */
 
+// todo
 public class MajorityElement
 {
-    public int majorityElement(int[] nums) {
-        
+    public int majorityElement( int[] nums )
+    {
+    	if ( nums.length == 0 )
+    	{
+    		throw new IllegalArgumentException("");
+    	}
+    	
+    	int candidate = nums[0];
+    	int candidateFreq = 1;
+    	for ( int i = 1; i < nums.length; i++ )
+    	{
+    		if ( candidateFreq == 0 )
+    		{
+    			candidate = nums[i];
+    			candidateFreq = 1;
+    		}
+    		else
+    		{
+    			if ( candidate == nums[i] )
+    			{
+    				candidateFreq++;
+    			}
+    			else
+    			{
+    				candidateFreq--;
+    			}
+    		}
+    	}
+    	return candidate;
     }
-
 }
