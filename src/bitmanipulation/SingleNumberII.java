96394f8084471ae6b63de0c15f1ec4a1030b1397
FreemanZhang <szhang@opentext.com>
2016-08-24 05:19:41 -0700

    init impl

--- src/bitmanipulation/SingleNumberII.java
+++ src/bitmanipulation/SingleNumberII.java
@@ -9,10 +9,25 @@ Your algorithm should have a linear runtime complexity. Could you implement it w
 
 public class SingleNumberII
 {
-
-	public SingleNumberII( )
+	public int singleNumber( int[] nums )
 	{
-		// TODO Auto-generated constructor stub
+		// calc bit 1 histogram in int[32] array
+		int[] histogram = new int[32];
+		for ( int num : nums )
+		{
+			for ( int i = 0; i < 32; i++ )
+			{
+				histogram[i] += ( num & ( 1 << i ) );
+			}
+		}
+		
+		// reconstruct the single one
+		int singleOne = 0;
+		for ( int i = 0; i < histogram.length; i++ )
+		{
+			// TODO: operator precedence of histogram
+			singleOne ^= ( histogram[i] % 3 << i );
+		}
+		return singleOne;		
 	}
-
 }
