07809dec6f763c85122699b5ccb3cfdcf7b34b01
FreemanZhang <szhang@opentext.com>
2016-08-23 19:56:47 -0700

    init impl

--- src/binarySearch/Sqrt.java
+++ src/binarySearch/Sqrt.java
@@ -1,4 +1,4 @@
-package math;
+package binarySearch;
 
 /**
 Implement int sqrt(int x).
@@ -6,13 +6,43 @@ Implement int sqrt(int x).
 Compute and return the square root of x.
  */
 
-//todo
 public class Sqrt
 {
-
-    public int mySqrt(int x) 
+    public int mySqrt( int x )
     {
-        
+    	if ( x < 0 )
+    	{
+    		throw new IllegalArgumentException("");
+    	}
+    	if ( x <= 1 )
+    	{
+    		return 1;
+    	}
+    	
+    	int start = 1;
+    	int end = x / 2;
+    	while ( start + 1 < end )
+    	{
+    		int mid = ( end - start ) / 2 + start;
+    		// TODO: techniques to avoid overflow
+    		// TODO: lowerbound or upperbound
+    		if ( x / mid < mid )
+    		{
+    			end = mid - 1;
+    		}
+    		else
+    		{
+    			start = mid;
+    		} 			
+    	}
+    	
+    	if ( x / end > end )
+    	{
+    		return end;
+    	}
+    	else
+    	{
+    		return start;
+    	}
     }
-
 }
