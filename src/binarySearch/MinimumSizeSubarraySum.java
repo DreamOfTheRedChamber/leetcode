952a1e6c4a4613d24c6219245a44966b4a13344f
FreemanZhang <szhang@opentext.com>
2016-08-24 06:17:05 -0700

    init impl

--- src/binarySearch/MinimumSizeSubarraySum.java
+++ src/binarySearch/MinimumSizeSubarraySum.java
@@ -9,10 +9,35 @@ the subarray [4,3] has the minimal length under the problem constraint.
 
  * */
 
-//todo
 public class MinimumSizeSubarraySum 
 {
-    public int minSubArrayLen(int s, int[] nums) 
+	// TODO: what if both postive and negative integers
+    public int minSubArrayLen( int s, int[] nums )
     {
+    	int currWindStart = 0;
+    	int currWindEnd = 0;
+    	int currWindSum = 0;
+    	int minSize = Integer.MAX_VALUE;
+    	while ( currWindEnd < nums.length )
+    	{
+    		while ( currWindEnd < nums.length
+    				&& currWindSum < s )
+    		{    			
+    			s += nums[currWindEnd];
+    			currWindEnd++;
+    		}
+    		
+    		if ( currWindSum < s )
+    		{
+    			while ( currWindStart < currWindEnd
+    					&& currWindSum >= s )
+    			{
+    				minSize = Math.min( currWindEnd-currWindStart, minSize );
+    				s -= nums[currWindStart];
+    				currWindStart++;
+    			}
+    		}
+    	}
+    	return minSize;
     }
 }
