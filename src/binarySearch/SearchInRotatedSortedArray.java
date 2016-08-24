103d3c18514a89277e3c806882cfa2a975fd2504
Shijie Zhang <szhang@opentext.com>
2016-08-24 12:56:00 -0700

    init impl

--- src/binarySearch/SearchInRotatedSortedArray.java
+++ src/binarySearch/SearchInRotatedSortedArray.java
@@ -15,8 +15,55 @@ Write a function to determine if a given target is in the array.
 
 public class SearchInRotatedSortedArray 
 {
-    public int search(int[] nums, int target) 
+    public int search( int[] nums, int target )
     {
-        
+    	if ( nums.length == 0 )
+    	{
+    		return -1;
+    	}
+    	
+    	int start = 0;
+    	int end = nums.length - 1;
+    	int pivotEnd = nums.length - 1;
+    	while ( start + 1 < end )
+    	{
+    		int mid = ( end - start ) / 2 + start;
+    		if ( nums[mid] < nums[pivotEnd] )
+    		{
+    			// TODO: put redundant logic into else, smart!!!
+    			if ( target < nums[mid] )
+    			{
+    				end = mid;
+    			}
+    			else
+    			{
+    				start = mid;
+    			}
+    		}
+    		else
+    		{
+    			if ( target > nums[mid] )
+    			{
+    				start = mid;
+    			}
+    			else
+    			{
+    				end = mid;
+    			}
+    		}
+    	}
+    	
+    	if ( nums[start] == target )
+    	{
+    		return start;
+    	}
+    	else if ( nums[end] == target )
+    	{
+    		return end;
+    	}
+    	else
+    	{
+    		return -1;
+    	}
     }
 }
--- src/binarySearch/SearchInRotatedSortedArrayII.java
+++ src/binarySearch/SearchInRotatedSortedArrayII.java
@@ -4,6 +4,67 @@ public class SearchInRotatedSortedArrayII
 {
     public boolean search(int[] nums, int target) 
     {
-        
+    	if ( nums.length == 0 )
+    	{
+    		return false;
+    	}
+    	
+    	int start = 0;
+    	int end = nums.length - 1;
+    	int pivotEnd = nums.length - 1;
+    	while ( start + 1 < end 
+    			&& start + 1 < pivotEnd )
+    	{
+    		int mid = ( end - start ) / 2 + start;
+    		// TODO: how to handle pivotEnd and end inconsistent problem
+    		if ( nums[mid] == nums[pivotEnd] )
+    		{
+    			pivotEnd--;
+    		}    		
+    		else if ( nums[mid] < nums[pivotEnd] )
+    		{
+    			// TODO: put redundant logic into else, smart!!!
+    			if ( target == nums[mid] )
+    			{
+    				return true;
+    			}
+    			else if ( target < nums[mid] )
+    			{
+    				end = mid;
+    			}
+    			else
+    			{
+    				start = mid;
+    			}
+    		}
+    		else
+    		{
+    			if ( target == nums[mid] )
+    			{
+    				return true;
+    			}
+    			else if ( target > nums[mid] )
+    			{
+    				start = mid;
+    			}
+    			else
+    			{
+    				end = mid;
+    			}
+    		}
+    	}
+    	
+    	if ( nums[start] == target )
+    	{
+    		return true;
+    	}
+    	else if ( nums[end] == target )
+    	{
+    		return true;
+    	}
+    	else
+    	{
+    		return false;
+    	}
     }
 }
