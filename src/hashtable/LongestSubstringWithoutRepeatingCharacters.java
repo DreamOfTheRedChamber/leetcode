09af806fb9765379eae1d99e4ee671e0bc2e1ae7
FreemanZhang <szhang@opentext.com>
2016-08-23 20:55:59 -0700

    init impl

--- src/hashtable/LongestSubstringWithoutRepeatingCharacters.java
+++ src/hashtable/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,5 +1,8 @@
 package hashtable;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
 Given a string, find the length of the longest substring without repeating characters.
 
@@ -12,13 +15,40 @@ Given "bbbbb", the answer is "b", with the length of 1.
 Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
  */
 
-//todo
 public class LongestSubstringWithoutRepeatingCharacters
 {
 
-    public int lengthOfLongestSubstring(String s) 
+    public int lengthOfLongestSubstring( String s )
     {
-        
+    	if ( s == null )
+    	{
+    		return 0;
+    	}
+    	Set<Character> currWindChars = new HashSet<>();
+    	int windStart = 0;
+    	int windEnd = 0;
+    	int maxLength = 0;
+    	while ( windEnd < s.length() )
+    	{
+    		// increase windEnd as much as possible
+    		while ( windEnd < s.length()
+    				&& !currWindChars.contains( windEnd ) )
+    		{
+    			windEnd++;
+    			currWindChars.add( s.charAt( windEnd ) );
+    			maxLength = Math.max( maxLength, windEnd - windStart );
+    		}    		
+    		// increase windStart as less as possible
+    		if ( windEnd < s.length() )
+    		{
+    			while ( currWindChars.contains( windEnd ) )
+    			{
+    				currWindChars.remove( s.charAt( windStart ) );
+    				windStart++;
+    			}
+    		}
+    	}
+    	return maxLength;
     }
 
 }
