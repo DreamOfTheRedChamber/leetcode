75ba317a5f29e09f4acd37d2b12a8e7d73d53d3d
Shijie Zhang <szhang@opentext.com>
2016-08-24 13:36:11 -0700

    init imp

--- src/bitmanipulation/Subsets.java
+++ src/bitmanipulation/Subsets.java
@@ -1,5 +1,8 @@
 package bitmanipulation;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /*
 Given a set of distinct integers, nums, return all possible subsets.
 
@@ -24,6 +27,23 @@ public class Subsets
 {
     public List<List<Integer>> subsets(int[] nums) 
     {
-        
+        List<List<Integer>> allSubsets = new ArrayList<>();
+        List<Integer> oneSubset = new ArrayList<>();
+        buildSubsets( allSubsets, oneSubset, nums, 0 );        
+        return allSubsets;
+    }
+    
+    private void buildSubsets( List<List<Integer>> allSubsets, List<Integer> oneSubset, int[] nums, int startPos )
+    {
+    	if ( startPos == nums.length )
+    	{
+    		allSubsets.add( new ArrayList<>( oneSubset ) );
+    		return;
+    	}
+    	
+    	oneSubset.add( nums[startPos] );
+    	buildSubsets( allSubsets, oneSubset, nums, startPos + 1 );
+    	oneSubset.remove( oneSubset.size( ) - 1 );
+    	buildSubsets( allSubsets, oneSubset, nums, startPos + 1 );
     }
 }
