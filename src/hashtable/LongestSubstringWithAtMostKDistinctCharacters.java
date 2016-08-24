6f735cc117f739d2bbb86baaf89b80ab798a84b8
FreemanZhang <szhang@opentext.com>
2016-08-23 21:13:06 -0700

    init impl

--- src/hashtable/LongestSubstringWithAtMostKDistinctCharacters.java
+++ src/hashtable/LongestSubstringWithAtMostKDistinctCharacters.java
@@ -1,5 +1,8 @@
 package hashtable;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 
  */
@@ -7,9 +10,48 @@ package hashtable;
 public class LongestSubstringWithAtMostKDistinctCharacters
 {
 
-    public int lengthOfLongestSubstringKDistinct(String s, int k) 
+    public int lengthOfLongestSubstringKDistinct( String s, int k )
     {
+        if ( k < 0 
+        		|| s == null )
+        {
+        	throw new IllegalArgumentException("");
+        }
         
+        int windStart = 0; // inclusive
+        int windEnd = 0; // exclusive
+        int maxLength = 0;
+        Map<Character, Integer> histogram = new HashMap<>();
+        while ( windEnd < s.length() )
+        {
+        	// TODO: simplify condition
+        	// increase windEnd as much as possible
+        	while ( windEnd < s.length() 
+        			 && ( histogram.size() < k || ( histogram.size() == k  && histogram.containsKey( s.charAt( windEnd ) ) ) ) )
+        	{
+        		histogram.put( s.charAt( windEnd ), 1 + histogram.getOrDefault( s.charAt( windEnd ), 0 ) );
+        		maxLength = Math.max( maxLength, windEnd - windStart );
+        	}
+        	
+        	// increase windStart as less as possible
+        	if ( windEnd < s.length() )
+        	{
+        		while ( histogram.size() == k 
+        				&& histogram.containsKey( s.charAt( windStart) ) )
+        		{
+        			if ( histogram.get( s.charAt( windStart ) ) == 1 )
+        			{
+        				histogram.remove( s.charAt( windStart ) );
+        			}
+        			else
+        			{
+        				histogram.put( s.charAt( windStart ), histogram.get( s.charAt( windStart ) ) - 1 );
+        			}
+        			windStart++;
+        		}
+        	}
+        }
+        
+        return maxLength;
     }
-
 }
