88fa1e2f10b07268a8a5365642c31965d6b19e59
FreemanZhang <szhang@opentext.com>
2016-08-25 08:06:15 -0700

    init impl

--- src/dynamicprogramming/DecodeWays.java
+++ src/dynamicprogramming/DecodeWays.java
@@ -17,8 +17,42 @@ The number of ways decoding "12" is 2.
 
 public class DecodeWays
 {
-    public int numDecodings(String s) 
+    public int numDecodings( String s )
     {
+        if ( s == null )
+        {
+        	throw new IllegalArgumentException("");
+        }
         
+        // TODO: check condition in dp
+        if ( s.length() <= 1 )
+        {
+        	return s.length();
+        }
+                
+        int[] numEndingAtPos = new int[s.length()];
+        // init dp table
+        numEndingAtPos[0] = 1;
+        numEndingAtPos[1] = 1 + ( isValidChar( s, 0, 1 ) ? 1 : 0 );
+        // fill in dp table
+        for ( int i = 2; i < s.length(); i++ )
+        {
+        	numEndingAtPos[i] = numEndingAtPos[i-1] + ( isValidChar( s, i-1, i ) ? numEndingAtPos[i-2] : 0 );
+        }
+        return numEndingAtPos[s.length()-1];
+    }
+    
+    private boolean isValidChar( String s, int start, int end )
+    {
+    	int convertedInt = Integer.parseInt( s.substring(start, end + 1) );
+    	if ( convertedInt >= 0
+    			&& convertedInt <= 25 )
+    	{
+    		return true;
+    	}
+    	else
+    	{
+    		return false;
+    	}
     }
 }
