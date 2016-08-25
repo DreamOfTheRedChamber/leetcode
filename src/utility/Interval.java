71742e1fdb9e0a5023e74e876ca32020f78c7801
FreemanZhang <szhang@opentext.com>
2016-08-24 18:30:28 -0700

    add utility classes

---
+++ src/utility/Interval.java
@@ -0,0 +1,17 @@
+package utility;
+
+public class Interval 
+{
+	public int start;
+	public int end;
+	public Interval()
+	{
+		start = 0;
+		end = 0;
+	}
+	public Interval( int s, int e )
+	{
+		start = s;
+		end = e;
+	}
+}
---
+++ src/utility/TreeLinkNode.java
@@ -0,0 +1,15 @@
+package utility;
+
+public class TreeLinkNode 
+{
+	public TreeLinkNode left;
+	public TreeLinkNode right;
+	public TreeLinkNode next;
+
+	public TreeLinkNode(TreeLinkNode left, TreeLinkNode right, TreeLinkNode next) {
+		super();
+		this.left = left;
+		this.right = right;
+		this.next = next;
+	}
+}
