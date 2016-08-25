c68020522458d21be19af538583c9ec235ed7859
FreemanZhang <szhang@opentext.com>
2016-08-24 23:51:42 -0700

    init impl

--- src/dynamicprogramming/PerfectSquares.java
+++ src/dynamicprogramming/PerfectSquares.java
@@ -6,8 +6,30 @@ package dynamicprogramming;
 
 public class PerfectSquares
 {
-    public int numSquares(int n) 
+    public int numSquares( int n )
     {
+    	if ( n == 1 )
+    	{
+    		return 1;
+    	}
+    	
+        int[] minNumSquare = new int[n+1];
         
+        for ( int i = 2; i <= n; i++ )
+        {
+        	for ( int j = 1; j * j <= i; j++ )
+        	{
+        		if ( j * j == i )
+        		{
+        			minNumSquare[i] = 1;
+        			break;
+        		}
+        		else
+        		{
+        			minNumSquare[i] = Math.min( minNumSquare[i], 1 + minNumSquare[i-j*j] );
+        		}
+        	}
+        }
+        return minNumSquare[n];
     }
 }
