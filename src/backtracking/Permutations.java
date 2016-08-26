cdf2c0cc7b9904703b9e3f5aa24327f150d6f1d7
Shijie Zhang <szhang@opentext.com>
2016-08-26 13:29:03 -0700

    init impl

--- src/backtracking/Permutations.java
+++ src/backtracking/Permutations.java
@@ -1,5 +1,8 @@
 package backtracking;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /*
 Given a collection of distinct numbers, return all possible permutations.
 
@@ -19,6 +22,37 @@ public class Permutations
 {
     public List<List<Integer>> permute( int[] nums ) 
     {
+    	List<List<Integer>> allPerms = new LinkedList<>();
+    	List<Integer> onePerm = new LinkedList<>();
     	
+    	generatePerms( allPerms, onePerm, nums, 0 );
+    	
+    	return allPerms;
     }
+    
+    private void generatePerms( List<List<Integer>> allPerms, List<Integer> onePerm, int[] nums, int startPos )
+    {
+    	if ( startPos == nums.length )
+    	{
+    		allPerms.add( new LinkedList<>( onePerm ) );
+    		return;
+    	}
+    	
+    	for ( int i = startPos; i < nums.length; i++ )
+    	{
+    		swap( nums, i, startPos );
+    		onePerm.add( nums[i] );
+    		generatePerms( allPerms, onePerm, nums, i + 1 );
+    		onePerm.remove( onePerm.size( ) - 1 );
+    		swap( nums, i, startPos );
+    	}
+    }
+    
+    private void swap( int[] nums, int pos1, int pos2 )
+    {
+    	int pos1NumBuffer = nums[pos1];
+    	nums[pos1] = nums[pos2];
+    	nums[pos2] = pos1NumBuffer;
+    }
+    
 }
