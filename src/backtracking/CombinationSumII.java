870ee87c0cc9fc07f1c59a6dd1736f0ecbb655fc
Shijie Zhang <szhang@opentext.com>
2016-08-26 13:01:58 -0700

    init impl

--- src/backtracking/CombinationSumII.java
+++ src/backtracking/CombinationSumII.java
@@ -1,7 +1,12 @@
 package backtracking;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 /*
-Given a collection of candidate numbers (C) and a target number (T), find all unique combinations in C where the candidate numbers sums to T.
+Given a collection of candidate numbers (C) and a target number (T), 
+find all unique combinations in C where the candidate numbers sums to T.
 
 Each number in C may only be used once in the combination.
 
@@ -20,7 +25,35 @@ A solution set is:
 
 public class CombinationSumII 
 {
+	// TODO: combination sum ( positive or negative )
     public List<List<Integer>> combinationSum2( int[] candidates, int target )
     {
+    	List<List<Integer>> allCombs = new LinkedList<>();
+    	List<Integer> oneComb = new LinkedList<>();
+    	// TODO: arrays sort, defensive programming
+    	Arrays.sort( candidates );
+    	generateCombs( allCombs, oneComb, candidates, 0, target );
+    	return allCombs;   	
     }
+    
+    private void generateCombs( List<List<Integer>> allCombs, List<Integer> oneComb, int[] candidates, int startPos, int targetSum )
+    {
+    	if ( targetSum == 0 )
+    	{
+    		allCombs.add( new LinkedList<>( oneComb ) );
+    		return;
+    	}
+    	
+    	for ( int i = startPos; i < candidates.length; i++ )
+    	{
+    		if ( i > startPos
+    				&& candidates[i] == candidates[i-1] )
+    		{
+    			continue;
+    		}
+    		oneComb.add( candidates[i] );
+    		generateCombs( allCombs, oneComb, candidates, i + 1, targetSum - candidates[i] );
+    		oneComb.remove( oneComb.size( ) - 1 );
+    	}
+    }    
 }
