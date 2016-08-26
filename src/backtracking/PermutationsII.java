eb2aa0cd3b759ce9d61dd86bd79f6293d13da15d
Shijie Zhang <szhang@opentext.com>
2016-08-26 13:49:06 -0700

    init impl

--- src/backtracking/PermutationsII.java
+++ src/backtracking/PermutationsII.java
@@ -1,5 +1,9 @@
 package backtracking;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 /*
 Given a collection of numbers that might contain duplicates, return all possible unique permutations.
 
@@ -16,5 +20,36 @@ public class PermutationsII
 {
     public List<List<Integer>> permuteUnique( int[] nums ) 
     {
+    	List<List<Integer>> allPerms = new LinkedList<>();
+    	List<Integer> onePerm = new LinkedList<>();
+    	Arrays.sort( nums );
+    	boolean[] isUsed = new boolean[nums.length];
+    	generatePerms( allPerms, onePerm, nums, isUsed, 0 );
+    	return allPerms;
     }
+    
+    private void generatePerms( List<List<Integer>> allPerms, List<Integer> onePerm, int[] nums, boolean[] isUsed, int totalNumUsed )
+    {   	
+    	if ( totalNumUsed == nums.length )
+    	{
+    		allPerms.add( new LinkedList<>( onePerm ) );
+    		return;
+    	}
+    	
+    	for ( int i = 0 ; i < nums.length; i++ )
+    	{
+    		if ( i > 0 
+    				&& nums[i] == nums[i-1]
+    				&& !isUsed[i] )
+    		{
+    			continue;
+    		}
+    		
+    		isUsed[i] = true;
+    		onePerm.add( nums[i] );
+    		generatePerms( allPerms, onePerm, nums, isUsed, i + 1 );
+    		onePerm.remove( onePerm.size( ) - 1 );
+    		isUsed[i] = false;
+    	}
+    }    
 }
