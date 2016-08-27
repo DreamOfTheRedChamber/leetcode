2fedb2c204573a544ad1e0b2dea00defcfa56b94
FreemanZhang <szhang@opentext.com>
2016-08-27 08:51:02 -0700

    add comments and refactored integer parser section to use built-in func
    1. Leetcode's comment style is bad
    2. Use built-in function when possible
    3. Result wrapper to return two results from recursive function

--- src/design/Codec.java
+++ src/design/Codec.java
@@ -22,7 +22,11 @@ class NodeAndPosWrapper
 public class Codec
 {
 
-	// Encodes a tree to a single string.
+	/**
+	 * Encodes a tree to a single string.
+	 * 
+	 * @return  ',' to separate nodes, '#' for null, actual int for other node
+	 */
 	public String serialize( TreeNode root )
 	{
 		StringBuilder result = new StringBuilder();
@@ -32,6 +36,7 @@ public class Codec
 
 	private void serializeRecurse( TreeNode root, StringBuilder result )
 	{
+		// preorder serialize
 		if ( root == null )
 		{
 			result.append( "#" );
@@ -46,7 +51,11 @@ public class Codec
 		}
 	}
 		
-	// Decodes your encoded data to tree.
+	/**
+	 * Decodes your encoded data to tree.
+	 * 
+	 * @return  deserialized tree root node
+	 */
 	public TreeNode deserialize( String data )
 	{
 		NodeAndPosWrapper result = deserializeRecurse( data, 0 );
@@ -67,30 +76,36 @@ public class Codec
 		}
 		else
 		{
+			// parse integer
 			int rootValue = 0;
 			boolean isNeg = false;
-			while ( pos < data.length()
+			int intStartPos = pos;
+			int intEndPos = pos;
+			while ( pos < data.length() 
 					&& data.charAt( pos ) != ',' )
 			{
 				if ( data.charAt( pos ) == '-' )
 				{
 					isNeg = true;
-					pos++;
-					continue;
+					intStartPos++;
+					intEndPos++;
 				}
-				else
+				else 
 				{
-					rootValue = (int)( data.charAt( pos ) - '0' ) + rootValue * 10;
-					pos++;
+					intEndPos++;
 				}
+				pos++; //
 			}
+			
+			rootValue = Integer.parseInt( data.substring( intStartPos, intEndPos ) );
 			if ( isNeg )
 			{
 				rootValue = -rootValue;
 			}
 			
+			// build tree root
 			TreeNode root = new TreeNode( rootValue );
-			
+			// recursively build left and right subtrees
 			NodeAndPosWrapper leftChildResult = deserializeRecurse( data,  pos + 1 );
 			NodeAndPosWrapper rightChildResult = deserializeRecurse( data, leftChildResult.nextPos );
 			root.left = leftChildResult.node;
@@ -104,16 +119,16 @@ public class Codec
 	{
 		Codec codec = new Codec();
 		TreeNode node1 = new TreeNode( 1 );
-		TreeNode node2 = new TreeNode( 2 );
-		TreeNode node3 = new TreeNode( 3 );
-		TreeNode node4 = new TreeNode( 4 );
-		TreeNode node5 = new TreeNode( 5 );
-		
-		node1.left = node2;
-		node1.right = node3;
-		node3.left = node4;
-		node3.right = node5;
-		
+//		TreeNode node2 = new TreeNode( 2 );
+//		TreeNode node3 = new TreeNode( 3 );
+//		TreeNode node4 = new TreeNode( 4 );
+//		TreeNode node5 = new TreeNode( 5 );
+//		
+//		node1.left = node2;
+//		node1.right = node3;
+//		node3.left = node4;
+//		node3.right = node5;
+
 		String serializedResult = codec.serialize( node1 );
 		System.out.println( serializedResult );
 		TreeNode root = codec.deserialize( serializedResult );
