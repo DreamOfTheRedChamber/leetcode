789e41ed1f45141122e0875ccbbc85fece695ef4
Shijie Zhang <szhang@opentext.com>
2016-08-29 12:23:03 -0700

    init impl

--- src/stack/SimplifyPath.java
+++ src/stack/SimplifyPath.java
@@ -1,5 +1,7 @@
 package stack;
 
+import java.util.Stack;
+
 /*
 Given an absolute path for a file (Unix-style), simplify it.
 
@@ -8,10 +10,39 @@ path = "/home/", => "/home"
 path = "/a/./b/../../c/", => "/c"
  * */
 
+// TODO: README. summarize use of exceptions: EmptyStackException
+
 public class SimplifyPath 
 {
     public String simplifyPath( String path )
     {
-        
+    	String[] tokens = path.split( "/" );
+    	Stack<String> tokenStack = new Stack<>();
+    	
+    	// remove .. and .
+    	for ( String token : tokens )
+    	{
+    		if ( token.equals( "." ))
+    		{
+    			continue;
+    		}
+    		else if ( token.equals( ".." ) )
+    		{
+    			tokenStack.pop( );
+    		}
+    		else
+    		{
+    			tokenStack.push( token );
+    		}
+    	}
+    	
+    	// reconstruct path
+    	StringBuilder simplifiedPath = new StringBuilder();
+    	while( !tokenStack.isEmpty( ) )
+    	{
+    		simplifiedPath.insert( 0, '/' );
+    		simplifiedPath.insert( 0, tokenStack.pop( ) );
+    	}
+    	return simplifiedPath.toString( );
     }
 }
