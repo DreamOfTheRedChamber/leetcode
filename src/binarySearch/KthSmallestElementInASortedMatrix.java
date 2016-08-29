b63247647c70decd433c56c0c19ee57e51f604d0
FreemanZhang <szhang@opentext.com>
2016-08-29 08:16:12 -0700

    fix bugs in coordinate typos
    When a variable's name is really really long such as
    matrix[qHead.xCoor][qHead.yCoor+1] and it needs to be used in multiple
    places, it is really easy to create typos

--- src/binarySearch/KthSmallestElementInASortedMatrix.java
+++ src/binarySearch/KthSmallestElementInASortedMatrix.java
@@ -1,6 +1,10 @@
 package binarySearch;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.PriorityQueue;
+
+import org.junit.Test;
 
 /*
 Given a n x n matrix where each of the rows and columns are sorted in ascending order, find the kth smallest element in the matrix.
@@ -52,17 +56,23 @@ public class KthSmallestElementInASortedMatrix
         			&& !isUsed[qHead.xCoor + 1][qHead.yCoor] )
         	{
         		isUsed[qHead.xCoor + 1][qHead.yCoor] = true;
-        		minQueue.offer( new MatrixEntry( qHead.xCoor + 1, qHead.yCoor, matrix[qHead.xCoor][qHead.yCoor] ) );
+        		minQueue.offer( new MatrixEntry( qHead.xCoor + 1, qHead.yCoor, matrix[qHead.xCoor + 1][qHead.yCoor] ) );
         	}
-        	if ( qHead.xCoor + 1 < heighth 
+        	if ( qHead.yCoor + 1 < width 
         			&& !isUsed[qHead.xCoor][qHead.yCoor + 1] )
         	{
-        		isUsed[qHead.xCoor][qHead.yCoor] = true;
+        		isUsed[qHead.xCoor][qHead.yCoor+1] = true;
         		minQueue.offer( new MatrixEntry( qHead.xCoor, qHead.yCoor+1, matrix[qHead.xCoor][qHead.yCoor + 1] ) );
         	}
         }
         
         return result;
+    }
+    
+    @Test
+    public void test()
+    {
+    	assertEquals( 2, kthSmallest( new int[][]{ { 1, 2 }, { 3, 3 } }, 2 ) );
     }
 }
 
