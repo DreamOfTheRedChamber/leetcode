726004af3279af3d5da39cdba92bf6bf5fe0f58e
FreemanZhang <szhang@opentext.com>
2016-08-29 09:02:55 -0700

    fix bugs
    ResultWrapper is still ugly designed.. The main problem is numNodes is
    not reliable. Sometimes it contains right value. Sometimes it contains
    bad value. Should come up with a more elegant solution

--- src/binarySearch/KthSmallestElementInABST.java
+++ src/binarySearch/KthSmallestElementInABST.java
@@ -1,5 +1,9 @@
 package binarySearch;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 import utility.TreeNode;
 
 /*
@@ -15,12 +19,14 @@ What if the BST is modified (insert/delete operations) often and you need to fin
 // TODO: why cannot have private classes
 class ResultWrapper
 {
-	public final int numNodes;
+	public final int numNodes;	// used internally only
 	public final TreeNode kthNode;
-	public ResultWrapper( TreeNode kthNode,  int numNodes )
+	public final boolean isKthNodeFound;
+	public ResultWrapper( TreeNode kthNode,  int numNodes, boolean isKthNodeFound )
 	{
 		this.numNodes = numNodes;
 		this.kthNode = kthNode;
+		this.isKthNodeFound = isKthNodeFound;
 	}
 }
 
@@ -40,12 +46,13 @@ public class KthSmallestElementInABST
     
     private ResultWrapper kthSmallestRecurse( TreeNode root, int kth )
     {
-    	int totalNumNodes = 0;
+    	int totalNumNodes = 0;    	
     	if ( root.left != null )
     	{
     		ResultWrapper leftChildResult = kthSmallestRecurse( root.left, kth );
-    		if ( leftChildResult.kthNode != null )
+    		if ( leftChildResult.isKthNodeFound )
     		{
+    			// kth node exist in left subtree
     			return leftChildResult;
     		}
     		else
@@ -54,17 +61,35 @@ public class KthSmallestElementInABST
     		}
     	}
     	
+    	// kth node is root
     	totalNumNodes += 1;
     	if ( totalNumNodes == kth )
     	{
-    		return new ResultWrapper( root, kth );
-    	}
+    		return new ResultWrapper( root, kth, true );
+    	}    	
     	
     	if ( root.right != null )
     	{
-    		return kthSmallestRecurse( root.right, kth - totalNumNodes );
+    		ResultWrapper rightChildResult = kthSmallestRecurse( root.right, kth - totalNumNodes );
+    		if ( rightChildResult.isKthNodeFound )
+    		{
+    			return rightChildResult;
+    		}
+    		else
+    		{	
+    			totalNumNodes += rightChildResult.numNodes;
+    		}
     	}
     	
-    	return new ResultWrapper( null, -1 );
+    	return new ResultWrapper( null, totalNumNodes, false );
+    }
+    
+    @Test
+    public void test()
+    {
+    	TreeNode node1 = new TreeNode( 2 );
+    	TreeNode node2 = new TreeNode( 1 );
+    	node1.left = node2;
+    	assertEquals( 2, kthSmallest( node1, 2 ) );
     }
 }
