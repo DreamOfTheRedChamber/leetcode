e32b3fad6b506cf930a7b8fbcc9a2c0b5e06d034
Shijie Zhang <szhang@opentext.com>
2016-08-29 13:00:03 -0700

    init impl

--- src/stack/EvaluateReversePolishNotation.java
+++ src/stack/EvaluateReversePolishNotation.java
@@ -1,5 +1,7 @@
 package stack;
 
+import java.util.Stack;
+
 /*
 Evaluate the value of an arithmetic expression in Reverse Polish Notation.
 
@@ -12,8 +14,41 @@ Some examples:
 
 public class EvaluateReversePolishNotation 
 {
-    public int evalRPN(String[] tokens) 
+    public int evalRPN( String[] tokens ) 
     {
-        
+    	Stack<Integer> operandStack = new Stack<>();
+    	for ( String token : tokens )
+    	{
+    		if ( token.equals( "+" )
+    				|| token.equals( "-" )
+    				|| token.equals( "*" ) 
+    				|| token.equals( "/" ) )
+    		{
+    			int operand2 = operandStack.pop( );
+    			int operand1 = operandStack.pop( );
+    			if ( token.equals( "+" ) )
+    			{
+    				operandStack.push( operand1 + operand2 );
+    			}
+    			else if ( token.equals( "-" ) )
+    			{
+    				operandStack.push( operand1 - operand2 );
+    			}
+    			else if ( token.equals( "*" ) )
+    			{
+    				operandStack.push( operand1 * operand2 );
+    			}
+    			else
+    			{
+    				operandStack.push( operand1 / operand2 );
+    			}
+    		}
+    		else
+    		{
+    			operandStack.push( Integer.valueOf( token ) );
+    		}
+    	}
+    	
+    	return operandStack.pop( );
     }
 }
