b44c696c978e7371f10442ff42fdedc40f128ae5
FreemanZhang <szhang@opentext.com>
2016-09-02 08:12:54 -0700

    fix bugs but still remains to be worked on
    dynamic programming based problem - array index out of boundary?

--- src/dynamicprogramming/DecodeWays.java
+++ src/dynamicprogramming/DecodeWays.java
@@ -1,5 +1,9 @@
 package dynamicprogramming;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 /**
 A message containing letters from A-Z is being encoded to numbers using the following mapping:
 
@@ -14,7 +18,7 @@ Given encoded message "12", it could be decoded as "AB" (1 2) or "L" (12).
 
 The number of ways decoding "12" is 2.
  */
-//TO_TEST
+// TO_TEST
 public class DecodeWays
 {
     public int numDecodings( String s )
@@ -23,30 +27,41 @@ public class DecodeWays
         {
         	throw new IllegalArgumentException("");
         }
-        
-        // TODO: check condition in dp
-        if ( s.length() <= 1 )
+        if ( s.length() == 0 )
         {
-        	return s.length();
+        	return 0;
         }
-                
+        
         int[] numEndingAtPos = new int[s.length()];
         // init dp table
-        numEndingAtPos[0] = 1;
-        numEndingAtPos[1] = 1 + ( isValidChar( s, 0, 1 ) ? 1 : 0 );
+        numEndingAtPos[0] = s.charAt( 0 ) == '0' ? 0 : 1;
         // fill in dp table
-        for ( int i = 2; i < s.length(); i++ )
+        for ( int i = 1; i < s.length(); i++ )
         {
-        	numEndingAtPos[i] = numEndingAtPos[i-1] + ( isValidChar( s, i-1, i ) ? numEndingAtPos[i-2] : 0 );
+        	if ( s.charAt( i ) == '0' )
+        	{
+        		numEndingAtPos[i] = isValidChar( s, i-1, i ) ? numEndingAtPos[i-1] : 0; // assume always valid
+        	}
+        	else
+        	{
+        		if ( i == 2 )
+        		{
+                	numEndingAtPos[i] = numEndingAtPos[i-1] + ( isValidChar( s, i-1, i ) ? 1 : 0 );         			
+        		}
+        		else
+        		{
+                	numEndingAtPos[i] = numEndingAtPos[i-1] + ( isValidChar( s, i-1, i ) ? numEndingAtPos[i-2] : 0 );
+        		}
+        	}
         }
         return numEndingAtPos[s.length()-1];
     }
     
     private boolean isValidChar( String s, int start, int end )
     {
-    	int convertedInt = Integer.parseInt( s.substring(start, end + 1) );
-    	if ( convertedInt >= 0
-    			&& convertedInt <= 25 )
+    	int convertedInt = Integer.parseInt( s.substring( start, end + 1 ) );
+    	if ( convertedInt >= 10		
+    			&& convertedInt <= 26 )		// '06' not valid
     	{
     		return true;
     	}
@@ -54,5 +69,15 @@ public class DecodeWays
     	{
     		return false;
     	}
+    }
+
+    @Test
+    public void test()
+    {
+    	assertEquals( 2, numDecodings( "12" ) );
+    	assertEquals( 2, numDecodings( "26" ) );
+    	assertEquals( 0, numDecodings( "0" ) );
+    	assertEquals( 1, numDecodings( "10" ) );
+    	assertEquals( 1, numDecodings( "100" ) );
     }
 }
