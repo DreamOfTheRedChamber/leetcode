118f5435dc34b4b45a82a99bd42bd3d908131ca7
FreemanZhang <szhang@opentext.com>
2016-09-02 09:40:47 -0700

    fix bugs
    1. the addition of another boolean[] array makes this program has a
    different structure with the previous one
    2. totalNumUsed

--- src/backtracking/PermutationsII.java
+++ src/backtracking/PermutationsII.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
+import org.junit.Test;
+
 /*
 Given a collection of numbers that might contain duplicates, return all possible unique permutations.
 
@@ -15,7 +17,6 @@ For example,
   [2,1,1]
 ]
  * */
-// TO_TEST
 public class PermutationsII 
 {
     public List<List<Integer>> permuteUnique( int[] nums ) 
@@ -30,26 +31,37 @@ public class PermutationsII
     
     private void generatePerms( List<List<Integer>> allPerms, List<Integer> onePerm, int[] nums, boolean[] isUsed, int totalNumUsed )
     {   	
+    	// recursion base
     	if ( totalNumUsed == nums.length )
     	{
     		allPerms.add( new LinkedList<>( onePerm ) );
     		return;
     	}
     	
+    	// recursion body
     	for ( int i = 0 ; i < nums.length; i++ )
-    	{
-    		if ( i > 0 
-    				&& nums[i] == nums[i-1]
-    				&& !isUsed[i] )
+    	{    		
+    		if ( !isUsed[i] )
     		{
-    			continue;
+	    		if ( i > 0 
+	    				&& nums[i] == nums[i-1]
+	    				&& !isUsed[i-1] )
+	    		{
+	    			continue;
+	    		}
+	    		
+	    		isUsed[i] = true;
+	    		onePerm.add( nums[i] );
+	    		generatePerms( allPerms, onePerm, nums, isUsed, totalNumUsed + 1 );
+	    		onePerm.remove( onePerm.size( ) - 1 );
+	    		isUsed[i] = false;
     		}
-    		
-    		isUsed[i] = true;
-    		onePerm.add( nums[i] );
-    		generatePerms( allPerms, onePerm, nums, isUsed, i + 1 );
-    		onePerm.remove( onePerm.size( ) - 1 );
-    		isUsed[i] = false;
     	}
-    }    
+    }
+        
+    @Test
+    public void test()
+    {
+    	System.out.println( permuteUnique( new int[]{ 1, 1, 2 } ) );    	
+    }
 }
