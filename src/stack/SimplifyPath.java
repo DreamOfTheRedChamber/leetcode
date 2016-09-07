ce7c332cc88881fe76f2045493067dd3a356e88c
FreemanZhang <szhang@opentext.com>
2016-09-07 09:05:02 -0700

    fix partial bugs
    1. bugs remained to be fixed: The test cases are really confusing
    themselves. Why /home/../../.. should return "" but "/.." just return
    "/"
    2. fixed bugs: add a "" placeholder into the tokenStack to handle
    /home/../../.. case and "/c" case (add the starting '/')
    3. fixed bugs: remember to delete tailing '/' for case like "/c"

--- src/stack/SimplifyPath.java
+++ src/stack/SimplifyPath.java
@@ -1,6 +1,10 @@
 package stack;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.Stack;
+
+import org.junit.Test;
 
 /*
 Given an absolute path for a file (Unix-style), simplify it.
@@ -11,7 +15,6 @@ path = "/a/./b/../../c/", => "/c"
  * */
 
 // TODO: README. summarize use of exceptions: EmptyStackException
-//TO_TEST
 public class SimplifyPath 
 {
     public String simplifyPath( String path )
@@ -19,17 +22,22 @@ public class SimplifyPath
     	// whether the path always starts with '/'
     	String[] tokens = path.split( "/" );
     	Stack<String> tokenStack = new Stack<>();
-    	
+    	tokenStack.push( "" );
+
     	// remove .. and .
     	for ( String token : tokens )
     	{
-    		if ( token.equals( "." ))
+    		if ( token.equals( "." )
+    				|| token.equals( "" ) ) // handle "//" case
     		{
     			continue;
     		}
     		else if ( token.equals( ".." ) )
     		{
-    			tokenStack.pop( );
+    			if ( !tokenStack.isEmpty() )
+    			{
+    				tokenStack.pop( );
+    			}
     		}
     		else
     		{
@@ -41,9 +49,30 @@ public class SimplifyPath
     	StringBuilder simplifiedPath = new StringBuilder();
     	while( !tokenStack.isEmpty( ) )
     	{
+    		String token = tokenStack.pop();
     		simplifiedPath.insert( 0, '/' );
-    		simplifiedPath.insert( 0, tokenStack.pop( ) );
+        	simplifiedPath.insert( 0, token );
+    	}
+    	// delete tailing '/'
+    	if ( simplifiedPath.length() > 1 )
+    	{
+    		simplifiedPath.deleteCharAt( simplifiedPath.length() - 1 );
     	}
     	return simplifiedPath.toString( );
+    }
+    
+    @Test
+    public void test()
+    {
+    	assertEquals( "", simplifyPath( "/home/../../.." ) );
+
+    	assertEquals( "/", simplifyPath( "/" ) );
+    	assertEquals( "/", simplifyPath( "/." ) );
+    	assertEquals( "/", simplifyPath( "/.." ) );
+    	
+    	assertEquals( "", simplifyPath( "/../" ) );
+    	assertEquals( "/c", simplifyPath( "/a/./b/../../c/" ) );
+    	
+    	assertEquals( "/home/foo", simplifyPath( "/home//foo/" ) );
     }
 }
