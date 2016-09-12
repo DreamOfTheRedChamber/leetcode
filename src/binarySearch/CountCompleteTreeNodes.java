87b61e95c2035e5775d86786efe4b95ad3052aba
FreemanZhang <szhang@opentext.com>
2016-09-12 08:35:32 -0700

    init recursive implementation

--- src/binarySearch/CountCompleteTreeNodes.java
+++ src/binarySearch/CountCompleteTreeNodes.java
@@ -27,85 +27,29 @@ public class CountCompleteTreeNodes
     		return 0;
     	}
     	
-    	// calc tree depth
-        int treeDepth = 0;
-        TreeNode currNode = root;
-        while ( currNode != null )
-        {
-        	treeDepth++;
-        	currNode = currNode.left;
-        }
-        
-        // binary search
-        int start = 1;
-        int end = (int) Math.pow( 2, treeDepth );
-        while ( start + 1 < end )
-        {
-        	int mid = ( end - start ) / 2 + start;
-        	if ( doesLeafExist( root, mid, treeDepth ) )
-        	{
-        		start = mid;
-        	}
-        	else
-        	{
-        		end = mid;
-        	}
-        }
-
-        int numNonLeafNodes = ( int )( Math.pow( 2, treeDepth-1) ) - 1;
-        if ( doesLeafExist( root, end, treeDepth ) )
-        {
-        	return numNonLeafNodes + end;
-        }
-        else
-        {
-        	return numNonLeafNodes + start;
-        }
-    }
-    
-    private boolean doesLeafExist( TreeNode root, int kthLeaf, int treeDepth )
-    {
-    	Stack<Boolean> rootToKthLeafPath = new Stack<>(); // false -> left, true -> right
-    	int numNonLeafNodes = ( int )( Math.pow( 2, treeDepth-1) ) - 1;
-    	
-    	// go from leaf to root, put path in a stack
-    	int currNodeOrder = numNonLeafNodes + kthLeaf;
-    	while ( currNodeOrder != 1 )
+    	// calc left most branch depth
+    	int leftMostDepth = 0;
+    	for ( TreeNode currNode = root; currNode != null; currNode = currNode.left )
     	{
-    		if ( currNodeOrder % 2 == 1 )
-    		{
-    			rootToKthLeafPath.push( true );
-    		}
-    		else
-    		{
-    			rootToKthLeafPath.push( false );
-    		}
-    		currNodeOrder /= 2;
+    		leftMostDepth++;
+    	}
+    	// calc right most branch depth
+    	int rightMostDepth = 0;
+    	for ( TreeNode currNode = root; currNode != null; currNode = currNode.right )
+    	{
+    		rightMostDepth++;
     	}
     	
-    	// go from root to leaf, check node existence
-    	TreeNode currNode = root;
-    	while ( !rootToKthLeafPath.isEmpty() )
-    	{
-    		boolean shouldGoRight = rootToKthLeafPath.pop();
-    		if ( shouldGoRight )
-    		{
-    			currNode = currNode.right;
-    		}
-    		else
-    		{
-    			currNode = currNode.left;
-    		}
-    	}
     	
-    	// whether specific leaf exists
-    	if ( currNode == null )
+    	if ( leftMostDepth == rightMostDepth )
     	{
-    		return false;
+    		return (int) Math.pow( 2, rightMostDepth ) - 1;
     	}
-    	else
+    	else // tree not a complete tree
     	{
-    		return true;
+    		int numLeftSubtree = countNodes( root.left );
+    		int numRightSubtree = countNodes( root.right );
+    		return numLeftSubtree + 1 + numRightSubtree;
     	}
     }
     
@@ -119,7 +63,7 @@ public class CountCompleteTreeNodes
     	node1.left = node2;
     	node1.right = node3;
     	node2.left = node4;
-    	assertEquals( 4, countNodes(node1) );
+    	assertEquals( 4, countNodes( node1 ) );
     	assertEquals( 11, countNodes( TreeFactory.createCompleteTree() ) );
     	assertEquals( 15, countNodes( TreeFactory.createFullTree() ) );
     }
