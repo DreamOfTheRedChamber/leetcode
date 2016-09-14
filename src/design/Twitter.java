0482db614b8023437109cd1c6bee8574583763d5
FreemanZhang <szhang@opentext.com>
2016-09-14 08:20:04 -0700

    Twitter
    failure attempt. 
    0. attempt is based on pull-based twitter model. put majority logic
    inside getNewsFeed, which is actually a merge k list algorithm. but
    there are many implementation details to consider
    1. Data structure is more complex than imagined. 
    2. Think every detail out before writing. This is not possible.

--- src/design/Twitter.java
+++ src/design/Twitter.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.ListIterator;
 import java.util.Map;
+import java.util.PriorityQueue;
 import java.util.Set;
 
 import org.junit.Test;
@@ -18,34 +18,96 @@ getNewsFeed(userId): Retrieve the 10 most recent tweet ids in the user's news fe
 follow(followerId, followeeId): Follower follows a followee.
 unfollow(followerId, followeeId): Follower unfollows a followee.
 */
-// TO_START
 public class Twitter 
 {
-    /** Initialize your data structure here. */
+	
+	private Map<Integer, Set<Integer>> userToFollowee;
+	private Map<Integer, List<Tweet>> userToTweets;
+	private int timestamp;
+	private final static int FEED_SIZE = 10;
+	
+	/** Initialize your data structure here. */
     public Twitter() 
     {
+    	userToFollowee = new HashMap<>();
+    	userToTweets = new HashMap<>();
+    	timestamp = 0;
     }
     
     /** Compose a new tweet. */
-    public void postTweet(int userId, int tweetId) 
+    public void postTweet( int userId, int tweetId ) 
     {
+    	setUpUser( userId );
+    	List<Tweet> tweetList = userToTweets.get( userId );
+    	tweetList.add( new Tweet( tweetId, timestamp, tweetList.size() ) );
+    	timestamp++;
     }
     
     /** Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent. */
-    public List<Integer> getNewsFeed(int userId) 
+    public List<Integer> getNewsFeed( int userId )
     {
+    	Set<Integer> followeeList = userToFollowee.get( userId );
+    	List<List<Tweet>> followeeTweets = new ArrayList<>();
+    	for ( Integer followee : followeeList )
+    	{
+    		followeeTweets.add( userToTweets.get( followee ) );
+    	}
+    	PriorityQueue<Tweet> minQueue = new PriorityQueue<>( (o1, o2) -> ( o2.timestamp - o1.timestamp ) );
+    	for ( List<Tweet> tweetList : followeeTweets )
+    	{
+    		if ( tweetList.size() > 0 )
+    		{
+    			minQueue.add( tweetList.get( tweetList.size() - 1 ) );
+    		}
+    	}
+    	int counter = 0;
+    	List<Integer> newsFeed = new ArrayList<>();
+    	while ( !minQueue.isEmpty() 
+    			&& counter < FEED_SIZE )
+    	{
+    		Tweet latestTweet = minQueue.poll();
+    		newsFeed.add( latestTweet.tweetID );
+    		if ( latestTweet.index > 0 )
+    		{
+    			minQueue.offer( userTo)
+    		}
+    	}
     }
     
     /** Follower follows a followee. If the operation is invalid, it should be a no-op. */
     public void follow(int followerId, int followeeId) 
     {
+    	setUpUser( followerId );
+    	setUpUser( followeeId );
+    	userToFollowee.get( followerId ).add( followeeId );
     }
     
     /** Follower unfollows a followee. If the operation is invalid, it should be a no-op. */
     public void unfollow(int followerId, int followeeId) 
     {
+    	userToFollowee.get( followerId ).remove( followeeId );
     }
-   
+
+    private void setUpUser( int userId )
+    {
+    	userToFollowee.putIfAbsent( userId, new HashSet<>() );
+    	userToFollowee.get( userId ).add( userId ); // add user itself to followeelist
+    	userToTweets.putIfAbsent( userId, new ArrayList<>() );	
+    }
+    
+    class Tweet
+    {
+    	public final int tweetID;
+    	public final int timestamp;
+    	public final int index;
+    	public Tweet( int tweetID, int timestamp, int index )
+    	{
+    		this.tweetID = tweetID;
+    		this.timestamp = timestamp;
+    		this.index = index;
+    	}
+    }    
+
     @Test
     public void test()
     {
