a932183d3b95a9b7410f456e083a5e4590946eee
FreemanZhang <szhang@opentext.com>
2016-09-16 14:37:59 -0700

    fix bugs
    the initial value for firstBiggerPos

--- src/stack/VerifyPreorderSequenceInBinarySearchTree.java
+++ src/stack/VerifyPreorderSequenceInBinarySearchTree.java
@@ -13,13 +13,14 @@ You may assume each number in the sequence is unique.
 Follow up:
 Could you do it using only constant space complexity?
  * */
-//TO_TEST
+
 public class VerifyPreorderSequenceInBinarySearchTree 
 {
 	@Test
 	public void test()
 	{
-		assertTrue( verifyPreorder( new int[]{ 4, 1, 3, 2, 9, 6, 5, 8, 7, 10, 11 } ) );
+		assertTrue( verifyPreorder( new int[]{ 4, 2, 3, 6, 5 } ) );
+		assertFalse( verifyPreorder( new int[]{ 4, 2, 6, 5, 3 } ) );		
 		assertFalse( verifyPreorder( new int[]{ 4, 5, 3, 2, 9, 6, 1, 8, 7, 10, 11 } ) );
 	}
 	
@@ -34,17 +35,17 @@ public class VerifyPreorderSequenceInBinarySearchTree
     		return true;
     	}
     	
-    	return verifyPreorderRecurse( preorder, 0, preorder.length - 1 );    	
+    	return verifyPreorder( preorder, 0, preorder.length - 1 );    	
     }
     
-    private boolean verifyPreorderRecurse( int[] preorder, int startPos, int endPos )
+    private boolean verifyPreorder( int[] preorder, int startPos, int endPos )
     {
     	if ( endPos <= startPos )
     	{
     		return true;
     	}
     	
-    	int firstBiggerPos = 0; // first pos bigger than tree root
+    	int firstBiggerPos = endPos + 1; // first pos bigger than tree root
     	// assume no duplicates
     	// left subtree all smaller
     	for ( int i = startPos; i <= endPos; i++ )
@@ -55,16 +56,16 @@ public class VerifyPreorderSequenceInBinarySearchTree
     			break;
     		}
     	}
-    	// right subtree all bigger
-    	for ( int i = firstBiggerPos; i <= endPos; i++ )
-    	{
-    		if ( preorder[i] < preorder[startPos] )
-    		{
-    			return false;
-    		}
-    	}
+	    	// right subtree all bigger
+	    for ( int i = firstBiggerPos; i <= endPos; i++ )
+	    {
+	    	if ( preorder[i] < preorder[startPos] )
+	    	{
+	    		return false;
+	    	}
+	    }
     	
-    	return verifyPreorderRecurse( preorder, startPos + 1, firstBiggerPos - 1 )
-    			&& verifyPreorderRecurse( preorder, firstBiggerPos, endPos );
+    	return verifyPreorder( preorder, startPos + 1, firstBiggerPos - 1 )
+    			&& verifyPreorder( preorder, firstBiggerPos, endPos );
     }
 }
