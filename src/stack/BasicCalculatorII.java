6a127fecbdfaf5b6a3f7957129bd63c788a8d27f
FreemanZhang <szhang@opentext.com>
2016-09-19 09:38:07 -0700

    init impl

--- src/stack/BasicCalculatorII.java
+++ src/stack/BasicCalculatorII.java
@@ -1,5 +1,11 @@
 package stack;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.Stack;
+
+import org.junit.Test;
+
 /**
 Implement a basic calculator to evaluate a simple expression string.
 
@@ -13,11 +19,94 @@ Some examples:
 " 3+5 / 2 " = 5
 Note: Do not use the eval built-in library function.
  */
-//TO_HURRY
 public class BasicCalculatorII
 {
-    public int calculate(String s) 
+	@Test
+	public void test()
+	{
+		assertEquals(7, calculate("3 + 2*2") );
+		assertEquals(1, calculate("3/2") );
+		assertEquals(5, calculate("3+5/2") );
+	}
+	
+    public int calculate( String s )
     {
-        
+    	if ( s == null 
+    			|| s.length() == 0 )
+    	{
+    		throw new IllegalArgumentException("");
+    	}
+    	// assertions on validity
+    	
+    	Stack<Integer> operandStack = new Stack<>();
+    	Stack<Character> operatorStack = new Stack<>();
+    	int currPos = 0;
+    	while ( currPos < s.length( ) )
+    	{
+    		if ( s.charAt( currPos ) == ' ' )
+    		{
+    			currPos++;
+    		}
+    		// pop from stack and calculate when dealing with +, -
+    		else if ( s.charAt( currPos ) == '+' 
+    				|| s.charAt( currPos ) == '-' )    			
+    		{
+    			if ( !operatorStack.isEmpty( ) )
+    			{
+    	    		calculate( operandStack, operatorStack );
+    			}    			
+    			operatorStack.push( s.charAt( currPos ) );    			
+    			currPos++;
+    		}
+    		else if ( s.charAt( currPos ) == '*'
+    				|| s.charAt( currPos ) == '/' )
+    		{
+    			operatorStack.push( s.charAt( currPos ) );
+    			currPos++;
+    		}
+    		// push into stack when dealing with num
+    		else
+    		{
+	    		int value = 0;
+	    		while ( currPos < s.length( ) 
+	    				&& s.charAt( currPos ) >= '0'
+	    				&& s.charAt( currPos ) <= '9' )
+	    		{
+	    			value = value * 10 + s.charAt( currPos ) - '0';
+	    			currPos++;
+	    		}
+	    		operandStack.push( value );
+    		}
+    	}
+    	
+    	while ( !operatorStack.isEmpty( ) )
+    	{
+    		calculate( operandStack, operatorStack );
+    	}
+    	return operandStack.pop();    	
+    }
+    
+    private void calculate( Stack<Integer> operandStack, Stack<Character> operatorStack )
+    {
+		int operand2 = operandStack.pop( );
+		int operand1 = operandStack.pop( );
+		char operator = operatorStack.pop( );
+
+    	if ( operator == '+' )
+    	{
+    		operandStack.push( operand1 + operand2 );
+    	}
+    	else if ( operator == '-' )
+    	{
+    		operandStack.push( operand1 - operand2 );
+    	}
+    	else if ( operator == '*' )
+    	{
+    		operandStack.push( operand1 * operand2 );
+    	}
+    	else
+    	{
+    		operandStack.push( operand1 / operand2 );
+    	}
     }
 }
