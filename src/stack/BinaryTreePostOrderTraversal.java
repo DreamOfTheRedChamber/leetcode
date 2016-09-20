97f05c377a3c950ff0a8370c4304c763fc64307a
Shijie Zhang <szhang@opentext.com>
2016-09-20 13:25:58 -0700

    init impl with Qinyuan's method

--- src/stack/BinaryTreePostOrderTraversal.java
+++ src/stack/BinaryTreePostOrderTraversal.java
@@ -1,7 +1,57 @@
 package stack;
 
-// TO_IMME
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+import utility.TreeNode;
+
+class Pair
+{
+	public final int type; // 1 for visit, 2 for print
+	public final TreeNode node;
+	public Pair( int type, TreeNode node )
+	{
+		this.type = type;
+		this.node = node;
+	}
+}
+
 public class BinaryTreePostOrderTraversal 
 {
+	private final static int VISIT = 1;
+	private final static int PRINT = 2;
 	
+	public List<Integer> postorderTraversal( TreeNode root )
+	{
+		List<Integer> result = new ArrayList<>();		
+		if ( root == null )
+		{
+			return result;
+		}
+		
+		Stack<Pair> postOrderStack = new Stack<>();
+		postOrderStack.push( new Pair( 1, root) );
+		while ( !postOrderStack.isEmpty( ) )
+		{
+			Pair stackTop = postOrderStack.pop( );
+			if ( stackTop.type == VISIT )
+			{
+				postOrderStack.push( new Pair( PRINT, stackTop.node ) );
+				if ( stackTop.node.right != null )
+				{
+					postOrderStack.push( new Pair( VISIT, stackTop.node.right ) );
+				}
+				if ( stackTop.node.left != null )
+				{
+					postOrderStack.push( new Pair( VISIT, stackTop.node.left ) );
+				}
+			}
+			else
+			{
+				result.add( stackTop.node.val );
+			}
+		}
+		return result;
+	}
 }
