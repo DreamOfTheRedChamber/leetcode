552ea5223c4579fbeb15e278b625dac4a2db03ec
Shijie Zhang <szhang@opentext.com>
2016-09-27 13:25:09 -0700

    init impl

--- src/sort/SortColors.java
+++ src/sort/SortColors.java
@@ -8,11 +8,43 @@ Here, we will use the integers 0, 1, and 2 to represent the color red, white, an
 Note:
 You are not suppose to use the library's sort function for this problem.
  */
-//TO_HURRY
 public class SortColors
 {
-    public void sortColors(int[] nums) 
+    public void sortColors( int[] nums )
     {
-        
+    	if ( nums.length  == 0 )
+    	{
+    		return;
+    	}
+    	
+    	int white = 0;
+    	int red = 0;
+    	int blue = nums.length - 1;
+    	while ( red < blue )
+    	{
+    		if ( nums[red] == 0 )
+    		{
+    			red++;
+    		}
+    		else if ( nums[red] == 1 )
+    		{
+    			swap( nums, red, white );
+    			red++;
+    			white++;
+    		}
+    		else
+    		{
+    			swap( nums, red, blue );
+    			red++;
+    			blue--;
+    		}
+    	}
+    }
+    
+    private void swap( int[] nums, int start, int end )
+    {
+    	int buffer = nums[start];
+    	nums[start] = nums[end];
+    	nums[end] = buffer;
     }
 }
