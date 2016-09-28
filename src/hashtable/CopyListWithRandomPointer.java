07e770deb22a457397a9b167dcd3f0c422cdf6ae
Shijie Zhang <szhang@opentext.com>
2016-09-28 12:55:38 -0700

    init impl

--- src/hashtable/CopyListWithRandomPointer.java
+++ src/hashtable/CopyListWithRandomPointer.java
@@ -1,20 +1,63 @@
 package hashtable;
 
-/**
- * Definition for singly-linked list with a random pointer.
- * class RandomListNode {
- *     int label;
- *     RandomListNode next, random;
- *     RandomListNode(int x) { this.label = x; }
- * };
- */
-// TO_IMME
+
+// Definition for singly-linked list with a random pointer.
+ class RandomListNode 
+ {
+     int label;
+     RandomListNode next, random;
+     RandomListNode(int x) { this.label = x; }
+ };
+ 
 public class CopyListWithRandomPointer
 {
-
-    public RandomListNode copyRandomList(RandomListNode head) 
+    public RandomListNode copyRandomList( RandomListNode head )
     {
-        
+    	if ( head == null )
+    	{
+    		return null;
+    	}
+    	
+    	// create and link next node
+    	RandomListNode currNode = head;
+    	while ( currNode != null )
+    	{
+    		RandomListNode nextNode = currNode.next;
+    		
+    		RandomListNode copiedNode = new RandomListNode( currNode.label );
+    		currNode.next = copiedNode;
+    		copiedNode.next = nextNode;
+    		
+    		currNode = nextNode;
+    	}
+    	
+    	// assign random pointer
+    	currNode = head;
+    	while ( currNode != null )
+    	{
+    		RandomListNode nextNode = currNode.next.next;
+    		
+    		currNode.next.random = currNode.random.next;
+    		
+    		currNode = nextNode;
+    	}
+    	
+    	// assign next pointer
+    	RandomListNode copiedHead = head.next;
+    	currNode = head;
+    	RandomListNode currCopiedNode = copiedHead;
+    	while ( currNode != null )
+    	{
+    		RandomListNode nextNode = currCopiedNode.next;
+    		RandomListNode nextCopiedNode = nextNode.next;
+    		
+    		currNode.next = nextNode;
+    		currCopiedNode.next = nextCopiedNode;
+    		
+    		currNode = nextNode;
+    		currCopiedNode = nextCopiedNode;
+    	}
+    	
+    	return copiedHead;
     }
-
 }
