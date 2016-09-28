aa22d9394b399a593166907ae637c091a4fc5148
Shijie Zhang <szhang@opentext.com>
2016-09-27 17:11:28 -0700

    add problem template

---
+++ src/palindrome/LongestPalindromicSubstring.java
@@ -0,0 +1,14 @@
+package palindrome;
+
+/*
+Given a string S, find the longest palindromic substring in S. 
+You may assume that the maximum length of S is 1000, and there exists one unique longest palindromic substring.
+*/
+
+public LongestPalindromicSubstring
+{
+	public String longestPalindrome(String s)
+	{
+        
+    }
+}
