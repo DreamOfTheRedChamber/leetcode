9edb352595e82ad4e4ba34ec51de8eef6f68e2f7
Shijie Zhang <szhang@opentext.com>
2016-09-28 12:26:51 -0700

    init impl

--- src/dfs/LargestBSTSubtree.java
+++ src/dfs/LargestBSTSubtree.java
@@ -1,5 +1,7 @@
 package dfs;
 
+import utility.TreeNode;
+
 /**
 Given a binary tree, find the largest subtree which is a Binary Search Tree (BST), where largest means subtree with largest number of nodes in it.
 
@@ -17,11 +19,51 @@ Show Hint
 Follow up:
 Can you figure out ways to solve it with O(n) time complexity?
  */
-//TO_HURRY
+
+// TO_TEST
+// TODO: improve complexity
 public class LargestBSTSubtree
 {
-    public int largestBSTSubtree(TreeNode root) 
+    public int largestBSTSubtree( TreeNode root )
     {
-        
+    	if ( root == null )
+    	{
+    		return 0;
+    	}
+    	if ( root.left == null 
+    			&& root.right == null )
+    	{
+    		return 1;
+    	}
+    	
+    	int leftResult = largestBSTSubtree( root.left );
+    	int rightResult = largestBSTSubtree( root.right );
+    	
+    	if ( leftResult >= 0 
+    			&& rightResult >= 0 )
+    	{
+    		int largestLeftChild = root.val;
+    		TreeNode currNode = root.left;
+    		while( currNode != null )
+    		{
+    			largestLeftChild = currNode.val;
+    			currNode = currNode.right;
+    		}
+    		
+    		int smallestRightChild = root.val;
+    		currNode = root.right;
+    		while ( currNode != null )
+    		{
+    			smallestRightChild = currNode.val;
+    			currNode = currNode.left;
+    		}
+    		
+    		if ( smallestRightChild >= root.val
+    				&& largestLeftChild <= root.val )
+    		{
+    			return largestLeftChild + smallestRightChild + 1;
+    		}
+    	}
+    	return Math.max( leftResult, rightResult );
     }
 }
