3caa18c7daed2ad78b42495c2a91044d7942a0b2
FreemanZhang <szhang@opentext.com>
2016-09-28 08:50:27 -0700

    init impl

--- src/dynamicprogramming/MaximumProductSubarray.java
+++ src/dynamicprogramming/MaximumProductSubarray.java
@@ -7,11 +7,35 @@ For example, given the array [2,3,-2,4],
 the contiguous subarray [2,3] has the largest product = 6.
  */
 
-// TO_HURRY
 public class MaximumProductSubarray
 {
-    public int maxProduct(int[] nums) 
+    public int maxProduct( int[] nums )
     {
-        
+    	if ( nums.length == 0 )
+    	{
+    		throw new IllegalArgumentException("");
+    	}
+    	
+    	// compute local peek values
+    	int[] localMaxProduct = new int[nums.length-1];
+    	int[] localMinProduct = new int[nums.length-1];
+    	localMaxProduct[0] = nums[0];
+    	localMinProduct[0] = nums[0];
+    	for ( int i = 1; i < nums.length; i++ )
+    	{
+    		localMaxProduct[i] = Math.max( nums[i], 
+    										Math.max( localMaxProduct[i-1] * nums[i], localMinProduct[i-1] * nums[i] ) );
+    		localMinProduct[i] = Math.min( nums[i], 
+    										Math.min( localMaxProduct[i-1] * nums[i], localMinProduct[i-1] * nums[i] ));
+    	}
+    	
+    	// compute global peek values
+    	int globalMax = localMaxProduct[0];
+    	for ( int i = 1; i < nums.length; i++ )
+    	{
+    		globalMax = Math.max( globalMax, localMaxProduct[i] );
+    	}
+    	
+    	return globalMax;
     }
 }
