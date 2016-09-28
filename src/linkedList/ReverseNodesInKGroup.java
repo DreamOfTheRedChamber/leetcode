6b3703efd4ddedab4154908e06fdc7be9b9c4cc0
Shijie Zhang <szhang@opentext.com>
2016-09-28 14:02:35 -0700

    init impl

--- src/linkedList/ReverseNodesInKGroup.java
+++ src/linkedList/ReverseNodesInKGroup.java
@@ -1,5 +1,7 @@
 package linkedList;
 
+import utility.ListNode;
+
 /**
 Given a linked list, reverse the nodes of a linked list k at a time and return its modified list.
 
@@ -16,12 +18,48 @@ For k = 2, you should return: 2->1->4->3->5
 
 For k = 3, you should return: 3->2->1->4->5
  */
-//TO_IMME
 public class ReverseNodesInKGroup
 {
-
-	public ReverseNodesInKGroup( )
-	{
-	}
-
+    public ListNode reverseKGroup( ListNode head, int k )
+    {
+    	ListNode dummyHead = new ListNode( 0 );
+    	dummyHead.next = head;
+    	ListNode tailNode = dummyHead;
+    	
+    	ListNode currNode = head;
+    	while ( currNode != null )
+    	{
+    		// find the next (k+1)th node
+    		int nodeCounter = 1;
+    		ListNode kthNode = currNode;
+    		while ( nodeCounter < k
+    				&& kthNode != null )
+    		{
+    			nodeCounter++;
+    			kthNode = kthNode.next;
+    		}
+    			
+    		if ( kthNode != null )
+    		{
+    			ListNode reversedListTail = currNode.next;
+    			ListNode nextNode = kthNode.next;
+    			    			
+    			for ( int numReversed = 0; numReversed < k; numReversed++, currNode = currNode.next )
+    			{
+    				tailNode.next = currNode;
+    				tailNode = currNode;
+    				currNode = currNode.next;
+    			}    			        		    			
+        		
+    			// move to the (k+1) th nodes for the next round
+    			currNode = nextNode;
+    		}
+    		else
+    		{
+    			break;
+    		}    		
+    	}
+    	tailNode.next = null;
+    	return dummyHead.next;
+    }
 }
