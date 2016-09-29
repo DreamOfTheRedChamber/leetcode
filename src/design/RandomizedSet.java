61ba4166ec7f875e29283580224b4abd57673576
FreemanZhang <szhang@opentext.com>
2016-09-28 22:36:23 -0700

    fix bug
    swap function does not only needs to swap elements inside an array but
    also position inside hashmap

--- src/design/RandomizedSet.java
+++ src/design/RandomizedSet.java
@@ -47,8 +47,9 @@ public class RandomizedSet
 		if ( elemToPos.containsKey( val ) )
 		{
 			int position = elemToPos.get( val );
-			swap( elements, position, elements.size() - 1 );
+			swap( elements, elemToPos, position, elements.size() - 1 );
 			elements.remove( elements.size() - 1 );
+			elemToPos.remove( val );
 			return true;
 		}
 		else
@@ -63,9 +64,12 @@ public class RandomizedSet
 		return elements.get( rand.nextInt( elements.size() ) );
 	}
 	
-	private void swap( List<Integer> elements, int pos1, int pos2 )
+	private void swap( List<Integer> elements, Map<Integer, Integer> elemToPos, int pos1, int pos2 )
 	{
-		int buffer = elements.get( pos1 );
+		elemToPos.put( elements.get( pos1 ), pos2 );
+		elemToPos.put( elements.get( pos2 ), pos1 );
+		
+		int buffer = elements.get( pos1 );		
 		elements.set( pos1, elements.get( pos2 ) );
 		elements.set( pos2, buffer );
 	}
