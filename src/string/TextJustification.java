f473078447ac74ceeb139470a57e61b89550a696
FreemanZhang <szhang@opentext.com>
2016-09-29 20:27:43 -0700

    init impl

--- src/string/TextJustification.java
+++ src/string/TextJustification.java
@@ -1,5 +1,8 @@
 package string;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
 Given an array of words and a length L, format the text such that each line has exactly L characters and is fully (left and right) justified.
 
@@ -26,6 +29,80 @@ public class TextJustification
 {
     public List<String> fullJustify( String[] words, int maxWidth )
     {
-        
+       List<String> justifiedLines = new ArrayList<>();
+       int wordPos = 0;
+       while ( wordPos < words.length )
+       {
+    	   // calculate num of words in this line
+    	   int startPos = wordPos;
+    	   int endPos = wordPos;
+    	   int usedCharNum = 0;
+    	   while ( endPos < words.length 
+    			   && usedCharNum + words[endPos].length() <= maxWidth )
+    	   {
+    		   // if word is equal to remaining space
+    		   usedCharNum += words[endPos].length();
+    		   endPos++;
+    		   if ( usedCharNum == maxWidth )
+    		   {
+    			   break;
+    		   }
+    		   
+    		   // if word is smaller than remaining space
+    		   usedCharNum += 1;  
+    	   }
+    	   int numWords = endPos - startPos;
+
+    	   // handle edge case: single word a line
+    	   if ( numWords == 1 )
+    	   {
+    		   StringBuilder currLine = new StringBuilder();
+    		   currLine.append( words[startPos] );
+    		   int spaces = maxWidth - words[startPos].length();
+    		   for ( int i = 0; i < spaces; i++ )
+    		   {
+    			   currLine.append(' ');
+    		   }
+    		   wordPos = endPos;
+    		   continue;
+    	   }
+    	   
+    	   // calculate how to disperse space
+    	   int numSpaces = maxWidth;
+    	   for ( int currPos = startPos; currPos < endPos; currPos++ )
+    	   {
+    		   numSpaces -= words[currPos].length();
+    	   }    	   
+    	   int averageSpaces = numSpaces / ( numWords - 1 );
+    	   int additionSpaces = numSpaces % ( numWords - 1 );
+    	       	   
+    	   StringBuilder currLine = new StringBuilder();
+    	   for ( int currPos = startPos; currPos < endPos; currPos++ )
+    	   {
+    		   // append word
+    		   currLine.append( words[currPos] );
+    		   
+    		   if ( currPos != endPos - 1 )
+    		   {
+	    		   // append space
+	    		   for ( int i = 0; i < averageSpaces; i++ )
+	    		   {
+	    			   currLine.append(' ');
+	    		   }
+	    		   
+	    		   // append additional space
+	    		   if ( additionSpaces > 0 )
+	    		   {
+	    			   currLine.append(' ');
+	    			   additionSpaces--;
+	    		   }
+    		   }    		   
+    	   }
+    	   
+    	   // build this line
+    	   justifiedLines.add( currLine.toString() );
+       }
+       
+       return justifiedLines;
     }
 }
