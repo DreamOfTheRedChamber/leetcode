47ae6905c6032834370b51ca82fb9c4739707e65
FreemanZhang <shijiezhang666@gmail.com>
2016-10-08 15:36:39 -0700

    init impl

--- src/recursionTree/MaximumDepthofBinaryTree.java
+++ src/recursionTree/MaximumDepthofBinaryTree.java
@@ -1,15 +1,32 @@
 package recursionTree;
 
+import utility.TreeNode;
+
 /**
- * 
+Given a binary tree, find its maximum depth.
+
+The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
  */
 
 public class MaximumDepthofBinaryTree
 {
-
-    public int maxDepth(TreeNode root) 
+    public int maxDepth( TreeNode root )
     {
-        
+    	int[] max = new int[1];
+    	maxDepth( root, 1, max );
+    	return max[0];
     }
-
+    
+    private void maxDepth( TreeNode root, int depth, int[] max )
+    {
+    	if ( root == null )
+    	{
+    		return;
+    	}
+    	
+    	max[0] = Math.max( max[0], depth );
+    	
+    	maxDepth( root.left, depth + 1, max );
+    	maxDepth( root.right, depth + 1, max );
+    }
 }
