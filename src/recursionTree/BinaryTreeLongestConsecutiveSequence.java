4a5de8036e8f080aa800fca500a120a883d92897
FreemanZhang <shijiezhang666@gmail.com>
2016-10-08 14:26:56 -0700

    init impl

--- src/recursionTree/BinaryTreeLongestConsecutiveSequence.java
+++ src/recursionTree/BinaryTreeLongestConsecutiveSequence.java
@@ -1,5 +1,12 @@
 package recursionTree;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import utility.TreeNode;
+
 /**
 Given a binary tree, find the length of the longest consecutive sequence path.
 
@@ -21,13 +28,85 @@ Longest consecutive sequence path is 3-4-5, so return 3.
    2    
   / 
  1
-Longest consecutive sequence path is 2-3,not3-2-1, so return 2.
+Longest consecutive sequence path is 2-3, not3-2-1, so return 2.
  */
-//TO_IMME
 public class BinaryTreeLongestConsecutiveSequence
 {
-    public int longestConsecutive(TreeNode root) 
+	
+    public int longestConsecutive( TreeNode root )
     {
-        
+    	ResultWrapper result = longestRecurse( root );
+    	return result.globalLongest;
+    }
+    
+    private ResultWrapper longestRecurse( TreeNode currNode )
+    {
+    	if ( currNode.left == null
+    			&& currNode.right == null )
+    	{
+    		return new ResultWrapper( 1, 1 );
+    	}
+    	
+    	int localLongest = 1;
+    	int globalLongest = 1;
+    	if ( currNode.left != null )
+    	{
+    		ResultWrapper leftResult = longestRecurse( currNode.left );
+    		if ( currNode.val + 1 == currNode.left.val )
+    		{
+    			localLongest = Math.max( leftResult.localLongest + 1, localLongest );
+    		}
+			globalLongest = Math.max( globalLongest, Math.max( localLongest, leftResult.globalLongest ) );
+    	}
+    	if ( currNode.right != null )
+    	{
+    		ResultWrapper rightResult = longestRecurse( currNode.right );
+    		if ( currNode.val + 1 == currNode.right.val )
+    		{
+    			localLongest = Math.max( rightResult.localLongest + 1, localLongest );
+    		}
+			globalLongest = Math.max( globalLongest, Math.max( localLongest, rightResult.globalLongest ) );
+    	}
+    	
+    	return new ResultWrapper( localLongest, globalLongest );
+    }
+    
+    private class ResultWrapper
+    {
+    	public final int globalLongest;
+    	public final int localLongest;
+    	public ResultWrapper( int localLongest, int globalLongest )
+    	{
+    		this.globalLongest = globalLongest;
+    		this.localLongest = localLongest;
+    	}
+    }
+    
+    @Test
+    public void test1()
+    {
+    	TreeNode node1 = new TreeNode( 1 );
+    	TreeNode node2 = new TreeNode( 2 );
+    	TreeNode node3 = new TreeNode( 3 );
+    	TreeNode node4 = new TreeNode( 4 );
+    	TreeNode node5 = new TreeNode( 5 );
+    	node1.left = node2;
+    	node1.right = node3;
+    	node3.right = node4;
+    	node4.right = node5;
+    	assertEquals( 3, longestConsecutive( node1 ) );
+    }
+    
+    @Test
+    public void test2()
+    {
+    	TreeNode node1 = new TreeNode( 2 );
+    	TreeNode node2 = new TreeNode( 3 );
+    	TreeNode node3 = new TreeNode( 2 );
+    	TreeNode node4 = new TreeNode( 1 ); 
+    	node1.right = node2;
+    	node2.left = node3;
+    	node3.left = node4;
+    	assertEquals( 2, longestConsecutive( node1 ) );
     }
 }
--- src/recursionTree/HouseRobberIII.java
+++ src/recursionTree/HouseRobberIII.java
@@ -81,28 +81,22 @@ public class HouseRobberIII
     	
     	return Math.max( sumIncludeRoot, sumExcludeRoot );
     }
-    
-    public static void main( String[] args )
-    {
-    	System.out.println("hello world");
-    }
-    
+
     @Test
     public void test()    
     {
-//    	TreeNode node1 = new TreeNode( 3 );
-//    	TreeNode node2 = new TreeNode( 4 );
-//    	TreeNode node3 = new TreeNode( 5 );
-//    	TreeNode node4 = new TreeNode( 1 );
-//    	TreeNode node5 = new TreeNode( 3 );
-//    	TreeNode node6 = new TreeNode( 3 );
-//    	node1.left = node2;
-//    	node1.right = node3;
-//    	node2.left = node4;
-//    	node2.right = node5;
-//    	node3.right = node6;
-//    	
-//    	assertEquals( 9, rob( node1 ) );
-    	System.out.println("Hello JUnit");
+    	TreeNode node1 = new TreeNode( 3 );
+    	TreeNode node2 = new TreeNode( 4 );
+    	TreeNode node3 = new TreeNode( 5 );
+    	TreeNode node4 = new TreeNode( 1 );
+    	TreeNode node5 = new TreeNode( 3 );
+    	TreeNode node6 = new TreeNode( 1 );
+    	node1.left = node2;
+    	node1.right = node3;
+    	node2.left = node4;
+    	node2.right = node5;
+    	node3.right = node6;
+    	
+    	assertEquals( 9, rob( node1 ) );
     }
 }
