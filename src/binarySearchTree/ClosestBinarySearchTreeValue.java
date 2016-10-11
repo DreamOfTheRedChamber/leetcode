e05b30ebe43738c68f962e7c4a7f7bbb0e23d35a
FreemanZhang <shijiezhang666@gmail.com>
2016-10-10 22:23:42 -0700

    init impl

--- src/binarySearchTree/ClosestBinarySearchTreeValue.java
+++ src/binarySearchTree/ClosestBinarySearchTreeValue.java
@@ -1,5 +1,7 @@
 package binarySearchTree;
 
+import utility.TreeNode;
+
 /*
 Given a non-empty binary search tree and a target value, find the value in the BST that is closest to the target.
 
@@ -12,6 +14,46 @@ public class ClosestBinarySearchTreeValue
 {
     public int closestValue( TreeNode root, double target )
     {
-        
+    	if ( root == null )
+    	{
+    		throw new IllegalArgumentException();
+    	}
+    	if ( root.left == null && root.right == null )
+    	{
+    		return root.val;
+    	}
+    	
+    	
+    	if ( root.val < target && root.right != null )
+    	{
+    		int childClosest = closestValue( root.right, target );
+    		double rootDistance = Math.abs( root.val - target );
+    		double childDistance = Math.abs( childClosest - target );
+    		if ( rootDistance < childDistance )
+    		{
+    			return root.val;
+    		}
+    		else
+    		{
+    			return childClosest;
+    		}
+    	}
+    	if ( root.val > target && root.left != null )
+    	{
+    		int childClosest = closestValue( root.left, target );
+    		double rootDistance = Math.abs( root.val - target );
+    		double childDistance = Math.abs( childClosest - target );
+    		if ( rootDistance < childDistance )
+    		{
+    			return root.val;
+    		}
+    		else
+    		{
+    			return childClosest;
+    		}
+    		
+    	}
+    	
+    	return root.val;
     }
 }
