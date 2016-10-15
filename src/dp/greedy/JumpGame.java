ec28e8a1cd3e189910771688f9b81b8b94f46308
FreemanZhang <shijiezhang666@gmail.com>
2016-10-15 14:45:12 -0700

    init impl
    Jump game II has TLE

---
+++ src/dp/greedy/JumpGame.java
@@ -0,0 +1,48 @@
+package dp.greedy;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/*
+Given an array of non-negative integers, you are initially positioned at the first index of the array.
+
+Each element in the array represents your maximum jump length at that position.
+
+Determine if you are able to reach the last index.
+
+For example:
+A = [2,3,1,1,4], return true.
+
+A = [3,2,1,0,4], return false
+ * */
+
+public class JumpGame 
+{
+    public boolean canJump( int[] nums )
+    {
+    	if ( nums.length == 0 )
+    	{
+    		return true;
+    	}
+    	
+    	int maxPos = 0;
+    	for ( int i = 0; i < nums.length; i++ )
+    	{
+    		if ( maxPos < i )
+    		{
+    			return false;
+    		}
+    		
+    		maxPos = Math.max( maxPos, i + nums[i] );
+    	}
+    	
+    	return maxPos >= nums.length - 1;
+    }
+    
+    @Test
+    public void test()
+    {
+    	assertEquals( false, canJump( new int[]{ 0, 1 } ) );
+    }
+}
--- src/dp/greedy/JumpGameII.java
+++ src/dp/greedy/JumpGameII.java
@@ -1,4 +1,11 @@
 package dp.greedy;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
 /**
 Given an array of non-negative integers, you are initially positioned at the first index of the array.
 
@@ -11,9 +18,32 @@ Given array A = [2,3,1,1,4]
 
 The minimum number of jumps to reach the last index is 2. (Jump 1 step from index 0 to 1, then 3 steps to the last index.)
  */
-// TO_HURRY
-public class JumpGameII {
-    public int jump(int[] nums) {
+public class JumpGameII 
+{
+    public int jump( int[] nums )
+    {
+    	if ( nums.length == 0 )
+    	{
+    		return 0;
+    	}
+    	
+        int[] minSteps = new int[nums.length];
+        Arrays.fill( minSteps, Integer.MAX_VALUE );
+        minSteps[0] = 0;
+        for ( int i = 0; i < nums.length; i++ )
+        {
+        	for ( int j = 1; j <= nums[i] && i + j < nums.length ; j++ )
+        	{
+        		minSteps[i+j] = Math.min( minSteps[i+j], minSteps[i] + 1 );
+        	}
+        }
         
+        return minSteps[nums.length-1];
+    }
+    
+    @Test
+    public void test()
+    {
+    	assertEquals( 2, jump( new int[]{2, 3, 1, 1, 4} ) );
     }
 }
