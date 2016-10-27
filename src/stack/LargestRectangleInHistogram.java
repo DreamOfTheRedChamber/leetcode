8765542e1e78a32a580cee9f99d41f5a57892150
freemanzhang <szhang@opentext.com>
2016-10-27 08:54:43 -0700

    refactor code and fix bugs
    1. Maintain an increasing heights array index in stack
    2. When taking a bar out of histogram, find the maximum distance it
    could expand forward/backward
    3. Push a -1 into array

--- src/stack/LargestRectangleInHistogram.java
+++ src/stack/LargestRectangleInHistogram.java
@@ -28,22 +28,27 @@ public class LargestRectangleInHistogram
     	
     	int maxRecArea = 0;
     	Stack<Integer> incSeq = new Stack<>();
+    	incSeq.push( -1 );
     	for ( int i = 0; i < heights.length; i++ )
     	{
-    		maxRecArea = Math.max( maxRecArea, updateMaxRecEndingBeforeI( incSeq, i, heights[i], heights ) );
+    		maxRecArea = Math.max( maxRecArea, updateMaxRec( incSeq, i, heights[i], heights ) );
 			incSeq.push( i );
     	}
-    	maxRecArea = Math.max( maxRecArea, updateMaxRecEndingBeforeI( incSeq, heights.length, 0, heights ) );
+    	maxRecArea = Math.max( maxRecArea, updateMaxRec( incSeq, heights.length, 0, heights ) );
     	return maxRecArea;
     }
     
-    private int updateMaxRecEndingBeforeI( Stack<Integer> incSeq, int nextIndex, int nextValue, int[] heights )
+    private int updateMaxRec( Stack<Integer> incSeq, int nextIndex, int nextValue, int[] heights )
     {
     	int maxRecArea = 0;
-	    while ( !incSeq.isEmpty() && heights[incSeq.peek()] >= nextValue )
+	    while ( incSeq.size() > 1 && heights[incSeq.peek()] >= nextValue )
 	    {
 	    	int currIndex = incSeq.pop();
-	    	maxRecArea = Math.max( maxRecArea, heights[currIndex] * ( incSeq.isEmpty() ? currIndex + 1 : nextIndex - currIndex ) );
+	    	// using heights[currIndex] as height, what is the maximum limit it could expand forwards and backwards
+	    	// next - currIndex is the maximum it could expand forwards
+	    	// currIndex = incSeq.seek() is the maximum it could expand backwards
+	    	// nextIndex - currIndex + currIndex - incSeq.peek() - 1 = nextIndex - incSeq.peek() - 1 is the maximum width it could have
+	    	maxRecArea = Math.max( maxRecArea, heights[currIndex] * ( nextIndex - incSeq.peek() - 1 ) ); 
 	    }
     	return maxRecArea;
     }
