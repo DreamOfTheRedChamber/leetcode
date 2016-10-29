3408692a4148af5783a38d8beb043b73b59f16ea
freemanzhang <szhang@opentext.com>
2016-10-29 15:11:57 -0700

    reimpl

--- src/facebook/medium/AddAndSearchWordDSDesign.java
+++ src/facebook/medium/AddAndSearchWordDSDesign.java
@@ -20,15 +20,91 @@ Note:
 You may assume that all words are consist of lowercase letters a-z.
  * */
 
-public class AddAndSearchWordDSDesign {
+public class AddAndSearchWordDSDesign 
+{
+	private Trie trie = new Trie();
+	
     // Adds a word into the data structure.
-    public void addWord(String word) {
-        
+    public void addWord( String word )
+    {
+        trie.addWord( word );
     }
 
     // Returns if the word is in the data structure. A word could
     // contain the dot character '.' to represent any one letter.
-    public boolean search(String word) {
-        
+    public boolean search( String word )
+    {
+        return trie.search( word );
+    }    
+    
+    private class Trie
+    {
+    	public TrieNode root = new TrieNode( '0' );    		
+    	
+    	public void addWord( String word )
+    	{
+    		if ( word == null )
+    		{
+    			return;
+    		}
+    		TrieNode curr = root;
+    		for ( char ch : word.toCharArray() )
+    		{
+    			if ( curr.children[ch - 'a'] != null )
+    			{
+    				curr = curr.children[ch - 'a'];
+    			}
+    			else
+    			{
+    				TrieNode newNode = new TrieNode( ch );
+    				curr.children[ch-'a'] = newNode;
+    				curr = newNode;
+    			}
+    		}
+    	}
+    	
+    	public boolean search( String word )
+    	{
+    		if ( word == null )
+    		{
+    			return false;
+    		}
+    		return search( word, root, 0 );
+    	}
+    	
+    	private boolean search( String word, TrieNode root, int index )
+    	{
+    		if ( index >= word.length() )
+    		{
+    			return true;
+    		}
+    		
+    		if ( word.charAt( index ) == '.' )
+    		{
+    			for ( int i = 0; i < root.children.length; i++ )
+    			{
+    				if ( root.children[i] != null 
+    						&& search( word, root.children[i], index + 1 ) )
+    				{
+    					return true;
+    				}
+    			}
+    			return false;
+    		}
+    		else
+    		{
+    			return root.children[word.charAt( index ) - 'a'] == null ? false : true;
+    		}
+    	}
+    }
+    
+    private class TrieNode
+    {
+    	public char value;
+    	public TrieNode[] children;    	
+    	public TrieNode( char value )
+    	{
+    		this.value = value;
+    	}
     }
 }
