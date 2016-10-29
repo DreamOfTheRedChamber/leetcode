2ac5b66c58299e0fb18cab8a97530e725fa70e20
freemanzhang <szhang@opentext.com>
2016-10-29 13:53:41 -0700

    re-impl

--- src/facebook/hard/LongestConsecutiveSequence.java
+++ src/facebook/hard/LongestConsecutiveSequence.java
@@ -1,5 +1,10 @@
 package facebook.hard;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 /**
 Given an unsorted array of integers, find the length of the longest consecutive elements sequence.
 
@@ -9,12 +14,45 @@ The longest consecutive elements sequence is [1, 2, 3, 4]. Return its length: 4.
 
 Your algorithm should run in O(n) complexity.
  */
-// TO_START
 public class LongestConsecutiveSequence
 {
-    public int longestConsecutive(int[] nums) 
+    public int longestConsecutive( int[] nums )
     {
-        
-    }
-
+    	if ( nums == null || nums.length == 0 )
+    	{
+    		return 0;
+    	}    	
+    	
+    	Set<Integer> existingNums = new HashSet<>();
+    	for ( int num : nums )
+    	{
+    		existingNums.add( num );
+    	}
+    	
+    	int longestConsecSeq = 1;
+    	for ( int num : nums )
+    	{
+    		if ( existingNums.contains( num ) )
+    		{
+    			existingNums.remove( num );
+	    		int currConsecSeq = 1;    	
+	    		
+	    		int nextBigger = num + 1;
+	    		while ( existingNums.contains( nextBigger ) )
+	    		{
+	    			currConsecSeq++;
+	    			existingNums.remove( nextBigger );
+	    		}
+	    		
+	    		int nextSmaller = num - 1;
+	    		while ( existingNums.contains( nextSmaller ) )
+	    		{
+	    			currConsecSeq++;
+	    			existingNums.remove( nextSmaller );
+	    		}
+	    		longestConsecSeq = Math.max( longestConsecSeq, currConsecSeq );
+    		}
+    	}
+    	return longestConsecSeq;
+     }
 }
