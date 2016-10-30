bd4e559ff83c8bd742d54d1be11115c4e1b0a1e1
freemanzhang <szhang@opentext.com>
2016-10-30 13:33:22 -0700

    re-impl

--- src/facebook/hard/SerializeAndDeserializeBinaryTree.java
+++ src/facebook/hard/SerializeAndDeserializeBinaryTree.java
@@ -1,5 +1,12 @@
 package facebook.hard;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
+import org.junit.Test;
+
+import utility.TreeNode;
+
 /*
 Serialization is the process of converting a data structure or object into a sequence of bits so that it can be stored in a file or memory buffer, or transmitted across a network connection link to be reconstructed later in the same or another computer environment.
 
@@ -21,14 +28,76 @@ public class SerializeAndDeserializeBinaryTree
 
     // Encodes a tree to a single string.
     public String serialize( TreeNode root )
-    {
-        
+    {    	
+    	StringBuilder result = new StringBuilder();
+    	if ( root == null )
+    	{
+    		return result.toString();
+    	}
+    	Queue<TreeNode> bfsQueue = new LinkedList<>();
+    	bfsQueue.offer( root );
+    	result.append( root.val );
+    	result.append( "," );
+    	while ( !bfsQueue.isEmpty() )
+    	{
+    		TreeNode head = bfsQueue.poll();
+    		// left
+    		result.append( head.left == null ? '#' : head.left.val );
+    		result.append( "," );
+    		if ( head.left != null )
+    		{
+    			bfsQueue.offer( head.left );
+    		}
+    		// right
+    		result.append( head.right == null ? '#' : head.right.val );
+    		result.append( "," );
+    		if ( head.right != null )
+    		{
+    			bfsQueue.offer( head.right );
+    		}    		
+    	}
+    	return result.toString();
     }
 
     // Decodes your encoded data to tree.
     public TreeNode deserialize( String data )
     {
-        
+    	// basic error handling
+    	if ( data == null || data.length() == 0 )
+    	{
+    		return null;
+    	}
+    	String[] tokens = data.split( "," );
+    	TreeNode root = new TreeNode( Integer.parseInt( tokens[0] ) );
+    	Queue<TreeNode> bfsQueue = new LinkedList<>();
+    	bfsQueue.offer( root );
+    	int index = 0;
+    	while ( !bfsQueue.isEmpty() )
+    	{
+    		TreeNode head = bfsQueue.poll();
+    		if ( !tokens[index + 1].equals( "#" ) )
+    		{
+    			head.left = new TreeNode( Integer.parseInt( tokens[index + 1] ) );
+    			bfsQueue.offer( head.left );
+    		}
+    		if ( !tokens[index + 2].equals( "#" ) )
+    		{
+    			head.right = new TreeNode( Integer.parseInt( tokens[index + 2] ) );
+    			bfsQueue.offer( head.right );
+    		}
+    		index += 3;
+    	}
+    	return root;
     }
-
+    
+    @Test
+    public void test()
+    {
+    	TreeNode node1 = new TreeNode( 1 );
+    	TreeNode node2 = new TreeNode( 2 );
+    	node1.left = node2;
+    	
+    	TreeNode node3 = deserialize( serialize( node1 ) );
+    	System.out.println( node3.val );
+    }
 }
