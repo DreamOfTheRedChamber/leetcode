e6b9efd6ea48f660d8af0bbb3bafe9020803ffec
freemanzhang <szhang@opentext.com>
2016-11-03 14:10:09 -0700

    init impl

--- src/facebook/medium/BestTimeToBuyAndSellStockIII.java
+++ src/facebook/medium/BestTimeToBuyAndSellStockIII.java
@@ -5,11 +5,41 @@ Say you have an array for which the ith element is the price of a given stock on
 
 Design an algorithm to find the maximum profit. You may complete at most two transactions.
  */
-//TO_START
 public class BestTimeToBuyAndSellStockIII
 {
-    public int maxProfit(int[] prices) 
+    public int maxProfit( int[] prices )
     {
-        
+    	if ( prices == null || prices.length <= 1 )
+    	{
+    		return 0;
+    	}
+    	
+    	int[] left = new int[prices.length];
+    	int[] right = new int[prices.length];
+    	
+    	// DP from left to right
+    	left[0] = 0;
+    	int min = prices[0];
+    	for ( int i = 1; i < prices.length; i++ )
+    	{
+    		min = Math.min( prices[i], min );
+    		left[i] = Math.max( left[i - 1], prices[i] - min );
+    	}
+    	
+    	// DP from right to left
+    	right[prices.length - 1] = 0;
+    	int max = prices[prices.length - 1];
+    	for ( int i = prices.length - 2; i >= 0; i-- )
+    	{
+    		max = Math.max( prices[i], max );
+    		right[i] = Math.max( right[i + 1], max - prices[i] );
+    	}
+    	
+    	int profit = 0;
+    	for ( int i = 0; i < prices.length; i++ )
+    	{
+    		profit = Math.max( left[i] + right[i], profit );
+    	}
+    	return profit;
     }
 }
