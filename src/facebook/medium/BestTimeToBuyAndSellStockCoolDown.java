e2d7892b1a176101a3c0cbd348203ffa6cef83bb
freemanzhang <szhang@opentext.com>
2016-11-03 14:29:47 -0700

    init impl

--- src/facebook/medium/BestTimeToBuyAndSellStockCoolDown.java
+++ src/facebook/medium/BestTimeToBuyAndSellStockCoolDown.java
@@ -13,8 +13,36 @@ prices = [1, 2, 3, 0, 2]
 maxProfit = 3
 transactions = [buy, sell, cooldown, buy, sell]
  */
-//TO_HURRY
+
 public class BestTimeToBuyAndSellStockCoolDown
 {
-
+    public int maxProfit( int[] prices )
+    {
+        if ( prices == null || prices.length == 0 ) 
+        {
+            return 0;
+        }
+        
+        // 表示当天最终未持股的情况下，当天结束后的累计最大利润
+        int[] sellDp = new int[prices.length];
+        // 表示当天最终持股的情况下，当天结束后的累计最大利润
+        int[] buyDp = new int[prices.length];
+        
+        // 考虑初始情况
+        buyDp[0] = -prices[0];
+        sellDp[0] = 0;
+        for (int i = 1; i < prices.length; i++) 
+        {
+            sellDp[i] = Math.max( sellDp[i - 1], buyDp[i - 1] + prices[i] );
+            if (i >= 2) 
+            {
+                buyDp[i] = Math.max( buyDp[i - 1], sellDp[i - 2] - prices[i] );
+            }
+            else 
+            {
+                buyDp[i] = Math.max( buyDp[i - 1], -prices[i] );
+            }
+        }
+        return sellDp[prices.length - 1];
+    }
 }
