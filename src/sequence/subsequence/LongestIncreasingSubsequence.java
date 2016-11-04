1ab36f4ab33d95dc2a693b93de6dd14fc1a710d3
freemanzhang <szhang@opentext.com>
2016-11-04 09:26:11 -0700

    add a nlogn solution based on binary search

--- src/sequence/subsequence/LongestIncreasingSubsequence.java
+++ src/sequence/subsequence/LongestIncreasingSubsequence.java
@@ -48,6 +48,56 @@ public class LongestIncreasingSubsequence
     	return longestLength;
     }
     
+    public int lengthOfLISBinarySearch( int[] nums )
+    {
+    	int[] minLast = new int[nums.length + 1];
+    	minLast[0] = Integer.MIN_VALUE;
+    	for ( int i = 1; i <= nums.length; i++ )
+    	{
+    		minLast[i] = Integer.MAX_VALUE;
+    	}
+    	
+    	for ( int i = 0; i < nums.length; i++ )
+    	{
+    		// find the first number in minLast > nums[i]
+    		int index = binarySearch( minLast, nums[i] );
+    		minLast[index] = nums[i];
+    	}
+    	
+    	for ( int i = nums.length; i >= 1; i-- )
+    	{
+    		if ( minLast[i] != Integer.MAX_VALUE )
+    		{
+    			return i;
+    		}
+    	}
+    	
+    	return 0;
+    }
+    
+    private int binarySearch( int[] minLast, int num )
+    {
+    	int start = 0, end = minLast.length - 1;
+    	while ( start + 1 < end )
+    	{
+    		int mid = ( end - start ) / 2 + start;
+    		if ( minLast[mid] < num )
+    		{
+    			start = mid;
+    		}
+    		else
+    		{
+    			end = mid;
+    		}
+    	}
+    	
+    	if ( minLast[start] > num )
+    	{
+    		return start;
+    	}
+    	return end;
+    }
+    
     @Test
     public void test()
     {
