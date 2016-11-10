fb1e377cbd5cf381ff7b9c04b52af59b5025914c
freemanzhang <szhang@opentext.com>
2016-11-10 14:34:57 -0800

    init impl

--- src/facebook/medium/AddDifferentBaseNumber.java
+++
@@ -1,9 +0,0 @@
-package facebook.medium;
-
-/**
- * 给两个string string是一串数字 再给一个int base, assume base <= 10 就是给两串数字 数字是base进制的 然后加起来 返回sum
- */
-
-public class AddDifferentBaseNumber
-{
-}
--- src/facebook/medium/GenerateParentheses.java
+++ src/facebook/medium/GenerateParentheses.java
@@ -1,7 +1,9 @@
 package facebook.medium;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+
+import org.junit.Test;
 
 /*
 Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
@@ -18,9 +20,38 @@ For example, given n = 3, a solution set is:
  * */
 public class GenerateParentheses 
 {
+	@Test
+	public void test()
+	{
+		System.out.println( generateParenthesis( 3 ) );
+	}
+	
     public List<String> generateParenthesis( int n )
     {
-    	List<String> result = new ArrayList<>();
+    	List<String> result = new LinkedList<>();
+    	generateParenthesis( result, new StringBuilder(), n, n );
+    	return result;
+    }
+    
+    private void generateParenthesis( List<String> result, StringBuilder currStr, int left, int right )
+    {
+    	if ( left == 0 && right == 0 )
+    	{
+    		result.add( currStr.toString() );
+    		return;
+    	}
     	
+    	if ( left > 0 )
+    	{
+    		currStr.append( '(' );
+    		generateParenthesis( result, currStr, left - 1, right );
+    		currStr.deleteCharAt( currStr.length() - 1 );
+    	}
+    	if ( right > 0 && left < right )
+    	{
+	    	currStr.append( ')' );
+	    	generateParenthesis( result, currStr, left, right -1 );
+	    	currStr.deleteCharAt( currStr.length() - 1 );
+    	}
     }
 }
