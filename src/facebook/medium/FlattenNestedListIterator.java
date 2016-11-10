88eb48e7b4434ab6670c9eb3bbd782ec74f07781
freemanzhang <szhang@opentext.com>
2016-11-10 14:08:59 -0800

    init impl

--- src/facebook/medium/FlattenNestedListIterator.java
+++ src/facebook/medium/FlattenNestedListIterator.java
@@ -1,5 +1,13 @@
 package facebook.medium;
 
+import java.util.Iterator;
+import java.util.List;
+import java.util.Stack;
+
+import org.junit.Test;
+
+import utility.NestedInteger;
+
 /*
 Given a nested list of integers, implement an iterator to flatten it.
 
@@ -16,19 +24,52 @@ Given the list [1,[4,[6]]],
 By calling next repeatedly until hasNext returns false, the order of elements returned by next should be: [1,4,6].
  * */
 
-public class FlattenNestedListIterator implements Iterator<Integer> {
-
-    public NestedIterator(List<NestedInteger> nestedList) {
-        
+public class FlattenNestedListIterator implements Iterator<Integer> 
+{
+	private Stack<Iterator<NestedInteger>> parentsIterator;	
+	private NestedInteger peekedVal;
+	
+    public NestedIterator( List<NestedInteger> nestedList ) 
+    {
+    	if ( nestedList == null )
+    	{
+    		throw new IllegalArgumentException();
+    	}
+    	
+    	parentsIterator = new Stack<>();
+    	parentsIterator.push( nestedList.iterator() );
+    	peekedVal = null;
     }
 
     @Override
-    public Integer next() {
-        
+    public Integer next()
+    {
+    	NestedInteger lastPeekedVal = peekedVal;
+    	peekedVal = null;    	
+    	return lastPeekedVal.getInteger();
     }
 
     @Override
-    public boolean hasNext() {
-        
+    public boolean hasNext() 
+    {
+    	if ( !parentsIterator.isEmpty() )
+    	{
+	        Iterator<NestedInteger> currIterator = parentsIterator.pop();
+	    	while ( currIterator.hasNext() )
+	    	{
+	    		peekedVal = currIterator.next();
+	    		if ( !peekedVal.isInteger() )
+	    		{
+	    			parentsIterator.push( currIterator );
+	    			currIterator = peekedVal.getList().iterator();
+	    		}
+	    		else
+	    		{
+	    			parentsIterator.push( currIterator );
+	    			return true;
+	    		}
+	    	}
+    	}
+    	return false;
     }
 }
