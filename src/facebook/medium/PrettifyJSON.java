1501f8ce6ac0446c07eb3167bf62102605bcba7c
freemanzhang <szhang@opentext.com>
2016-11-10 19:36:02 -0800

    init impl

--- src/facebook/medium/PrettifyJSON.java
+++ src/facebook/medium/PrettifyJSON.java
@@ -1,5 +1,7 @@
 package facebook.medium;
 
+import org.junit.Test;
+
 /**
  * 输入[1,2,3, {"id": 1, "name": "wang", "tag":[1,"home",2], "price":234}]
  * Pretty print a json object using proper indentation.
@@ -25,7 +27,7 @@ Output :
 }
 Example 2:
 
-Input : ["foo", {"bar":["baz",null,1.0,2]}]
+Input : ["foo",{"bar":["baz",null,1.0,2]}]
 Output : 
 [
     "foo", 
@@ -48,4 +50,64 @@ Your solution should return a list of strings, where each entry corresponds to a
 
 public class PrettifyJSON
 {
+	@Test
+	public void test()
+	{
+		System.out.println( printJSON( "[\"foo\",{\"bar\":[\"baz\",null,1.0,2]}]" ) );
+		System.out.println( printJSON( "{A:\"B\",C:{D:\"E\",F:{G:\"H\",I:\"J\"}}}" ) );
+	}
+	
+	public String printJSON( String input )
+	{
+		StringBuilder result = new StringBuilder();
+		int indentNum = 0;
+		int index = 0;
+		while ( index < input.length() )
+		{
+			switch ( input.charAt( index ) )
+			{
+				case '{':
+				case '[':
+					addIndent( result, indentNum );
+					result.append( input.charAt( index ) );
+					result.append( '\n' );
+					indentNum++;
+
+					index++;
+					break;
+				case '}':
+				case ']':
+					indentNum--;
+					addIndent( result, indentNum );
+					result.append( input.charAt( index ) );
+					result.append( '\n' );
+
+					index++;
+					break;
+				default:
+					addIndent( result, indentNum );
+					while ( input.charAt( index ) != ',' && input.charAt( index ) != ']' && input.charAt( index ) != '[' && input.charAt( index ) != '{' && input.charAt( index ) != '}')
+					{
+						result.append( input.charAt( index ) );
+						index++;
+					}
+					if ( input.charAt( index ) == ',' )
+					{
+						result.append( input.charAt( index ) );
+						index++;
+					}
+					result.append( '\n' );
+					break;
+			}			
+		}
+		return result.toString();
+	}
+	
+	private void addIndent( StringBuilder result, int indentNum )
+	{
+		for ( int i = 0; i < indentNum; i++ )
+		{
+			result.append( '\t' );
+		}
+	}
 }
