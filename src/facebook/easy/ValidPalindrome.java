1166ad70232a631409488427b97ffd2877c9155d
freemanzhang <szhang@opentext.com>
2016-11-11 22:20:53 -0800

    init impl

--- src/facebook/easy/ValidPalindrome.java
+++ src/facebook/easy/ValidPalindrome.java
@@ -1,5 +1,9 @@
 package facebook.easy;
 
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
 /**
 Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
 
@@ -13,9 +17,45 @@ Have you consider that the string might be empty? This is a good question to ask
 For the purpose of this problem, we define empty string as valid palindrome.
  */
 
-// TO_IMME
-public class ValidPalindrome {
-    public boolean isPalindrome(String s) {
-        
+public class ValidPalindrome 
+{
+	@Test
+	public void test()
+	{
+		assertTrue( isPalindrome( "aA" ) );
+		assertTrue( !isPalindrome( "0P" ) );
+	}
+	
+    public boolean isPalindrome( String s )
+    {
+    	int start = 0;
+    	int end = s.length() - 1;
+    	while ( start < end )
+    	{
+    		while ( start < end && !isValid( s.charAt( start ) ) )
+    		{
+    			start++;
+    		}
+    		while ( start < end && !isValid( s.charAt( end ) ) )
+    		{
+    			end--;
+    		}
+    		
+    		if ( start < end ) 
+    		{
+    			if ( Character.toLowerCase( s.charAt( start ) ) != Character.toLowerCase( s.charAt( end ) ) )
+    			{
+        			return false;    				    				
+    			}
+    			start++;
+    			end--;
+    		}    		
+    	}
+    	return true;
+    }
+    
+    private boolean isValid( char ch )
+    {
+    	return Character.isLetter( ch ) || Character.isDigit( ch );
     }
 }
