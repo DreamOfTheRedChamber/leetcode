7da798d97b43c24a289a8ca165f264df914e3c55
freemanzhang <szhang@opentext.com>
2016-11-11 19:28:21 -0800

    add recursive implementation

--- src/recursion/tree/FindLeavesOfBinaryTree.java
+++ src/recursion/tree/FindLeavesOfBinaryTree.java
@@ -8,66 +8,56 @@ import org.junit.Test;
 import utility.TreeNode;
 
 /**
- * 
+Given a binary tree, collect a tree's nodes as if you were doing this: Collect and remove all leaves, repeat until the tree is empty.
+
+Example:
+Given binary tree 
+          1
+         / \
+        2   3
+       / \     
+      4   5    
+Returns [4, 5, 3], [2], [1].
+
+Explanation:
+1. Removing the leaves [4, 5, 3] would result in this tree:
+
+          1
+         / 
+        2          
+2. Now removing the leaf [2] would result in this tree:
+
+          1          
+3. Now removing the leaf [1] would result in the empty tree:
+
+          []         
+Returns [4, 5, 3], [2], [1].
  */
 
 public class FindLeavesOfBinaryTree
 {
-    public List<List<Integer>> findLeaves(TreeNode root) 
+    public List<List<Integer>> findLeaves( TreeNode root )
     {
     	List<List<Integer>> result = new ArrayList<>();
-    	    	
-    	while ( root != null )
-    	{
-    		List<Integer> leaves = new ArrayList<>();
-    		if ( root.left == null 
-    				&& root.right == null )
-    		{
-    			// tree root
-    			leaves.add( root.val );
-    			root = null;
-    		}
-    		else
-    		{
-    			// recurse on non-leaf nodes
-    			collectLeaves( root, leaves );
-    		}
-    		result.add( leaves );
-    	}
-    	
+    	helper( result, root );    	
     	return result;
     }
     
-    private void collectLeaves( TreeNode root, List<Integer> leaves )
+    private int helper( List<List<Integer>> result, TreeNode root )
     {
-    	if ( root.left != null )
+    	if ( root == null )
     	{
-    		if ( root.left.left == null 
-    				&& root.left.right == null )
-    		{
-    			// leaf node
-    			leaves.add( root.left.val );
-    			root.left = null;
-    		}
-    		else
-    		{
-    			// non-leaf node
-    			collectLeaves( root.left, leaves );
-    		}
+    		return -1;
     	}
     	
-    	if ( root.right != null )
+    	int left = helper( result, root.left );
+    	int right = helper( result, root.right );
+    	int curr = Math.max( left, right ) + 1;
+    	if ( result.size() == curr )
     	{
-    		if ( root.right.left == null 
-    				&& root.right.right == null )
-    		{
-    			leaves.add( root.right.val );
-    			root.right = null;
-    		}
-    		else
-    		{
-    			collectLeaves( root.right, leaves );
-    		}
+    		result.add( new ArrayList<>() );
     	}
+    	result.get( curr ).add( root.val );
+    	return curr;
     }
 }
