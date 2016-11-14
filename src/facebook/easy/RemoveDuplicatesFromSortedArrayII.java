d897a93671a29db0908fcae532e654825bc905f1
freemanzhang <szhang@opentext.com>
2016-11-13 23:11:36 -0800

    init impl

--- src/facebook/easy/RemoveDuplicatesFromSortedArrayII.java
+++ src/facebook/easy/RemoveDuplicatesFromSortedArrayII.java
@@ -9,9 +9,38 @@ Given sorted array nums = [1,1,1,2,2,3],
 
 Your function should return length = 5, with the first five elements of nums being 1, 1, 2, 2 and 3. It doesn't matter what you leave beyond the new length.
  */
-// TO_HURRY
-public class RemoveDuplicatesFromSortedArrayII {
-    public int removeDuplicates(int[] nums) {
+public class RemoveDuplicatesFromSortedArrayII 
+{
+    public int removeDuplicates( int[] nums ) 
+    {
+        if ( nums.length <= 2 )
+        {
+        	return nums.length;
+        }
         
+        int count = 1;
+        int validPos = 0;
+        for ( int i = 1; i < nums.length; i++ )
+        {        	
+        	if ( nums[i] == nums[validPos] 
+        			&& count == 2 )
+        	{
+        		continue;
+        	}
+        	
+        	if ( nums[i] == nums[validPos] )
+        	{
+        		count++;
+        	}
+        	else
+        	{
+        		count = 1;
+        	}
+
+        	validPos++;
+        	nums[validPos] = nums[i];        		
+        }
+        
+        return validPos + 1;
     }
 }
