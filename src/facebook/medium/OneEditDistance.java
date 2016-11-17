f09b8e977222983af28e65346bdd26e0f05cfce7
freemanzhang <szhang@opentext.com>
2016-11-17 13:49:37 -0800

    add a clean oneEditDistance solution

--- src/facebook/medium/OneEditDistance.java
+++ src/facebook/medium/OneEditDistance.java
@@ -13,65 +13,30 @@ public class OneEditDistance
 	@Test
 	public void test()
 	{
-		assertTrue( isOneEditDistance( "ab", "abc" ) );
-		
+		assertTrue( isOneEditDistance( "ab", "abc" ) );		
 	}
 	
-    public boolean isOneEditDistance( String s, String t )
-    {
-    	if ( s == null 
-    			|| t == null )
-    	{
-    		throw new IllegalArgumentException("");
-    	}
-    	if ( s.length( ) < t.length( ) )
-    	{
-    		return isOneEditDistance( t, s );
-    	}
-    	
-    	if ( s.length( ) - t.length( ) > 1 )
-    	{
-    		return false;
-    	}
-    	else if ( s.length( ) == t.length( ) )
-    	{
-    		// is one replace
-    		int numDiffCh = 0;
-    		for ( int i = 0; i < s.length(); i++ )
-    		{
-    			if ( s.charAt( i ) != t.charAt( i ) )
-    			{
-    				numDiffCh++;
-    			}
-    			if ( numDiffCh > 1 )
-    			{
-    				return false;
-    			}
-    		}
-    		return true;
-    	} 
-    	else
-    	{
-    		// is one insert
-    		boolean hasAlreadyInserted = false;
-			for ( int m = 0, n = 0; m < s.length() && n < t.length();) 
-			{
-				if ( s.charAt( m ) != t.charAt( n ) )
+	public boolean isOneEditDistance( String s, String t ) 
+	{
+	    for ( int i = 0; i < Math.min( s.length(), t.length() ); i++ ) 
+	    { 
+	    	if ( s.charAt( i ) != t.charAt( i ) ) 
+	    	{
+	    		if ( s.length() == t.length() ) // s has the same length as t, so the only possibility is replacing one char in s and t
+	    		{
+	    			return s.substring( i + 1 ).equals( t.substring( i + 1 ) );
+	    		}
+				else if ( s.length() < t.length() ) // t is longer than s, so the only possibility is deleting one char from t
 				{
-					if ( hasAlreadyInserted )
-					{
-						return false;
-					}
-					m++;
-					hasAlreadyInserted = true;
+					return s.substring( i ).equals( t.substring( i + 1 ) );	        	
 				}
-				else
+				else // s is longer than t, so the only possibility is deleting one char from s
 				{
-					m++;
-					n++;
+					return t.substring( i ).equals( s.substring( i + 1 ) );
 				}
-			}    			
-    		return true;    		
-    	}    	    	
-    }
+	    	}
+	    }       
+	    //All previous chars are the same, the only possibility is deleting the end char in the longer one of s and t 
+	    return Math.abs( s.length() - t.length() ) == 1;        
+	}
 }
--- src/utility/TreeLinkNode.java
+++ src/utility/TreeLinkNode.java
@@ -2,10 +2,16 @@ package utility;
 
 public class TreeLinkNode 
 {
+	public int val;
 	public TreeLinkNode left;
 	public TreeLinkNode right;
 	public TreeLinkNode next;
 
+	public TreeLinkNode( int val )
+	{
+		this.val = val;
+	}
+	
 	public TreeLinkNode(TreeLinkNode left, TreeLinkNode right, TreeLinkNode next) {
 		super();
 		this.left = left;
