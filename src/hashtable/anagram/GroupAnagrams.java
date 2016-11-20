0ba9d3ef115a7b7c1e00caf8490a65e8c96a2fcd
freemanzhang <szhang@opentext.com>
2016-11-20 10:51:46 -0800

    provide implementation for only lowercase alpha or unicode case

--- src/hashtable/anagram/GroupAnagrams.java
+++ src/hashtable/anagram/GroupAnagrams.java
@@ -6,6 +6,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import org.junit.Test;
+
 /**
 Given an array of strings, group anagrams together.
 
@@ -22,23 +24,62 @@ Note: All inputs will be in lower-case.
 
 public class GroupAnagrams
 {
-	private final static int CHARSET_SIZE = 26;
     public List<List<String>> groupAnagrams( String[] strs )
     {
         Map<String, List<String>> groupedAnagrams = new HashMap<>();
         for ( String str : strs )
         {
-        	char[] freqMap = new char[CHARSET_SIZE];
-        	for ( char ch : str.toCharArray() )
-        	{
-        		freqMap[ch-'a']++;
-        	}
-        	String anagramKey = String.valueOf( freqMap );
+        	String anagramKey = getAnagramKey( str );
         	groupedAnagrams.putIfAbsent( anagramKey, new ArrayList<>() );
         	groupedAnagrams.get( anagramKey ).add( str );
         }
         return groupedAnagrams.values( ).stream( ).collect( Collectors.toList( ) );
     }
     
+    public String getAnagramKey( String s )
+    {
+    	int[] histogram = new int[26];
+    	for ( char ch : s.toCharArray() )
+    	{
+    		histogram[ch - 'a']++;
+    	}
+    	StringBuilder result = new StringBuilder();
+    	for ( int i = 0; i < histogram.length; i++ )
+    	{
+    		if ( histogram[i] > 0 )
+    		{
+    			result.append( ( char ) ( i + 'a' ) );
+    			result.append( histogram[i] );
+    		}
+    	}
+    	return result.toString();
+    }
+        
+    public String getAnagramKeyUnicode( String s )
+    {
+    	Map<Character, Integer> histogram = new HashMap<>();
+    	for ( char ch : s.toCharArray() )
+    	{
+    		histogram.put( ch, histogram.getOrDefault( ch, 0) + 1 );
+    	}
+    	StringBuilder result = new StringBuilder();
+    	for ( char i = Character.MIN_VALUE; i <= Character.MAX_VALUE; i++ )
+    	{
+    		if ( histogram.containsKey( i ) )
+    		{
+    			result.append( i );
+    			result.append( histogram.get( i ) );
+    		}
+    	}
+    	return result.toString();
+    }
     
+    @Test
+    public void test()
+    {
+    	Character a = (char) 4000;
+    	System.out.println( (int) a );
+    	System.out.println( (int) Character.MAX_VALUE );
+    	System.out.println( (int) Character.MIN_VALUE );    	
+    }
 }
