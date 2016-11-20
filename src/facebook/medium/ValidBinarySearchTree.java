5c56d31795b96e9490fdb620e79feb10b22e558c
freemanzhang <szhang@opentext.com>
2016-11-19 22:28:25 -0800

    add an inorder based O(h) solution

--- src/facebook/medium/ValidBinarySearchTree.java
+++ src/facebook/medium/ValidBinarySearchTree.java
@@ -1,9 +1,28 @@
 package facebook.medium;
 
+import java.util.Stack;
+
 import utility.TreeNode;
 
 /**
  * 
+Given a binary tree, determine if it is a valid binary search tree (BST).
+
+Assume a BST is defined as follows:
+
+The left subtree of a node contains only nodes with keys less than the node's key.
+The right subtree of a node contains only nodes with keys greater than the node's key.
+Both the left and right subtrees must also be binary search trees.
+Example 1:
+    2
+   / \
+  1   3
+Binary tree [2,1,3], return true.
+Example 2:
+    1
+   / \
+  2   3
+Binary tree [1,2,3], return false.
  */
 
 public class ValidBinarySearchTree
@@ -25,8 +44,8 @@ public class ValidBinarySearchTree
     	{
     		return true;
     	}
-    	else if ( root.val <= lowerBound
-    			|| root.val >= upperBound )
+    	else if ( root.val < lowerBound
+    			|| root.val > upperBound )
     	{
     		return false;
     	}
@@ -37,4 +56,38 @@ public class ValidBinarySearchTree
     	}    	    	
     }
 
+    public boolean isValidBSTTraversal( TreeNode root )
+    {
+    	if ( root == null )
+    	{
+    		return true;
+    	}
+    	Stack<TreeNode> stack = new Stack<>();
+    	pushLeftChild( root, stack );
+    	TreeNode prevNode = null;
+    	while ( !stack.isEmpty() )
+    	{
+    		TreeNode top = stack.pop();
+    		if ( prevNode != null && top.val <= prevNode.val )
+    		{
+    			return false;
+    		}
+    		prevNode = top;
+    		if ( top.right != null )
+    		{
+    			pushLeftChild( top.right, stack );
+    		}
+    	}
+    	return true;
+    }
+    
+    private void pushLeftChild( TreeNode root, Stack<TreeNode> stack )
+    {
+    	TreeNode currNode = root;
+    	while ( currNode != null )
+    	{
+    		stack.push( currNode );
+    		currNode = currNode.left;
+    	}
+    }
 }
