ba9872835356a56dee11a25a4dd9434fc7d7b49b
freemanzhang <szhang@opentext.com>
2016-11-20 17:05:13 -0800

    refactor code

--- src/facebook/medium/WordLadder.java
+++ src/facebook/medium/WordLadder.java
@@ -61,7 +61,22 @@ public class WordLadder
 	
     public int ladderLength( String beginWord, String endWord, Set<String> wordList )
     {    	
-    	wordList.add( endWord );
+    	if ( beginWord == null || endWord == null )
+    	{
+    		throw new IllegalArgumentException();
+    	}
+    	if ( beginWord.equals( endWord ) )
+    	{
+    		return 0;
+    	}
+    	if ( wordList.size() == 0 )
+    	{
+    		return -1;
+    	}
+    	
+    	Set<String> toBeVisited = new HashSet<>( wordList );
+    	toBeVisited.add( endWord );
+    	toBeVisited.remove( beginWord );
     	Queue<String> bfsQueue = new LinkedList<>();
     	bfsQueue.add( beginWord );
     	int level = 0;
@@ -75,24 +90,31 @@ public class WordLadder
     			{
     				return level + 1;
     			}
-    			for ( int j = 0; j < head.length(); j++ )
-    			{
-    				char[] word = head.toCharArray();
-    				for ( char ch = 'a'; ch <= 'z'; ch++ )
-    				{
-    					word[j] = ch;
-    					String check = new String( word );
-    					if ( !check.equals( head ) && wordList.contains( check ) )
-    					{
-    						bfsQueue.add( check );
-    						wordList.remove( check );
-    					}
-    				}
-    			}
+    			generateNextLevel( head, bfsQueue, toBeVisited );
     		}    		
 			level++;
     	}
     	return 0;
+    }
+    
+    private void generateNextLevel( String currWord, Queue<String> bfsQueue, Set<String> toBeVisited )
+    {
+		char[] word = currWord.toCharArray();
+    	for ( int i = 0; i < currWord.length(); i++ )
+    	{
+    		char chBeforeChange = word[i];
+    		for ( char ch = 'a'; ch <= 'z'; ch++ )
+    		{
+    			word[i] = ch;
+    			String nextStr = new String( word );
+    			if ( toBeVisited.contains( nextStr ) )
+    			{
+    				bfsQueue.add( nextStr );
+    				toBeVisited.remove( nextStr );
+    			}
+    		}
+    		word[i] = chBeforeChange;
+    	}
     }
     
 }
