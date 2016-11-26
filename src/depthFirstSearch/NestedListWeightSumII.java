2d324b22d19323eee384e554eee6116e8b78ae28
freemanzhang <szhang@opentext.com>
2016-11-26 12:27:42 -0800

    add a dfs implementation

--- src/depthFirstSearch/NestedListWeightSumII.java
+++ src/depthFirstSearch/NestedListWeightSumII.java
@@ -1,10 +1,25 @@
 package depthFirstSearch;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 import utility.NestedInteger;
 
 /**
+Given a nested list of integers, return the sum of all integers in the list weighted by their depth.
+
+Each element is either an integer, or a list -- whose elements may also be integers or other lists.
+
+Different from the previous question where weight is increasing from root to leaf, now the weight is defined from bottom up. 
+i.e., the leaf level integers have weight 1, and the root level integers have the largest weight.
+
+Example 1:
+Given the list [[1,1],2,[1,1]], return 8. (four 1's at depth 1, one 2 at depth 2)
+
+Example 2:
+Given the list [1,[4,[6]]], return 17. (one 1 at depth 3, one 4 at depth 2, and one 6 at depth 1; 1*3 + 4*2 + 6*1 = 17)
+ * 
  * // This is the interface that allows for creating nested lists.
  * // You should not implement it, or speculate about its implementation
  * public interface NestedInteger {
@@ -35,6 +50,33 @@ import utility.NestedInteger;
 
 public class NestedListWeightSumII
 {
+	public int depthSumInverseDFS( List<NestedInteger> nestedList )
+	{
+		return depthSumInverseDFS( nestedList, 0 );
+	}
+	
+	public int depthSumInverseDFS( List<NestedInteger> nestedList, int prevLevelSum )
+	{
+		if ( nestedList.size() == 0 )
+		{
+			return 0;
+		}
+		int currLevelSum = 0;
+		List<NestedInteger> nextLevelList = new LinkedList<>();
+		for ( NestedInteger nestedInteger : nestedList )
+		{
+			if ( nestedInteger.isInteger() )
+			{
+				currLevelSum += nestedInteger.getInteger();
+			}
+			else
+			{
+				nextLevelList.addAll( nestedInteger.getList() );
+			}
+		}
+		return prevLevelSum + currLevelSum + depthSumInverseDFS( nextLevelList, prevLevelSum + currLevelSum );
+	}	
+	
     public int depthSumInverse( List<NestedInteger> nestedList )
     {
         return calcWeightSum( nestedList, calcMaxNestDepth( nestedList, 1 ) );
