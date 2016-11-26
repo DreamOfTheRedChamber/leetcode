1a5f134dd8c6685416bc40480b78745414cc509d
freemanzhang <szhang@opentext.com>
2016-11-26 15:04:49 -0800

    init impl

--- src/hashtable/IsomorphicStrings.java
+++ src/hashtable/IsomorphicStrings.java
@@ -1,13 +1,59 @@
 package hashtable;
 
+import static org.junit.Assert.assertTrue;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
 /**
- * 
+Given two strings s and t, determine if they are isomorphic.
+
+Two strings are isomorphic if the characters in s can be replaced to get t.
+
+All occurrences of a character must be replaced with another character while preserving the order of characters. 
+No two characters may map to the same character but a character may map to itself.
+
+For example,
+Given "egg", "add", return true.
+
+Given "foo", "bar", return false.
+
+Given "paper", "title", return true.
+
+Note:
+You may assume both s and t have the same length.
  */
 
 public class IsomorphicStrings
 {
-    public boolean isIsomorphic(String s, String t) 
+    public boolean isIsomorphic( String s, String t )
     {
-        
+    	Map<Character, Character> sToT = new HashMap<>();
+    	Map<Character, Character> tToS = new HashMap<>();
+    	for ( int i = 0; i < s.length(); i++ )
+    	{
+    		if ( sToT.containsKey( s.charAt( i ) ) && sToT.get( s.charAt( i ) ) != t.charAt( i ) 
+    				|| tToS.containsKey( t.charAt( i ) ) && tToS.get( t.charAt( i ) ) != s.charAt( i ) )
+    		{
+    			return false;
+    		}
+    		else
+    		{
+    			sToT.put( s.charAt( i ), t.charAt( i ) );
+    			tToS.put( t.charAt( i ), s.charAt( i ) );
+    		}
+    	}
+    	return true;
+    }
+    
+    @Test
+    public void test()
+    {
+    	assertTrue( !isIsomorphic( "ab", "aa" ) );
+    	assertTrue( isIsomorphic( "egg", "add" ) );
+    	assertTrue( !isIsomorphic( "foo", "bar" ) );
+    	assertTrue( isIsomorphic( "paper", "title" ) );
     }
 }
