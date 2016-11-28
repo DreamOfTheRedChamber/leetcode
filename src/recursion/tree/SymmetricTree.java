8c6fd306ca8d2caaf8771d60c1e5acea9fbc4d5b
freemanzhang <szhang@opentext.com>
2016-11-28 13:35:42 -0800

    add more concise recursive and iterative solution

--- src/recursion/tree/SymmetricTree.java
+++ src/recursion/tree/SymmetricTree.java
@@ -1,13 +1,61 @@
 package recursion.tree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 import utility.TreeNode;
 
 /**
- * 
+Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+
+For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+But the following [1,2,2,null,3,null,3] is not:
+    1
+   / \
+  2   2
+   \   \
+   3    3
  */
 
 public class SymmetricTree
 {
+	public boolean isSymmetricIterative( TreeNode root )
+	{
+		if ( root == null ) return true;
+		Queue<TreeNode> leftFirst = new LinkedList<>();
+		Queue<TreeNode> rightFirst = new LinkedList<>();
+		leftFirst.add( root );
+		rightFirst.add( root );
+		while ( !leftFirst.isEmpty() && !rightFirst.isEmpty() )
+		{
+			TreeNode left = leftFirst.poll();
+			TreeNode right = rightFirst.poll();
+			if ( left == null && right != null 
+					|| left != null && right == null )
+			{
+				return false;
+			}
+			if ( left != null && right != null )
+			{
+				if ( left.val != right.val )
+				{
+					return false;
+				}
+				leftFirst.add( left.left );
+				leftFirst.add( left.right );
+				rightFirst.add( right.right );
+				rightFirst.add( right.left );
+			}
+		}
+		return true;
+	}
+	
     public boolean isSymmetric( TreeNode root )
     {
     	if ( root == null )
@@ -18,31 +66,12 @@ public class SymmetricTree
     	return isSymmetricRecurse( root.left, root.right );
     }
     
-    boolean isSymmetricRecurse( TreeNode left, TreeNode right )
+    private boolean isSymmetricRecurse( TreeNode left, TreeNode right )
     {
-    	if ( left == null )
+    	if ( left == null || right == null )
     	{
-    		return right == null;
+    		return left == right;
     	}
-    	else if ( right == null )
-    	{
-    		return left == null;
-    	}
-    	else if ( left.val != right.val )
-    	{
-    		return false;
-    	}
-    	else if ( !isSymmetricRecurse( left.left, right.right ) )
-    	{
-    		return false;
-    	}
-    	else if ( !isSymmetricRecurse( left.right, right.left ) )
-    	{
-    		return false;
-    	}
-    	else
-    	{
-    		return true;
-    	}
+    	return left.val == right.val && isSymmetricRecurse( left.left, right.right ) && isSymmetricRecurse( left.right, right.left );
     }
 }
