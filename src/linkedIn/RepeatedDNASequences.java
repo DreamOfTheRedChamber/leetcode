f2d9f8cb2ed1c047595721220a7812aead12fc0f
freemanzhang <szhang@opentext.com>
2017-01-01 15:19:49 -0800

    init imp

--- src/linkedIn/RepeatedDNASequences.java
+++ src/linkedIn/RepeatedDNASequences.java
@@ -1,5 +1,10 @@
 package linkedIn;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /*
 All DNA is composed of a series of nucleotides abbreviated as A, C, G, and T, for example: "ACGAATTCCG". 
 When studying DNA, it is sometimes useful to identify repeated sequences within the DNA.
@@ -18,10 +23,53 @@ Return: ["AAAAACCCCC", "CCCCCAAAAA"].
 follow up 重复元素要允许存在
  * */
 
-//TO_HURRY
-public class RepeatedDNASequences 
+public class RepeatedDNASequences
 {
-    public List<String> findRepeatedDnaSequences( String s ) {
-        
+    public List<String> findRepeatedDnaSequences( String s ) 
+    {
+    	List<String> result = new ArrayList<>();
+    	if ( s == null || s.length () < 10 )
+    	{
+    		return result;
+    	}
+    	
+    	Map<Integer, Integer> map = new HashMap<>();
+    	
+    	// calculate initial hashcode
+    	int hashCode = 0;
+    	int base = 1;
+    	for ( int i = 0; i < 10; i++ )
+    	{
+    		hashCode += getCode( s.charAt ( i ) ) * base;
+    		base *= 5;
+    	}
+    	
+    	map.put ( hashCode, 1 );
+    	for ( int i = 10; i < s.length (); i++ )
+    	{
+    		hashCode = hashCode / 5 + getCode( s.charAt ( i ) ) * base;
+    		if ( !map.containsKey ( hashCode ) )
+    		{
+    			map.put ( hashCode, 1 );
+    		}
+    		else if ( map.get ( hashCode ) == 1 )
+    		{
+    			result.add ( s.substring ( i - 9, i + 1 ) );
+    			map.put ( hashCode, 2 );
+    		}
+    	}
+    	return result;
     }
+    
+    private int getCode( char c )
+    {
+    	switch ( c )
+    	{
+    		case 'A': return 1;
+    		case 'C': return 2;
+    		case 'G': return 3;
+    		case 'T': return 4;
+    		default: return 0;
+    	}
+    }    
 }
