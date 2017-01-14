ca3c9e5f3137446adfff243f11c4b80305cac5f2
freemanzhang <szhang@opentext.com>
2017-01-13 23:06:04 -0800

    set up questions

---
+++ src/linkedList/LinkedListCycle.java
@@ -0,0 +1,18 @@
+package linkedList;
+
+import utility.ListNode;
+
+/*
+Given a linked list, determine if it has a cycle in it.
+
+Follow up:
+Can you solve it without using extra space?
+ * */
+
+public class LinkedListCycle
+{
+    public boolean hasCycle( ListNode head )
+    {
+        
+    }
+}
--- src/math/ExcelSheetColumnNumber.java
+++ src/math/ExcelSheetColumnNumber.java
@@ -1,7 +1,20 @@
 package math;
 
 /**
- * 
+ * Related to question Excel Sheet Column Title
+
+Given a column title as appear in an Excel sheet, return its corresponding column number.
+
+For example:
+
+    A -> 1
+    B -> 2
+    C -> 3
+    ...
+    Z -> 26
+    AA -> 27
+    AB -> 28 
+
  */
 
 public class ExcelSheetColumnNumber
