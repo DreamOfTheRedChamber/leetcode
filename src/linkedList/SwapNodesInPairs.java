429a3b04138b3d51bd54cc1e506d39cd27f3f160
freemanzhang <szhang@opentext.com>
2017-01-19 15:47:35 -0800

    init impl

--- src/linkedList/SwapNodesInPairs.java
+++ src/linkedList/SwapNodesInPairs.java
@@ -15,6 +15,18 @@ public class SwapNodesInPairs
 {
     public ListNode swapPairs( ListNode head ) 
     {
-        
+    	ListNode dummy = new ListNode( 0 );
+    	dummy.next = head;
+    	ListNode current = dummy;
+    	while ( current.next != null && current.next.next != null )
+    	{
+    		ListNode first = current.next;
+    		ListNode second = current.next.next;
+    		first.next = second.next;
+    		current.next = second;
+    		current.next.next = first;
+    		current = current.next.next;
+    	}
+    	return dummy.next;
     }
 }
