88a6fc59a9b54bfd7f6a5fb044697a2fc7a2c90d
freemanzhang <szhang@opentext.com>
2017-01-19 13:41:05 -0800

    init impl

--- src/breathFirstSearch/SetMatrixZeroes.java
+++ src/breathFirstSearch/SetMatrixZeroes.java
@@ -14,8 +14,66 @@ Could you devise a constant space solution?
 
 public class SetMatrixZeroes
 {
-    public void setZeroes(int[][] matrix) 
+    public void setZeroes( int[][] matrix )
     {
-        
+		boolean firstRowZero = false;
+		boolean firstColumnZero = false;
+
+		// set first row and column zero or not
+		for ( int i = 0; i < matrix.length; i++ )
+		{
+			if ( matrix[i][0] == 0 )
+			{
+				firstColumnZero = true;
+				break;
+			}
+		}
+
+		for ( int i = 0; i < matrix[0].length; i++ )
+		{
+			if ( matrix[0][i] == 0 )
+			{
+				firstRowZero = true;
+				break;
+			}
+		}
+
+		// mark zeros on first row and column
+		for ( int i = 1; i < matrix.length; i++ )
+		{
+			for ( int j = 1; j < matrix[0].length; j++ )
+			{
+				if ( matrix[i][j] == 0 )
+				{
+					matrix[i][0] = 0;
+					matrix[0][j] = 0;
+				}
+			}
+		}
+
+		// use mark to set elements
+		for ( int i = 1; i < matrix.length; i++ )
+		{
+			for ( int j = 1; j < matrix[0].length; j++ )
+			{
+				if ( matrix[i][0] == 0 || matrix[0][j] == 0 )
+				{
+					matrix[i][j] = 0;
+				}
+			}
+		}
+
+		// set first column and row
+		if ( firstColumnZero )
+		{
+			for ( int i = 0; i < matrix.length; i++ )
+				matrix[i][0] = 0;
+		}
+
+		if ( firstRowZero )
+		{
+			for ( int i = 0; i < matrix[0].length; i++ )
+				matrix[0][i] = 0;
+		}
     }
 }
