f657177d80fbf2c1368a3e647fe1e22d82da5b57
freemanzhang <szhang@opentext.com>
2017-01-19 18:52:26 -0800

    init impl

--- src/math/add/AddDigits.java
+++ src/math/add/AddDigits.java
@@ -1,15 +1,41 @@
 package math.add;
 
 /**
- * 
+ *
+Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+
+For example:
+
+Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it.
+
+Follow up:
+Could you do it without any loop/recursion in O(1) runtime?
+
+Hint:
+
+A naive implementation of the above process is trivial. Could you come up with other methods?
+What are all the possible results?
+How do they occur, periodically or randomly?
+You may find this Wikipedia article useful.
  */
 
 public class AddDigits
 {
-
-    public int addDigits(int num) 
+    public int addDigits( int num )
     {
-        return 0;
+    	if ( num < 10 )
+    	{
+    		return num;
+    	}
+    	else
+    	{
+    		int result = 0;
+    		while ( num > 0 )
+    		{
+    			result += ( num % 10 );
+    			num = num / 10;
+    		}
+    		return addDigits( result );
+    	}
     }
-
 }
