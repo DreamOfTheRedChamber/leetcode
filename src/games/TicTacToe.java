1da07e42c885b75e238ec7371d39659ff0480ee3
freemanzhang <szhang@opentext.com>
2017-01-20 12:30:10 -0800

    refactor code

--- src/games/TicTacToe.java
+++ src/games/TicTacToe.java
@@ -1,26 +1,76 @@
 
 package games;
 
+/*
+Design a Tic-tac-toe game that is played between two players on a n x n grid.
+
+You may assume the following rules:
+
+A move is guaranteed to be valid and is placed on an empty block.
+Once a winning condition is reached, no more moves is allowed.
+A player who succeeds in placing n of their marks in a horizontal, vertical, or diagonal row wins the game.
+Example:
+Given n = 3, assume that player 1 is "X" and player 2 is "O" in the board.
+
+TicTacToe toe = new TicTacToe(3);
+
+toe.move(0, 0, 1); -> Returns 0 (no one wins)
+|X| | |
+| | | |    // Player 1 makes a move at (0, 0).
+| | | |
+
+toe.move(0, 2, 2); -> Returns 0 (no one wins)
+|X| |O|
+| | | |    // Player 2 makes a move at (0, 2).
+| | | |
+
+toe.move(2, 2, 1); -> Returns 0 (no one wins)
+|X| |O|
+| | | |    // Player 1 makes a move at (2, 2).
+| | |X|
+
+toe.move(1, 1, 2); -> Returns 0 (no one wins)
+|X| |O|
+| |O| |    // Player 2 makes a move at (1, 1).
+| | |X|
+
+toe.move(2, 0, 1); -> Returns 0 (no one wins)
+|X| |O|
+| |O| |    // Player 1 makes a move at (2, 0).
+|X| |X|
+
+toe.move(1, 0, 2); -> Returns 0 (no one wins)
+|X| |O|
+|O|O| |    // Player 2 makes a move at (1, 0).
+|X| |X|
+
+toe.move(2, 1, 1); -> Returns 1 (player 1 wins)
+|X| |O|
+|O|O| |    // Player 1 makes a move at (2, 1).
+|X|X|X|
+Follow up:
+Could you do better than O(n2) per move() operation?
+
+Hint:
+
+Could you trade extra space such that move() operation can be done in O(1)?
+You need two arrays: int rows[n], int cols[n], plus two variables: diagonal, anti_diagonal.
+ * */
+
 public class TicTacToe
 {
 
-	private int[] player1Row;
-	private int[] player1Col;
-	private int player1Diagnal;
-	private int player1ReverseDiagnal;
-	private int[] player2Row;
-	private int[] player2Col;
-	private int player2Diagnal;
-	private int player2ReverseDiagnal;
+	private int[] rows;
+	private int[] cols;
+	private int diagnal;
+	private int reverseDiagnal;
 	private int boardSize;
 	
 	/** Initialize your data structure here. */
 	public TicTacToe( int n )
 	{
-		player1Row = new int[n];
-		player1Col = new int[n];
-		player2Row = new int[n];
-		player2Col = new int[n];
+		rows = new int[n];
+		cols = new int[n];
 		boardSize = n;
 	}
 
@@ -38,59 +88,29 @@ public class TicTacToe
 	 */
 	public int move( int row, int col, int player )
 	{
-		if ( player == 1 )
+		int toAdd = player == 1 ? 1 : -1;
+
+		// record current move
+		rows[row] += toAdd;
+		cols[col] += toAdd;
+		if ( row == col )
 		{
-			// record current move
-			player1Row[row] += 1;
-			player1Col[col] += 1;
-			if ( row == col )
-			{
-				player1Diagnal += 1;
-			}
-			if ( row + col == boardSize - 1 )
-			{
-				player1ReverseDiagnal += 1;
-			}
-			
-			// check if win
-			if ( player1Row[row] == boardSize
-					|| player1Col[col] == boardSize
-					|| player1Diagnal == boardSize
-					|| player1ReverseDiagnal == boardSize )
-			{
-				return 1;
-			}
-			else
-			{
-				return 0;
-			}
+			diagnal += toAdd;
 		}
-		else
+		if ( row + col == boardSize - 1 )
 		{
-			player2Row[row] += 1;
-			player2Col[col] += 1;
-			if ( row == col )
-			{
-				player2Diagnal += 1;
-			}
-			if ( row + col == boardSize - 1 )
-			{
-				player2ReverseDiagnal += 1;
-			}
-			
-			// check if win
-			if ( player2Row[row] == boardSize
-					|| player2Col[col] == boardSize
-					|| player2Diagnal == boardSize 
-					|| player2ReverseDiagnal == boardSize )
-			{
-				return 2;
-			}
-			else
-			{
-				return 0;
-			}
+			reverseDiagnal += toAdd;
 		}
+			
+		// check if win
+		if ( Math.abs( rows[row] ) == boardSize
+				|| Math.abs( cols[col] ) == boardSize
+				|| Math.abs( diagnal ) == boardSize
+				|| Math.abs( reverseDiagnal ) == boardSize )
+		{
+			return player;
+		}
+		return 0;
 	}
 }
 
