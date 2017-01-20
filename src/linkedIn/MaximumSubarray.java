18ef76f9fc69346959027a011a91075d04a05b71
freemanzhang <szhang@opentext.com>
2017-01-20 15:25:44 -0800

    refactor code and add test cases

--- src/linkedIn/MaximumSubarray.java
+++ src/linkedIn/MaximumSubarray.java
@@ -1,5 +1,9 @@
 package linkedIn;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 /**
 Find the contiguous subarray within an array (containing at least one number) which has the largest sum.
 
@@ -12,18 +16,33 @@ public class MaximumSubarray
 {
     public int maxSubArray( int[] nums )
     {
-    	if ( nums.length == 0 )
+    	if ( nums == null || nums.length == 0 )
     	{
     		throw new IllegalArgumentException("");
     	}
     	
-    	int maxEndingAtCurrPos = nums[0];
+    	int localMaxSum = nums[0];
     	int globalMaxSum = nums[0];
     	for ( int i = 1; i < nums.length; i++ )
     	{
-    		maxEndingAtCurrPos = Math.max( maxEndingAtCurrPos + nums[i], nums[i] );
-    		globalMaxSum = Math.max( globalMaxSum, maxEndingAtCurrPos );
+    		localMaxSum = Math.max( localMaxSum + nums[i], nums[i] );
+    		globalMaxSum = Math.max( globalMaxSum, localMaxSum );
     	}
     	return globalMaxSum;
+    }
+    
+    @Test
+    public void test()
+    {    	
+    	// normal
+    	assertEquals( 6, maxSubArray( new int[]{ 4, -1, 2, 1 } ) );
+    	
+    	// extreme case, all pos/neg
+    	assertEquals( -1, maxSubArray( new int[]{ -5, -1, -2, -3 } ) );    	
+    	assertEquals( 10, maxSubArray( new int[]{ 4, 3, 2, 1 } ) );
+
+    	// edge case
+    	assertEquals( 4, maxSubArray( new int[]{ 4 } ) );
+    	assertEquals( 0, maxSubArray( new int[]{ } ) );
     }
 }
