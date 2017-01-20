fd22efb3dd32a9bba0e7e39e85465639003b5a6c
freemanzhang <szhang@opentext.com>
2017-01-19 18:58:12 -0800

    init impl

--- src/newProblems/FindUniqueCharacterInAString.java
+++ src/newProblems/FindUniqueCharacterInAString.java
@@ -17,6 +17,18 @@ public class FindUniqueCharacterInAString
 {
     public int firstUniqChar( String s )
     {
-        return 0;
+		int freq[] = new int[26];
+		for ( int i = 0; i < s.length(); i++ )
+		{
+			freq[s.charAt( i ) - 'a']++;
+		}
+		for ( int i = 0; i < s.length(); i++ )
+		{
+			if ( freq[s.charAt( i ) - 'a'] == 1 )
+			{
+				return i;
+			}
+		}
+		return -1;
     }
 }
