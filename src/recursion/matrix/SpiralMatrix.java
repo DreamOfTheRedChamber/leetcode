890d76a6c5c82ddee23ad87d66c8857bd106ce0e
freemanzhang <szhang@opentext.com>
2017-01-20 13:09:47 -0800

    recursive and iterative implementation

--- src/recursion/matrix/SpiralMatrix.java
+++ src/recursion/matrix/SpiralMatrix.java
@@ -20,76 +20,132 @@ You should return [1,2,3,6,9,8,7,4,5].
  */
 public class SpiralMatrix
 {
-    public List<Integer> spiralOrder( int[][] matrix )
+    public List<Integer> spiralOrderIterative( int[][] matrix )
     {
-    	if ( matrix == null 
-    			|| matrix.length == 0 
-    			|| matrix[0].length == 0 )
-    	{
-    		return new ArrayList<>();
-    	}
-    	
-    	List<Integer> result = new ArrayList<>();
-    	spiralOrder( matrix, 0, 0, matrix.length, matrix[0].length, result );
-    	return result;
+		List<Integer> result = new ArrayList<Integer>();
+		if ( matrix == null || matrix.length == 0 )
+			return result;
+		int height = matrix.length;
+		int width = matrix[0].length;
+		int xStart = 0;
+		int yStart = 0;
+		while ( height > 0 && width > 0 )
+		{
+
+			// if one row/column left, no circle can be formed
+			if ( height == 1 )
+			{
+				for ( int i = 0; i < width; i++ )
+				{
+					result.add( matrix[xStart][yStart++] );
+				}
+				break;
+			}
+			else if ( width == 1 )
+			{
+				for ( int i = 0; i < height; i++ )
+				{
+					result.add( matrix[xStart++][yStart] );
+				}
+				break;
+			}
+			// below, process a circle
+			// top - move right
+			for ( int i = 0; i < width - 1; i++ )
+			{
+				result.add( matrix[xStart][yStart++] );
+			}
+			// right - move down
+			for ( int i = 0; i < height - 1; i++ )
+			{
+				result.add( matrix[xStart++][yStart] );
+			}
+			// bottom - move left
+			for ( int i = 0; i < width - 1; i++ )
+			{
+				result.add( matrix[xStart][yStart--] );
+			}
+			// left - move up
+			for ( int i = 0; i < height - 1; i++ )
+			{
+				result.add( matrix[xStart--][yStart] );
+			}
+			xStart++;
+			yStart++;
+			height = height - 2;
+			width = width - 2;
+		}
+
+		return result;
+    }
+
+    public List<Integer> spiralOrderRecursive( int[][] matrix )
+    {
+		if ( matrix == null || matrix.length == 0 )
+		{
+			return new ArrayList<Integer>();
+		}
+		return spiralOrder( matrix, 0, 0, matrix.length, matrix[0].length );
     }
     
-    private void spiralOrder( int[][] matrix, int xCoor, int yCoor, int height, int width, List<Integer> result )
-    {
-    	// handle edge cases
-    	if ( height <= 0 || width <= 0 )
-    	{
-    		return;
-    	}    	
-    	if ( height == 1 && width == 1 )
-    	{
-    		result.add( matrix[xCoor][yCoor] );
-    		return;
-    	}
-    	
-    	// handle current level
-    	for ( int i = 0; i < width - 1; i++ )
-    	{
-    		result.add( matrix[xCoor][yCoor++] );
-    	}
-    	for ( int i = 0; i < height - 1; i++ )
-    	{
-    		result.add( matrix[xCoor++][yCoor] );
-    	}
-    	
-    	if ( height > 1 )
-    	{
-    		for ( int i = 0; i < width - 1; i++ )
-    		{
-    			result.add( matrix[xCoor][yCoor--] );
-    		}
-    	}
-    	
-    	if ( width > 1 )
-    	{
-    		for ( int i = 0; i < height - 1; i++ )
-    		{
-    			result.add( matrix[xCoor--][yCoor] );
-    		}
-    	}
-    	
-    	// recurse for the next level
-    	if ( height == 1 || width == 1 )
-    	{
-    		spiralOrder( matrix, xCoor, yCoor, 1, 1, result );
-    	}
-    	else
-    	{
-    		spiralOrder( matrix, xCoor + 1, yCoor + 1, height - 2, width - 2, result );
-    	}    	
-    }
+	private List<Integer> spiralOrder( int[][] matrix, int xStart, int yStart, int height, int width )
+	{
+		List<Integer> result = new ArrayList<Integer>();
+		if ( height <= 0 || width <= 0 )
+		{
+			return result;
+		}
+		// only one element left
+		if ( height == 1 && width == 1 )
+		{
+			result.add( matrix[xStart][yStart] );
+			return result;
+		}
+
+		// top - move right
+		for ( int i = 0; i < width - 1; i++ )
+		{
+			result.add( matrix[xStart][yStart++] );
+		}
+		// right - move down
+		for ( int i = 0; i < height - 1; i++ )
+		{
+			result.add( matrix[xStart++][yStart] );
+		}
+		// bottom - move left
+		if ( width > 1 )
+		{
+			for ( int i = 0; i < width - 1; i++ )
+			{
+				result.add( matrix[xStart][yStart--] );
+			}
+		}
+		// left - move up
+		if ( height > 1 )
+		{
+			for ( int i = 0; i < height - 1; i++ )
+			{
+				result.add( matrix[xStart--][yStart] );
+			}
+		}
+
+		if ( height == 1 || width == 1 )
+		{
+			result.addAll( spiralOrder( matrix, xStart, yStart, 1, 1 ) );
+		}
+		else
+		{
+			result.addAll( spiralOrder( matrix, xStart + 1, yStart + 1, height - 2, width - 2 ) );
+		}
+		return result;
+	}
     
     @Test
     public void test()
     {
-    	System.out.println( spiralOrder( new int[][]{ { 1 } } ) );
-    	System.out.println( spiralOrder( new int[][]{ { 1, 2, 3, 4 } } ) );    	
-    	System.out.println( spiralOrder( new int[][]{ { 1, 2, 3}, {4, 5, 6}, {7, 8, 9} } ) );
-    	System.out.println( spiralOrder( new int[][]{ { 1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12} } ) );
+//    	System.out.println( spiralOrder( new int[][]{ { 1 } } ) );
+//    	System.out.println( spiralOrder( new int[][]{ { 1, 2, 3, 4 } } ) );    	
+//    	System.out.println( spiralOrder( new int[][]{ { 1, 2, 3}, {4, 5, 6}, {7, 8, 9} } ) );
+//    	System.out.println( spiralOrder( new int[][]{ { 1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12} } ) );
     }
 }
