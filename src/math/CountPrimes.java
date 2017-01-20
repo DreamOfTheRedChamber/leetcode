499090390c8dd5522b803d5d7d24542b64385e08
freemanzhang <szhang@opentext.com>
2017-01-19 19:02:30 -0800

    init impl

--- src/math/CountPrimes.java
+++ src/math/CountPrimes.java
@@ -4,12 +4,26 @@ package math;
 Count the number of prime numbers less than a non-negative number, n.
  */
 
-// TO_START
 public class CountPrimes
 {
 
-    public int countPrimes(int n) {
-        return 0;
-    }
+	public int countPrimes( int n )
+	{
+		boolean[] notPrime = new boolean[n];
+		int count = 0;
+		for ( int i = 2; i < n; i++ )
+		{
+			if ( notPrime[i] == false )
+			{
+				count++;
+				for ( int j = 2; i * j < n; j++ )
+				{
+					notPrime[i * j] = true;
+				}
+			}
+		}
+
+		return count;
+	}
 
 }
