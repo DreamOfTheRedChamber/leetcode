a47eb13229878a6cfd3d63f41aae29f4be4730fc
Shijie Zhang <sjzhang1901@gmail.com>
2019-10-26 22:44:49 -0700

    add solution for validateBinarySearchTree

--- Python/ValidateBinarySearchTree.py
+++ Python/ValidateBinarySearchTree.py
@@ -2,8 +2,8 @@
 import unittest
 
 # TODO:
-# 1. Understand how polymorphism works in python
-# 2. Try other two methods to solve the problem
+# 1. Understand how polymorphism works in python, needs external libraries
+# 2. Try other two methods to solve the problem, done
 
 class TreeNode:
     def __init__(self, x):
@@ -12,9 +12,11 @@ class TreeNode:
         self.right = None
 
 class ValidateBinarySearchTree(unittest.TestCase):
+
     def isValidBST(self, root: TreeNode) -> bool:
         return self.isValidBSTWithBound(root, float('-inf'), float('inf'))
 
+    # worst case space O(n), average case space O(logn), time O(n)
     def isValidBSTWithBound(self, node, lower, upper):
         if not node:
             return True
@@ -24,8 +26,30 @@ class ValidateBinarySearchTree(unittest.TestCase):
         else:
             return self.isValidBSTWithBound(node.left, lower, node.val) and self.isValidBSTWithBound(node.right, node.val, upper)
 
+    # worst case space O(n), average case space O(logn) time O(n)
+    def isValidBSTInOrder(self, root: TreeNode) -> bool:
+        prev = float('-inf')
+        stack = [(root, True)]
+
+        while stack:
+            node, isFirstTime = stack.pop()
+            if not node:
+                continue
+            if not isFirstTime:
+                if node.val <= prev:
+                    return False
+                else:
+                    prev = node.val
+            else:
+                stack.append((node.right, True))
+                stack.append((node, False))
+                stack.append((node.left, True))
+
+        return True
+
     def test_SingleNodeTest(self):
         node1 = TreeNode(3)
+        self.assertEqual(True, self.isValidBSTInOrder(node1))
         self.assertEqual(True, self.isValidBST(node1))
 
     def test_NormalTree(self):
@@ -40,10 +64,38 @@ class ValidateBinarySearchTree(unittest.TestCase):
         node4.left = node3
         node4.right = node5
 
+        self.assertEqual(True, self.isValidBSTInOrder(node2))
         self.assertEqual(True, self.isValidBST(node2))
 
-    
+    def test_TreeWithMaximumNode(self):
+        node1 = TreeNode(1)
+        node2 = TreeNode(2)
+        node3 = TreeNode(3)
+        node4 = TreeNode(4)
+        node5 = TreeNode(5)
 
+        node3.left = node1
+        node3.right = node4
+        node4.left = node2
+        node4.right = node5
+
+        self.assertEqual(False, self.isValidBSTInOrder(node3))
+        self.assertEqual(False, self.isValidBST(node3))
+
+    def test_SkewedTree(self):
+        node1 = TreeNode(1)
+        node2 = TreeNode(2)
+        node3 = TreeNode(3)
+        node4 = TreeNode(4)
+        node5 = TreeNode(5)
+
+        node1.right = node2
+        node2.right = node3
+        node3.right = node4
+        node4.right = node5
+
+        self.assertEqual(True, self.isValidBSTInOrder(node3))
+        self.assertEqual(True, self.isValidBST(node3))
 
 if __name__ == '__main__':
     unittest.main()
