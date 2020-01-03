cf451f74c7737b87f8a1656862837ec089d96704
Shijie Zhang <ericzhangworking@gmail.com>
2020-01-03 10:47:17 -0800

    add skeleton for dictionary elements

---
+++ Python/TestDataStructureDict.py
@@ -0,0 +1,33 @@
+# Definition for a binary tree node.
+import unittest
+
+# Read about enumerate in python
+
+class TestDataStructureDict(unittest.TestCase):
+
+    @unittest.skip
+    def test_CreateDictionary(self):
+        # default dictionary
+
+        return True
+
+    @unittest.skip
+    def test_Loop(self):
+        # Loop through keys
+
+        # Loop through values
+
+        # Loop through tuples
+        return True
+
+    @unittest.skip
+    def test_AddDeleteElement(self):
+        return True
+
+    @unittest.skip
+    def test_GetElement(self):
+        # setdefault
+        return True
+
+if __name__ == '__main__':
+    unittest.main()
