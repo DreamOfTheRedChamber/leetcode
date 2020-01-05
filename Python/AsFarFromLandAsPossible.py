12ca649d1a52bcf815c27a82ba095a314d4a725d
Shijie Zhang <ericzhangworking@gmail.com>
2020-01-05 09:01:32 -0800

    initial solution for AsFarFromLandAsPossible

---
+++ Python/AsFarFromLandAsPossible.py
@@ -0,0 +1,54 @@
+# Definition for a binary tree node.
+import unittest
+from collections import deque
+
+class AsFarFromLandAsPossible(unittest.TestCase):
+
+    def maxDistance(self, grid: list) -> int:
+        maxDis = -1
+
+        for row in range(len(grid)):
+            for col in range(len(grid[row])):
+                if grid[row][col] == 0:
+                    maxDis = max(maxDis, self.bfs(grid, row, col))
+
+        return maxDis
+
+    def bfs(self, grid: list, row: int, col: int) -> int:
+        bfsQueue = deque()
+        bfsQueue.append((row, col))
+        discovered = [[False for col in range(len(grid[0]))] for row in range(len(grid))]
+
+        depth = 0
+        closestDis = -1
+        while len(bfsQueue) > 0:
+            levelSize = len(bfsQueue)
+
+            for i in range(levelSize):
+                xCor, yCor = bfsQueue.popleft()
+
+                if closestDis == -1 and grid[xCor][yCor] == 1:
+                    closestDis = depth
+                    return closestDis
+
+                for direction in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                    neighborX = xCor + direction[0]
+                    neighborY = yCor + direction[1]
+                    if 0 <= neighborX < len(grid) and 0 <= neighborY < len(grid[0]) and discovered[neighborX][neighborY] is False:
+                        discovered[neighborX][neighborY] = True
+                        bfsQueue.append((neighborX, neighborY))
+
+            depth += 1
+
+        return closestDis
+
+    def test_NoWaterOrLand(self):
+        self.assertEqual(-1, self.maxDistance([[1, 1, 1]]))
+        self.assertEqual(-1, self.maxDistance([[0, 0, 0]]))
+
+    def test_NormalTestCases(self):
+        self.assertEqual(2, self.maxDistance([[1, 0, 1], [0, 0, 0], [1, 0, 1]]))
+        self.assertEqual(4, self.maxDistance([[1, 0, 0], [0, 0, 0], [0, 0, 0]]))
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ Python/Template.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+import unittest
+
+# Read about enumerate in python
+
+class Template(unittest.TestCase):
+
+    @unittest.skip
+    def test_Test1(self):
+        str1 = None
+        str2 = ""
+
+    def test_Test2(self):
+        str1 = "ab"
+        str2 = "bc"
+        result = str1 + str2
+        print(result)
+
+if __name__ == '__main__':
+    unittest.main()
