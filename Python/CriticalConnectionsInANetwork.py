8519a3a8a2263424252b07a31197f1cc1e9413ba
Shijie Zhang <ericzhangworking@gmail.com>
2020-01-04 22:31:35 -0800

    add a critical connections in a network solution

---
+++ Python/CriticalConnectionsInANetwork.py
@@ -0,0 +1,6 @@
+class CriticalConnectionsInANetwork:
+
+
+    def CriticalConnectionsInaNetwork(self, s: str, k: int) -> str:
+        # https://www.youtube.com/watch?v=aZXi1unBdJA&list=WL&index=97&t=0s
+        return None
