0fee2ddf6c2c7cc3e7426ae04f7ba31891564b63
Shijie Zhang <ericzhangworking@gmail.com>
2020-01-05 09:55:26 -0800

    version two which optimize with where to start bfs

--- Python/AsFarFromLandAsPossible.py
+++ Python/AsFarFromLandAsPossible.py
@@ -5,6 +5,33 @@ from collections import deque
 class AsFarFromLandAsPossible(unittest.TestCase):
 
     def maxDistance(self, grid: list) -> int:
+        bfsQueue = deque()
+        discovered = [[False for col in range(len(grid[0]))] for row in range(len(grid))]
+        for row in range(len(grid)):
+            for col in range(len(grid[row])):
+                if grid[row][col] == 1:
+                    bfsQueue.append((row, col))
+
+        depth = 0
+        farestDis = -1
+        while len(bfsQueue) > 0:
+            levelSize = len(bfsQueue)
+
+            for i in range(levelSize):
+                xCor, yCor = bfsQueue.popleft()
+
+                for direction in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                    neighborX = xCor + direction[0]
+                    neighborY = yCor + direction[1]
+                    if 0 <= neighborX < len(grid) and 0 <= neighborY < len(grid[0]) and discovered[neighborX][neighborY] is False and grid[neighborX][neighborY] == 0:
+                        discovered[neighborX][neighborY] = True
+                        bfsQueue.append((neighborX, neighborY))
+                        farestDis = depth + 1
+            depth += 1
+
+        return farestDis
+
+    def maxDistanceSolution2(self, grid: list) -> int:
         maxDis = -1
 
         for row in range(len(grid)):
@@ -14,7 +41,7 @@ class AsFarFromLandAsPossible(unittest.TestCase):
 
         return maxDis
 
-    def bfs(self, grid: list, row: int, col: int) -> int:
+    def bfsSolution2(self, grid: list, row: int, col: int) -> int:
         bfsQueue = deque()
         bfsQueue.append((row, col))
         discovered = [[False for col in range(len(grid[0]))] for row in range(len(grid))]
@@ -41,6 +68,8 @@ class AsFarFromLandAsPossible(unittest.TestCase):
             depth += 1
 
         return closestDis
+
+
 
     def test_NoWaterOrLand(self):
         self.assertEqual(-1, self.maxDistance([[1, 1, 1]]))
