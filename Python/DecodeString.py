6f8297c240b4faefa905ea8c14c37bd225f22181
Shijie Zhang <ericzhangworking@gmail.com>
2020-01-05 19:34:10 -0800

    add new implementation based on hashmap

--- Python/DecodeString.py
+++ Python/DecodeString.py
@@ -6,7 +6,41 @@ from collections import deque
 
 class DecodeString(unittest.TestCase):
 
-    def decodeString(self, s: str) -> str:
+    # Please look at HashMap solution here: https://leetcode.com/problems/decode-string/discuss/449085/Python3-Solution-Using-HashMap-greater99-Runtime-100-Memory
+    def decodeString(self, s):
+        from collections import defaultdict
+
+        # hashmap explained: key = current level, value = tuple with (multiplier, string)
+        d = defaultdict(lambda: [0, ""])
+        level = 1  # start at level 1 so level 0 is global chars not contained in brackets
+        n = ""
+        res = ""
+
+        for i, ch in enumerate(s):
+            # increment level
+            if ch == "[":
+                d[level] = [int(n), ""]
+                n = ""
+                level += 1
+
+            # decrement level
+            elif ch == "]":
+                d[level - 2][1] += d[level - 1][0] * d[level - 1][1]
+                d[level - 1] = [0, ""]
+                level -= 1
+
+            # write to current level, d[0] = global level
+            elif ch.isalpha():
+                d[level - 1][1] += ch
+
+            # build current number
+            else:
+                n += ch
+
+        return d[0][1]  # return d[0], result of global level string
+
+
+    def decodeStringWrong(self, s: str) -> str:
         if len(s) == 0:
             return s
 
@@ -23,30 +57,22 @@ class DecodeString(unittest.TestCase):
                     chPos += 1
                 numberStack.append(number)
             elif s[chPos] == '[':
-                chPos = chPos + 1
-            elif 'a' <= s[chPos] <= 'z' or 'A' <= s[chPos] <= 'Z':
-                word = word + s[chPos]
+                letterStack.append(word)
+                word = ""
                 chPos = chPos + 1
             elif s[chPos] == ']':
+                temp = letterStack.pop()
                 number = numberStack.pop()
-                wordTemp = number * word
-                if len(letterStack) == 0:
-                    letterStack.append(wordTemp)
-                else:
-                    stackTop = letterStack.pop()
-                    letterStack.append(stackTop + wordTemp)
+                word = number * temp
                 chPos += 1
             else:
-                raise ValueError("invalid cases")
+                word = word + s[chPos]
+                chPos += 1
 
-        result = letterStack.pop()
-        while len(letterStack) > 0:
-            result = letterStack.pop() + result
+        return word
 
-        return result
-
-    # recursion two stack always push not working for case self.assertEqual("aaabFFFFcbFFFFc", self.decodeString("3[a]2[b4[F]c]"))
-    def decodeStringAlwaysPush(self, s: str) -> str:
+    # stack based algorithm is so error prone
+    def decodeStringStackErrorProne(self, s: str) -> str:
         if len(s) == 0:
             return s
 
@@ -69,22 +95,27 @@ class DecodeString(unittest.TestCase):
                     letters = letters + s[chPos]
                     chPos = chPos + 1
                 letterStack.append(letters)
+
+                if len(letterStack) == len(numberStack) + 1:
+                    numberStack.append(1)
             elif s[chPos] == ']':
                 number = numberStack.pop()
                 letter = letterStack.pop()
                 numberTemp = number * letter
                 if len(letterStack) == 0:
                     letterStack.append(numberTemp)
+                    numberStack.append(1)
                 else:
                     stackTop = letterStack.pop()
                     letterStack.append(stackTop + numberTemp)
+
                 chPos += 1
             else:
                 raise ValueError("invalid cases")
 
-        result = letterStack.pop()
+        result = ""
         while len(letterStack) > 0:
-            result = letterStack.pop() + result
+            result = letterStack.pop() * numberStack.pop() + result
 
         return result
 
@@ -113,6 +144,7 @@ class DecodeString(unittest.TestCase):
         self.assertEqual("accaccacc", self.decodeString("3[a2[c]]"))
         self.assertEqual("abcabccdcdcdef", self.decodeString("2[abc]3[cd]ef"))
         self.assertEqual("aaabFFFFcbFFFFc", self.decodeString("3[a]2[b4[F]c]"))
+        self.assertEqual("zzzyypqjkjkefjkjkefjkjkefjkjkefyypqjkjkefjkjkefjkjkefjkjkefef", self.decodeString("3[z]2[2[y]pq4[2[jk]e1[f]]]ef"))
 
 if __name__ == '__main__':
     unittest.main()
