3321585c9675dbc4b295e2eb5e2b1edc2c57c5b4
Shijie Zhang <ericzhangworking@gmail.com>
2020-02-14 07:09:59 -0800

    add implementtion for connections

--- Python/Graph/CriticalConnectionsInGraph.py
+++ Python/Graph/CriticalConnectionsInGraph.py
@@ -1,8 +1,10 @@
 # Definition for a binary tree node.
 import unittest
+import collections
 from collections import defaultdict
 
 # Read about enumerate in python
+# https://www.youtube.com/watch?v=aZXi1unBdJA
 
 class CriticalConnectionsInGraph(unittest.TestCase):
 
@@ -15,20 +17,22 @@ class CriticalConnectionsInGraph(unittest.TestCase):
     def criticalConnections(self, n: int, connections: list) -> list:
         discovered = dict()
         low = dict()
-        parents = defaultdict(lambda: -1)
         criticalConnections = []
 
-        graph = self.buildGraph(connections)
+        graph = defaultdict(list)
+        for connection in connections:
+            graph[connection[0]].append(connection[1])
+            graph[connection[1]].append(connection[0])
 
         for i in range(n):
             if i not in discovered:
-                self.dfs(graph, i, parents, low, discovered, criticalConnections)
+                self.dfs(graph, i, -1, low, discovered, criticalConnections)
 
         return criticalConnections
 
     sequence = 0
 
-    def dfs(self, graph: dict, curNode: int, parents: dict, low: dict, discovered: dict, criticalConnections: list):
+    def dfs(self, graph: dict, curNode: int, parent: int, low: dict, discovered: dict, criticalConnections: list):
         if curNode in discovered:
             return
 
@@ -37,23 +41,39 @@ class CriticalConnectionsInGraph(unittest.TestCase):
         discovered[curNode] = self.sequence
 
         for neighbor in graph[curNode]:
-            if neighbor not in discovered:
-                parents[neighbor] = curNode
-                self.dfs(graph, neighbor, parents, low, discovered, criticalConnections)
+            if parent == neighbor:
+                continue
+            elif neighbor not in discovered:
+                self.dfs(graph, neighbor, curNode, low, discovered, criticalConnections)
 
                 if low[neighbor] > discovered[curNode]:
                     criticalConnections.append([curNode, neighbor])
 
                 low[curNode] = min(low[curNode], low[neighbor])
-            elif parents[curNode] != neighbor:
+            else:
                 low[curNode] = min(low[curNode], discovered[neighbor])
 
-    def buildGraph(self, connections: list) -> dict:
-        graph = defaultdict(lambda: [])
-        for connection in connections:
-            graph[connection[0]].append(connection[1])
-            graph[connection[1]].append(connection[0])
-        return graph
+    def criticalConnections2(self, n: int, connections: list) -> list:
+        g = collections.defaultdict(list)
+        for a, b in connections:
+            g[a].append(b)
+            g[b].append(a)
+        low = [0] * n
+
+        def dfs(rank, cur, parent):
+            low[cur] = rank
+            ans = []
+            for child in g[cur]:
+                if child == parent: continue
+                if low[child] == 0:
+                    ans += dfs(rank + 1, child, cur)
+                low[cur] = min(low[cur], low[child])
+
+                if low[child] > rank:
+                    ans.append([child, cur])
+            return ans
+
+        return dfs(1, 0, -1)
 
 if __name__ == '__main__':
     unittest.main()
