d435f59f30fc05e47c681d1f936fbdb529034843
Shijie Zhang <ericzhangworking@gmail.com>
2020-04-01 23:08:37 -0700

    Fix the bug

--- Python/Graph/ValidateBinaryTreeNodes.py
+++ Python/Graph/ValidateBinaryTreeNodes.py
@@ -9,16 +9,19 @@ class ValidateBinaryTreeNodes(unittest.TestCase):
 
     def validateBinaryTreeNodes(self, n: int, leftChild: List[int], rightChild: List[int]) -> bool:
 
-        def dfs(start: int, discovered: set, leftChild: List[int], rightChild: List[int]) -> bool:
+        def dfs(start: int, discovered: set, leftChild: List[int], rightChild: List[int]) -> (bool, int):
             if start in discovered:
-                return False
+                return False, 0
             if start == -1:
-                return True
+                return True, 0
 
             discovered.add(start)
-            return dfs(leftChild[start], discovered, leftChild, rightChild) and dfs(rightChild[start], discovered, leftChild, rightChild)
+            leftChildValid, leftChildCount = dfs(leftChild[start], discovered, leftChild, rightChild)
+            rightChildValid, rightChildCount = dfs(rightChild[start], discovered, leftChild, rightChild)
 
-        rootCandidates = set(range(1, n + 1))
+            return leftChildValid and rightChildValid, leftChildCount + rightChildCount + 1
+
+        rootCandidates = set(range(n))
         for i in range(len(leftChild)):
             if leftChild[i] in rootCandidates:
                 rootCandidates.remove(leftChild[i])
@@ -28,13 +31,19 @@ class ValidateBinaryTreeNodes(unittest.TestCase):
             return False
         root = rootCandidates.pop()
 
-        return dfs(root, set(), leftChild, rightChild)
+        isValidTree, nodeCount = dfs(root, set(), leftChild, rightChild)
+        return isValidTree and nodeCount == n
+
 
     def test_Leetcode(self):
         self.assertTrue(self.validateBinaryTreeNodes(4, [1, -1, 3, -1], [2, -1, -1, -1]))
-        self.assertTrue(self.validateBinaryTreeNodes(4, [1, -1, 3, -1], [2, 3, -1, -1]))
-        self.assertTrue(self.validateBinaryTreeNodes(2, [1, 0], [-1, -1]))
-        self.assertTrue(self.validateBinaryTreeNodes(6, [1, -1, -1, 4, -1, -1], [2, -1, -1, 5, -1, -1]))
+        self.assertFalse(self.validateBinaryTreeNodes(4, [1, -1, 3, -1], [2, 3, -1, -1]))
+        self.assertFalse(self.validateBinaryTreeNodes(2, [1, 0], [-1, -1]))
+        self.assertFalse(self.validateBinaryTreeNodes(6, [1, -1, -1, 4, -1, -1], [2, -1, -1, 5, -1, -1]))
+
+
+    def test_ErrorCase(self):
+        self.assertFalse(self.validateBinaryTreeNodes(4, [1, 2, 0, -1], [-1, -1, -1, -1]))
 
 if __name__ == '__main__':
     unittest.main()
