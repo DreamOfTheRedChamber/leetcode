b47cdb530232756c979765a373b9b8d7786e6061
Shijie Zhang <ericzhangworking@gmail.com>
2020-04-09 16:55:21 -0700

    Fix error case

--- Python/BinarySearch/FindMinimumInRotatedSortedArrayII.py
+++ Python/BinarySearch/FindMinimumInRotatedSortedArrayII.py
@@ -14,6 +14,11 @@ class FindMinimumInRotatedSortedArrayII(unittest.TestCase):
 
             low = 0
             high = len(nums) - 1
+
+            # Adding this condition makes the difference
+            while nums[high] == nums[low] and high > low:
+                high -= 1
+
             while low + 1 < high:
                 mid = (high - low) // 2 + low
                 if nums[mid] == nums[low]:
@@ -28,13 +33,17 @@ class FindMinimumInRotatedSortedArrayII(unittest.TestCase):
         minIndex = (findPeak(nums) + 1) % len(nums)
         return nums[minIndex]
 
+
+
     @unittest.skip
     def test_Leetcode(self):
         self.assertEqual(1, self.findMin([3, 4, 5, 1, 2]))
         self.assertEqual(0, self.findMin([4, 5, 6, 7, 0, 1, 2]))
 
     def test_wrongSubmission(self):
+        # Adding this condition makes the difference
         self.assertEqual(1, self.findMin([10, 1, 10, 10, 10]))
+        self.assertEqual(1, self.findMin([10, 10, 10, 1, 10]))
 
 if __name__ == '__main__':
     unittest.main()
