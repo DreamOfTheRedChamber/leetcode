5e8de86df57f1c421db9dafad7ba35d60893601e
Shijie Zhang <ericzhangworking@gmail.com>
2020-04-12 19:38:21 -0700

    add DP implementation

---
+++ Python/DynamicProgramming/DistinctSubsequences.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+import unittest
+
+# Read about enumerate in python
+
+class Template(unittest.TestCase):
+
+    def printVertically(self, s: str) -> list:
+        return []
+
+    def test_Test1(self):
+        str1 = "TO BE OR NOT TO BE"
+        print(self.printVertically(str1))
+
+        str2 = "HOW ARE YOU"
+        print(self.printVertically(str2))
+
+
+
+if __name__ == '__main__':
+    unittest.main()
