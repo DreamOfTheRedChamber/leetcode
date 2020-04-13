9b13980aa37f79eab67bdd781f7864f8146d8c5f
Shijie Zhang <ericzhangworking@gmail.com>
2020-04-13 08:50:30 -0700

    add implementation for distinct subsequences

--- Python/DynamicProgramming/DistinctSubsequences.py
+++ Python/DynamicProgramming/DistinctSubsequences.py
@@ -3,19 +3,25 @@ import unittest
 
 # Read about enumerate in python
 
-class Template(unittest.TestCase):
+class DistinctSubsequences(unittest.TestCase):
 
-    def printVertically(self, s: str) -> list:
-        return []
+    def numDistinct(self, s: str, t: str) -> int:
+        numWays = [[0 for x in range(len(t) + 1)] for y in range(len(s) + 1)]
+        for row in range(len(s) + 1):
+            numWays[row][0] = 1
 
-    def test_Test1(self):
-        str1 = "TO BE OR NOT TO BE"
-        print(self.printVertically(str1))
+        for row in range(1, len(s) + 1):
+            for col in range(1, len(t) + 1):
+                if s[row - 1] == t[col - 1]:
+                    numWays[row][col] = numWays[row - 1][col - 1] + numWays[row - 1][col]
+                else:
+                    numWays[row][col] = numWays[row - 1][col]
 
-        str2 = "HOW ARE YOU"
-        print(self.printVertically(str2))
+        return numWays[len(s)][len(t)]
 
-
+    def test_Leetcode(self):
+        self.assertEqual(3, self.numDistinct("rabbbit", "rabbit"))
+        self.assertEqual(5, self.numDistinct("babgbag", "bag"))
 
 if __name__ == '__main__':
     unittest.main()
