4a2d6183133257e88720b978bccf3b7598cf4867
Shijie Zhang <ericzhangworking@gmail.com>
2020-04-13 23:32:06 -0700

    refactor

--- Python/DynamicProgramming/PalindromeRemoval.py
+++ Python/DynamicProgramming/PalindromeRemoval.py
@@ -8,8 +8,7 @@ from typing import List
 class PalindromeRemoval(unittest.TestCase):
 
     def minimumMoves(self, arr: List[int]) -> int:
-        # dp = [[0 for x in range(len(arr))] for y in range(len(arr))]
-        dp = [[len(arr) for i in arr] for j in arr]
+        dp = [[len(arr) for x in range(len(arr))] for y in range(len(arr))]
 
         for i in range(len(arr)):
             dp[i][i] = 1
@@ -17,14 +16,12 @@ class PalindromeRemoval(unittest.TestCase):
             dp[i][i + 1] = 1 if arr[i] == arr[i + 1] else 2
 
         for length in range(3, len(arr) + 1):
-            for start in range(3, len(arr) - length + 1):
+            for start in range(0, len(arr) - length + 1):
                 end = start + length - 1
                 if arr[start] == arr[end]:
                     dp[start][end] = dp[start + 1][end - 1]
-                else:
-                    dp[start][end] = dp[start + 1][end - 1] + 2
 
-                for mid in range(start, start + length):
+                for mid in range(start, end):
                     dp[start][end] = min(dp[start][end], dp[start][mid] + dp[mid + 1][end])
 
         return dp[0][len(arr) - 1]
