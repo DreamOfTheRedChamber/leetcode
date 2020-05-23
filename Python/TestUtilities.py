782ec802b12c5b5f83fc7798af720ead1a37078a
Shijie Zhang <ericzhangworking@gmail.com>
2020-05-23 15:17:09 -0700

    init impl

--- Python/TestUtilities.py
+++ Python/TestUtilities.py
@@ -1,11 +1,13 @@
 # Definition for a binary tree node.
 import bisect
+import collections
 import unittest
 
 # Read about enumerate in python
 
 class TestUtilities(unittest.TestCase):
 
+    @unittest.skip
     def test_bisectSearch(self):
         li = [1, 3, 4, 4, 4, 6, 7]
 
@@ -17,6 +19,13 @@ class TestUtilities(unittest.TestCase):
 
         print("The rightmost index to insert, so list remains sorted is  : ", end="")
         print(bisect.bisect_right(li, 4))
+
+    def test_collectionsCounter(self):
+        li = [1, 3, 4, 4, 4, 6, 7]
+
+        # returns a default dictionary
+        result = collections.Counter(li)
+        self.assertEqual(0, result[0])
 
 if __name__ == '__main__':
     unittest.main()
