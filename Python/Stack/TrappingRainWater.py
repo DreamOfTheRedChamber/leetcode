8dcc9cfa3f1fe336769fbc9eefb8309c6aedc957
Shijie Zhang <ericzhangworking@gmail.com>
2020-06-07 23:59:21 -0700

    Update TrappingRainWater.py

--- Python/Stack/TrappingRainWater.py
+++ Python/Stack/TrappingRainWater.py
@@ -4,19 +4,19 @@ import unittest
 class TrappingRainWater(unittest.TestCase):
 
     def trap(self, height: list) -> int:
-        monoDecStack = []
-        maxTrappedWater = 0
+        decStack = []
+        result = 0
+        i = 0
+        while i < len(height):
+            if decStack and height[decStack[-1]] < height[i]:
+                top = decStack.pop()
+                if decStack:
+                    result += (min(height[decStack[-1]], height[i]) - height[top]) * (i - 1 - decStack[-1])
+            else:
+                decStack.append(i)
+                i += 1
 
-        for i in range(len(height)):
-            while len(monoDecStack) > 0 and height[monoDecStack[-1]] < height[i]:
-                stackTop = monoDecStack.pop()
-                trappedHeighth = min(0 if len(monoDecStack) == 0 else height[monoDecStack[-1]], height[i]) - height[stackTop]
-                trappedWidth = 0 if len(monoDecStack) == 0 else i - 1 - monoDecStack[-1]
-                maxTrappedWater += trappedHeighth * trappedWidth
-
-            monoDecStack.append(i)
-
-        return maxTrappedWater
+        return result
 
     def test_MonoContainer(self):
         self.assertEqual(0, self.trap([0, 1, 2, 3, 4, 5]))
@@ -24,12 +24,17 @@ class TrappingRainWater(unittest.TestCase):
 
     def test_Normal(self):
         self.assertEqual(6, self.trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]))
-        self.assertEqual(7, self.trap([0, 1, 0, 2, 1, 1, 0, 1, 3, 2, 1, 2, 1]))
 
     def test_EdgeCase(self):
         self.assertEqual(0, self.trap([]))
         self.assertEqual(0, self.trap([1, 2]))
         self.assertEqual(0, self.trap([1]))
+
+    def test_WrongAnswer1(self):
+        self.assertEqual(9, self.trap([4, 2, 0, 3, 2, 5]))
+
+    def test_WrongAnswer2(self):
+        self.assertEqual(7, self.trap([0, 1, 0, 2, 1, 1, 0, 1, 3, 2, 1, 2, 1]))
 
 if __name__ == '__main__':
     unittest.main()
