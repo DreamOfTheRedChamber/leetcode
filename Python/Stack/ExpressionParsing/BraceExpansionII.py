4912e23d009b39e0a29977105d5d5a8bb9210840
Shijie Zhang <ericzhangworking@gmail.com>
2020-06-17 22:32:25 -0700

    Update BraceExpansionII.py

--- Python/Stack/ExpressionParsing/BraceExpansionII.py
+++ Python/Stack/ExpressionParsing/BraceExpansionII.py
@@ -14,84 +14,83 @@ class BraceExpansionII(unittest.TestCase):
 
         def multiply(setA: set, setB: set) -> set:
             result = set()
+            if not setA:
+                setA.add("")
+            if not setB:
+                setB.add("")
             for aElement in setA:
                 for bElement in setB:
                     result.add(aElement+bElement)
             return result
 
+        processedExp = ""
+        for i in range(len(expression)):
+            if expression[i].isalpha():
+                processedExp += "{"
+                processedExp += expression[i]
+                processedExp += "}"
+            else:
+                processedExp += expression[i]
+
         typeStack = []
         expressionStack = []
-        token = ""
         curSet = set()
-        for i in range(len(expression)):
-            if expression[i] == "{":
+        i = 0
+        while i < len(processedExp):
+            if processedExp[i] == "{":
                 # normal example without comma: {a{b{c}}}
                 # reason to add two if condition:
                 # 1. successive left curly: {{{a}bd}c}
                 # 2. left curly prefixed by comma: {a{b,{c}}}
-                if token and curSet:
-                    newUnion = set()
-                    for element in curSet:
-                        newUnion.add(element + token)
-                    curSet = newUnion
-                    token = ""
-                if token:
-                    curSet.add(token)
-                    token = ""
-                if curSet:
-                    expressionStack.append(curSet)
-                    typeStack.append("{")
-                    curSet = set()
-            elif expression[i] == ",":
+                expressionStack.append(curSet)
+                typeStack.append("{")
+                curSet = set()
+                i += 1
+            elif processedExp[i] == ",":
                 # normal example with comma: {a, b, cd, e}, {a, {bc}, {d}e}
                 # avoid pushing empty token and union: {a{b,c},{ab,z}}
-                if token:
-                    curSet.add(token)
-                    token = ""
-                if curSet:
-                    expressionStack.append(curSet)
-                    typeStack.append(",")
-                    curSet = set()
-            elif expression[i] == "}":
+                expressionStack.append(curSet)
+                typeStack.append(",")
+                curSet = set()
+                i += 1
+            elif processedExp[i] == "}":
                 # example: does not push stack immediately after popping out {{{c}b}a}
-
-                if token:
-                    curSet.add(token)
-                    token = ""
-
                 while typeStack and typeStack[-1] == ",":
                     # example: {a,b,d,c}
                     # example: will need to curSet all ones pushed inside due to comma: {b{a,b,d,c}c}
-                    top = expressionStack.pop()
-                    curSet = curSet.union(top)
+                    curSet = curSet.union(expressionStack[-1])
+                    expressionStack.pop()
                     typeStack.pop()
-
-                while typeStack and typeStack[-1] == "{":
-                    top = expressionStack.pop()
+                if typeStack and typeStack[-1] == "{":
+                    curSet = multiply(expressionStack[-1], curSet)
                     typeStack.pop()
-                    curSet = multiply(top, curSet)
-
+                    expressionStack.pop()
+                i += 1
             else:
-                # {{{c}b}abcc}
-                token += expression[i]
+                # {{{c}b}{a}{b}{c}{c}}
+                end = i + 1
+                while end < len(processedExp) and processedExp[end].isalpha():
+                    end += 1
+                curSet = {processedExp[i: end]}
+                i = end
 
         return list(sorted(curSet)) if curSet else []
 
     def test_SimpleCase(self):
         # only comma
-        print(self.braceExpansionII("{a,b,c}"))
+        # print(self.braceExpansionII("{a,b,c}"))
 
         # curly + comma
         print(self.braceExpansionII("{a{b,c}d}"))
 
         # duplicate from comma
-        print(self.braceExpansionII(""))
+        # print(self.braceExpansionII(""))
 
         # curly + curly
-        print(self.braceExpansionII("{a,b}{c,d}"))
+        # print(self.braceExpansionII("{a,b}{c,d}"))
 
         # only curly braces
-        print(self.braceExpansionII("{ab{{de}c}}"))
+        # print(self.braceExpansionII("{ab{{de}c}}"))
 
     @unittest.skip
     def test_Leetcode(self):
