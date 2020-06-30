9b7fe9f82ebf9aa3b26ba03a22871ed9b310947a
Shijie Zhang <ericzhangworking@gmail.com>
2020-06-29 23:19:15 -0700

    Update SearchInRotatedSortedArrayII.py

--- Python/BinarySearch/SearchInRotatedSortedArrayII.py
+++ Python/BinarySearch/SearchInRotatedSortedArrayII.py
@@ -8,48 +8,32 @@ from typing import List
 class SearchInRotatedSortedArrayII(unittest.TestCase):
 
     def search(self, nums: List[int], target: int) -> bool:
-        def findPeak(nums: List[int]) -> int:
-            if len(nums) == 0:
-                return -1
-
-            low = 0
-            high = len(nums) - 1
-            while low + 1 < high:
-                mid = (high - low) // 2 + low
-                if nums[mid] == nums[low]:
-                    low += 1
-                elif nums[mid] > nums[low]:
-                    low = mid
-                else: # nums[mid] > nums[high]
-                    high = mid
-
-            return low if nums[low] > nums[high] else high
-
-        def findTarget(nums: List[int], target: int, low: int, high: int) -> bool:
-            if len(nums) == 0 or low > high:
-                return False
-
-            while low + 1 < high:
-                mid = (high - low) // 2 + low
-                if nums[mid] == target:
-                    return mid
-                elif nums[mid] > target:
-                    high = mid
-                else:
-                    low = mid
-
-            if nums[low] == target or nums[high] == target:
-                return True
-            else:
-                return False
-
         if len(nums) == 0:
             return False
-        peakIndex = findPeak(nums)
-        if target >= nums[0]:
-            return findTarget(nums, target, 0, peakIndex)
+
+        left = 0
+        right = len(nums) - 1
+        while left + 1 < right:
+            mid = (right - left) // 2 + left
+            if nums[mid] == nums[left]:
+                left += 1
+            elif nums[mid] > nums[left]:
+                if target >= nums[left] and target <= nums[mid]:
+                    right = mid
+                else:
+                    left = mid
+            else:
+                if target >= nums[mid] and target <= nums[right]:
+                    left = mid
+                else:
+                    right = mid
+
+        if nums[left] == target:
+            return True
+        elif nums[right] == target:
+            return True
         else:
-            return findTarget(nums, target, peakIndex + 1, len(nums) - 1)
+            return False
 
     @unittest.skip
     def test_Leetcode(self):
