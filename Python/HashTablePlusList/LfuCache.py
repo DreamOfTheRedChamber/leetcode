db62b053f6fbac8225b052a68df6891855102743
Eric Zhang <ericzhangworking@gmail.com>
2020-07-16 16:54:58 -0700

    first refactor

--- Python/HashTablePlusList/LfuCache.py
+++ Python/HashTablePlusList/LfuCache.py
@@ -13,7 +13,7 @@ class LFUCache:
         """
         :type capacity: int
         """
-        self.keyToValFreq = {}  # K: key, V: (val, freq)
+        self.keyToFreq = {}  # K: key, V: (val, freq)
         # E.g. { freq 1 : {2 : None, 4 : None, 6: None}, freq 2 : {1 : None, 3 : None, 5 : None} }
         self.freqToKeyValue = defaultdict(OrderedDict)  # K: freq, V: {key : None}
         self.capacity = capacity  # self.capacity does not change
@@ -24,15 +24,15 @@ class LFUCache:
         :type key: int
         :rtype: int
         """
-        if key not in self.keyToValFreq:
+        if key not in self.keyToFreq:
             return -1
-        val, freq = self.keyToValFreq.pop(key)
-        self.freqToKeyValue[freq].pop(key)
+        freq = self.keyToFreq.pop(key)
+        value = self.freqToKeyValue[freq].pop(key)
         if len(self.freqToKeyValue[freq]) == 0 and freq == self.minFreq:
             self.minFreq = freq + 1
-        self.freqToKeyValue[freq + 1][key] = None
-        self.keyToValFreq[key] = (val, freq + 1)
-        return val
+        self.freqToKeyValue[freq + 1][key] = value
+        self.keyToFreq[key] = freq + 1
+        return value
 
     def put(self, key, value):
         """
@@ -44,17 +44,17 @@ class LFUCache:
         if self.capacity <= 0:
             return
 
-        if key in self.keyToValFreq:
+        if key in self.keyToFreq:
             self.get(key)  # Update the key freq in dict
-            self.keyToValFreq[key] = (value, self.keyToValFreq[key][1])
+            self.freqToKeyValue[self.keyToFreq[key]][key] = value
             return
 
-        if self.capacity <= len(self.keyToValFreq):
+        if self.capacity <= len(self.keyToFreq):
             delKey, _ = self.freqToKeyValue[self.minFreq].popitem(last=False)  # pop first
-            self.keyToValFreq.pop(delKey)
+            self.keyToFreq.pop(delKey)
 
-        self.keyToValFreq[key] = (value, 1)
-        self.freqToKeyValue[1][key] = None
+        self.keyToFreq[key] = 1
+        self.freqToKeyValue[1][key] = value
         self.minFreq = 1
 
 # Your LFUCache object will be instantiated and called as such:
@@ -64,6 +64,7 @@ class LFUCache:
 
 class LFUCacheTest(unittest.TestCase):
 
+    @unittest.skip
     def test_Leetcode(self):
  #       ["LFUCache", "put", "put", "get", "put", "get", "get", "put", "get", "get", "get"]
  #       [[2], [1, 1], [2, 2], [1], [3, 3], [2], [3], [4, 4], [1], [3], [4]]
@@ -75,6 +76,9 @@ class LFUCacheTest(unittest.TestCase):
         self.assertEqual(-1, lfuCache.get(2))
         self.assertEqual(3, lfuCache.get(3))
         lfuCache.put(4,4)
+        self.assertEqual(-1, lfuCache.get(1))
+        self.assertEqual(3, lfuCache.get(3))
+        self.assertEqual(4, lfuCache.get(4))
 
     @unittest.skip
     def test_ErrorCase(self):
@@ -84,6 +88,7 @@ class LFUCacheTest(unittest.TestCase):
         lfuCache.put(0,0)
         lfuCache.get(0)
 
+    @unittest.skip
     def test_Errorcase2(self):
         lfuCache = LFUCache(10)
         lfuCache.put(10,13)
@@ -95,6 +100,16 @@ class LFUCacheTest(unittest.TestCase):
         lfuCache.put(2,19)
         lfuCache.get(2)
         lfuCache.get(3)
+
+    def test_Errorcase3(self):
+        # ["LFUCache", "put", "put", "put", "put", "get"]
+        # [[2], [3, 1], [2, 1], [2, 2], [4, 4], [2]]
+        lfuCache = LFUCache(2)
+        lfuCache.put(3,1)
+        lfuCache.put(2,1)
+        lfuCache.put(2,2)
+        lfuCache.put(4,4)
+        lfuCache.get(2)
 
 if __name__ == '__main__':
     unittest.main()
