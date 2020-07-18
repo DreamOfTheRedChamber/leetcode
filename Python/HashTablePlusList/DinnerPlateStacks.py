73c10a1206648d60cb2d37200c70b7d49224dff9
Eric Zhang <ericzhangworking@gmail.com>
2020-07-18 01:06:14 -0700

    second implementation

--- Python/HashTablePlusList/DinnerPlateStacks.py
+++ Python/HashTablePlusList/DinnerPlateStacks.py
@@ -25,9 +25,17 @@ class DinnerPlates:
                 heapq.heappush(self.pq, len(self.stacks)-1)
         else:
             index = heapq.heappop(self.pq)
-            self.stacks[index].append(val)
+            while len(self.stacks[index]) == self.capacity and self.pq:
+                index = heapq.heappop(self.pq)
+
             if len(self.stacks[index]) < self.capacity:
-                heapq.heappush(self.pq, index)
+                self.stacks[index].append(val)
+                if len(self.stacks[index]) < self.capacity:
+                    heapq.heappush(self.pq, index)
+            else:
+                self.stacks.append([val])
+                if self.capacity > 1:
+                    heapq.heappush(self.pq, len(self.stacks) - 1)
         return
 
     def pop(self) -> int:
@@ -68,6 +76,42 @@ class DinnerPlateStacks(unittest.TestCase):
         self.assertEqual(2, dinnerPlates.popAtStack(1))
         self.assertEqual(3, dinnerPlates.pop())
         self.assertEqual(1, dinnerPlates.pop())
+        return
+
+    def test_WrongAnswer2(self):
+        # ["DinnerPlates", "push", "push", "push", "push", "push", "push", "push", "push", "popAtStack", "popAtStack",
+        # "popAtStack", "popAtStack", "push", "push", "push", "push", "push", "push", "push", "push", "pop", "pop",
+        # "pop", "pop"]
+        # [[2], [471], [177], [1], [29], [333], [154], [130], [333], [1], [0], [2], [0], [165], [383], [267], [367], [53],
+        # [373], [388], [249], [], [], [], []]
+        # expected: [null,null,null,null,null,null,null,null,null,29,177,154,471,null,null,null,null,null,null,null,null,249,388,373,53]
+        # output:   [null,null,null,null,null,null,null,null,null,29,177,154,471,null,null,null,null,null,null,null,null,249,388,373,333]
+        dinnerPlates = DinnerPlates(2)
+        dinnerPlates.push(471)
+        dinnerPlates.push(177)
+        dinnerPlates.push(1)
+        dinnerPlates.push(29)
+        dinnerPlates.push(333)
+        dinnerPlates.push(154)
+        dinnerPlates.push(130)
+        dinnerPlates.push(333)
+        self.assertEqual(29, dinnerPlates.popAtStack(1))
+        self.assertEqual(177, dinnerPlates.popAtStack(0))
+        self.assertEqual(154, dinnerPlates.popAtStack(2))
+        self.assertEqual(471, dinnerPlates.popAtStack(0))
+        dinnerPlates.push(165)
+        dinnerPlates.push(383)
+        dinnerPlates.push(267)
+        dinnerPlates.push(367)
+        dinnerPlates.push(53)
+        dinnerPlates.push(373)
+        dinnerPlates.push(388)
+        dinnerPlates.push(249)
+        self.assertEqual(249, dinnerPlates.pop())
+        self.assertEqual(388, dinnerPlates.pop())
+        self.assertEqual(373, dinnerPlates.pop())
+        self.assertEqual(53, dinnerPlates.pop())
+
         return
 
 if __name__ == '__main__':
