6dcf100435856d93976f4be465c6e0f8dd19e543
Eric Zhang <ericzhangworking@gmail.com>
2020-07-18 00:27:43 -0700

    Update DinnerPlateStacks.py

--- Python/HashTablePlusList/DinnerPlateStacks.py
+++ Python/HashTablePlusList/DinnerPlateStacks.py
@@ -18,25 +18,31 @@ class DinnerPlates:
     def push(self, val: int) -> None:
         if not self.pq:
             self.stacks.append([val])
-            heapq.heappush(self.pq, (len(self.stacks)-1, 1))
+            heapq.heappush(self.pq, len(self.stacks)-1)
         else:
-            index, length = heapq.heappop(self.pq)
+            index = heapq.heappop(self.pq)
             self.stacks[index].append(val)
             if len(self.stacks[index]) == 1:
-                heapq.heappush(self.pq, (index, 1))
+                heapq.heappush(self.pq, index)
         return
 
     def pop(self) -> int:
+        resultIndex = len(self.stacks) - 1
+        while resultIndex >= 0 and len(self.stacks[resultIndex]) == 0:
+            resultIndex -= 1
 
-        return 0
+        result = self.popAtStack(resultIndex)
+        return result
 
     def popAtStack(self, index: int) -> int:
         if index >= len(self.stacks):
             return -1
 
         if len(self.stacks[index]) > 0:
-
-            return self.stacks[index][-1]
+            result = self.stacks[index].pop()
+            if len(self.stacks[index]) <= 1:
+                heapq.heappush(self.pq, index)
+            return result
 
         return -1
 
