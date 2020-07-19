0db476dbee33397613b3b5a6f344efebcaf6c7ab
Shijie Zhang <ericzhangworking@gmail.com>
2020-07-18 18:22:04 -0700

    Update DesignTwitter.py

--- Python/HashTablePlusList/DesignTwitter.py
+++ Python/HashTablePlusList/DesignTwitter.py
@@ -1,10 +1,14 @@
 # Definition for a binary tree node.
+import collections
 import heapq
+import time
 import unittest
 
 # Read about enumerate in python
 from collections import defaultdict
 from typing import List
+
+# please see https://leetcode.com/problems/design-twitter/discuss/714702/python-solution%3A-simplest-data-structure
 
 class Twitter:
 
@@ -12,61 +16,56 @@ class Twitter:
         """
         Initialize your data structure here.
         """
-        self.userToFollowees = defaultdict(set)
-        self.userToTweets = defaultdict(list)
-        self.timeStamp = 1
+        self.twitter = collections.defaultdict(list)
+        self.following = collections.defaultdict(set)
 
     def postTweet(self, userId: int, tweetId: int) -> None:
         """
         Compose a new tweet.
         """
-        if userId not in self.userToFollowees:
-            self.userToFollowees[userId].add(tweetId)
-
-        self.userToTweets[userId].append((self.timeStamp, tweetId))
-        return
+        self.twitter[userId].append((tweetId, time.time()))
 
     def getNewsFeed(self, userId: int) -> List[int]:
         """
         Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent.
         """
-        pq = []
-        for followee in self.userToFollowees[userId]:
-            for tweets in self.userToTweets[followee]:
-                for i in range(-1, -9, -1):
-                    if len(pq) <= 10:
-                        heapq.heappush(pq, tweets[-i])
-                    else:
-                        heapq.heappush(pq, tweets[-i])
-                        heapq.heappop(pq)
+        allNews = []
+        for tweets in self.twitter[userId]:
+            allNews.append(tweets)
 
-        result = []
-        while pq:
-            result.append(heapq.heappop(pq)[1])
-        return result
+        for followingId in self.following[userId]:
+            if followingId == userId: continue
+            for tweets in self.twitter[followingId]:
+                allNews.append(tweets)
+
+        allNews.sort(key=lambda x: x[1], reverse=True)
+
+        return list(map(lambda x: x[0], allNews))[:10]
 
     def follow(self, followerId: int, followeeId: int) -> None:
         """
         Follower follows a followee. If the operation is invalid, it should be a no-op.
         """
-        if followeeId == followerId:
-            return
-        self.userToFollowees[followerId].add(followeeId)
-        return
+        self.following[followerId].add(followeeId)
 
     def unfollow(self, followerId: int, followeeId: int) -> None:
         """
-        Follow
+        Follower unfollows a followee. If the operation is invalid, it should be a no-op.
         """
-        if followeeId in self.userToFollowees[followerId]:
-            self.userToFollowees[followerId].remove(followeeId)
-
-        return
+        if followeeId in self.following[followerId]:
+            self.following[followerId].remove(followeeId)
 
 class DesignTwitter(unittest.TestCase):
 
     def test_Leetcode(self):
-        print([1])
+        twitter = Twitter()
+        twitter.postTweet(1, 5)
+        twitter.getNewsFeed(1)
+        twitter.follow(1, 2)
+        twitter.postTweet(2, 6)
+        twitter.getNewsFeed(1)
+        twitter.unfollow(1, 2)
+        twitter.getNewsFeed(1)
 
 if __name__ == '__main__':
     unittest.main()
