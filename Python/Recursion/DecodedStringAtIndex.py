7e7ae94556d9ef646f6669c4cbf25a44a019e40d
Eric Zhang <ericzhangworking@gmail.com>
2020-07-22 12:53:19 -0700

    Update DecodedStringAtIndex.py

--- Python/Recursion/DecodedStringAtIndex.py
+++ Python/Recursion/DecodedStringAtIndex.py
@@ -12,28 +12,21 @@ class DecodedStringAtIndex(unittest.TestCase):
 
         count = 0
         i = 0
-        curr = ""
         while i < len(S):
-            if count == K:
-                return S[i-1]
-            elif S[i].isalpha():
-                curr += S[i]
-                i += 1
+            if S[i].isalpha():
                 count += 1
-            elif S[i].isdigit():
-                j = i
-                while j < len(S) and S[j].isdigit():
-                    j += 1
-                number = int(S[i:j])
-                multiplyLength = i * number
-                if multiplyLength > K:
-                    return self.decodeAtIndex(S[:i], K % i)
-                elif multiplyLength == K:
-                    return curr[-1]
-                else:
-                    curr = curr * number
-                    i = j
-                    count = multiplyLength
+                if count == K:
+                    return S[i]
+                i += 1
+            else: # S[i].isdigit():
+                number = int(S[i])
+                if count * number < K:
+                    i += 1
+                    count = count * number
+                elif K % count == 0:
+                    return self.decodeAtIndex(S[:i], count)
+                else: # if K % multiplyLength == 0:
+                    return self.decodeAtIndex(S[:i], K % count)
 
         return ""
 
@@ -45,6 +38,26 @@ class DecodedStringAtIndex(unittest.TestCase):
     def test_Edgecase(self):
         self.assertEqual("t", self.decodeAtIndex("leet2code3", 8))
         return
+
+    def test_WrongAnswer(self):
+        self.assertEqual("c", self.decodeAtIndex("abc", 3))
+
+    def test_WrongAnswer2(self):
+        self.assertEqual("c", self.decodeAtIndex("a2b3c4d5", 40))
+
+    def test_WrongAnswer3(self):
+        self.assertEqual("x", self.decodeAtIndex("ixm5xmgo78", 711))
+
+    def test_WrongAnswer4(self):
+        self.assertEqual("z", self.decodeAtIndex("vzpp636m2y", 2920))
+
+    @unittest.skip
+    def test_Leetcode5(self):
+        self.assertEqual("a", self.decodeAtIndex("abcde2", 6))
+        self.assertEqual("b", self.decodeAtIndex("abcde2", 7))
+        self.assertEqual("c", self.decodeAtIndex("abcde2", 8))
+        self.assertEqual("d", self.decodeAtIndex("abcde2", 9))
+        self.assertEqual("e", self.decodeAtIndex("abcde2", 10))
 
 if __name__ == '__main__':
     unittest.main()
