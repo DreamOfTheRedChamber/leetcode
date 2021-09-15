82360060623a73c2c0ae39dc280c6e14ccc66fd9
Shijie Zhang <ericzhangworking@gmail.com>
2021-09-15 08:32:40 -0700

    add namedtuple

---
+++ Python/TestDataStructureTuple.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+import re
+import unittest
+
+# Read about enumerate in python
+from collections import namedtuple
+
+
+class TestDataStructuresTuple(unittest.TestCase):
+
+    @unittest.skip
+    def test_CreateNamedTuple(self):
+        Point = namedtuple("Point", "x y")
+        point = Point(2, 4)
+        point
+        
+if __name__ == '__main__':
+    unittest.main()
