f88b40e087723d48bd773b7c1c9fc51609aeddbc
Shijie Zhang <ericzhangworking@gmail.com>
2021-10-21 20:50:10 -0700

    Update tree.py

--- company-specific/tree.py
+++ company-specific/tree.py
@@ -89,7 +89,41 @@ class Tree(unittest.TestCase):
 
     def compute_diff(self, root1: TreeNode, root2: TreeNode) -> int:
 
-        return 0
+        def countNodes(node: TreeNode) -> int:
+            if node is None:
+                return 0
+            else:
+                result = 1
+                for child in node.children:
+                    result += countNodes(child)
+                return result
+
+        if root1 is None:
+            return countNodes(root2)
+        elif root2 is None:
+            return countNodes(root1)
+        elif root1.key != root2.key:
+            return countNodes(root1) + countNodes(root2)
+        else:
+            result = 0
+            if root1.val != root2.val:
+                result += 1
+
+            root1Children = {x.key: x for x in root1.children}
+            root2Children = {x.key: x for x in root2.children}
+            for key, value in root1Children.items():
+                if key in root2Children:
+                    result = result + self.compute_diff(root1Children[key], root2Children[key])
+
+            for key, value in root1Children.items():
+                if key not in root2Children:
+                    result = result + countNodes(root1Children[key])
+
+            for key, value in root2Children.items():
+                if key not in root1Children:
+                    result = result + countNodes(root2Children[key])
+
+            return result
 
 if __name__ == '__main__':
     unittest.main()
