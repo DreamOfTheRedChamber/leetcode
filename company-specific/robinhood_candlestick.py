8c34c82c4a94d534239322c57ec5d6b3c02504d5
Shijie Zhang <ericzhangworking@gmail.com>
2021-12-13 01:42:34 -0800

    Update robinhood_candlestick.py

--- company-specific/robinhood_candlestick.py
+++ company-specific/robinhood_candlestick.py
@@ -9,10 +9,49 @@ from sortedcontainers import SortedList
 
 class CandleStick(unittest.TestCase):
 
-    def test_NoMatch(self):
-        return
+    def calcCandleStick(self, input: str) -> List[List[int]]:
+        class Interval:
 
-    def test_MultiMatchWithSameDis(self):
+            def __init__(self):
+                self.maxV = float('-inf')
+                self.minV = float('inf')
+                self.firstPrice = (float('inf'), 0)
+                self.lastPrice = (-float('inf'), 0)
+
+            def add(self, timestamp: int, price: int):
+                self.maxV = max(self.maxV, price)
+                self.minV = min(self.minV, price)
+                if timestamp < self.firstPrice[0]:
+                    self.firstPrice = (timestamp, price)
+                if timestamp > self.lastPrice[0]:
+                    self.lastPrice = (timestamp, price)
+
+        priceTsPairs = input.split(",")
+        startToInterval = defaultdict(lambda: Interval())
+
+        for pair in priceTsPairs:
+            price, time = pair.split(":")
+            price = int(price)
+            time = int(time)
+            floorS = time // 10
+            startToInterval[floorS].add(time, price)
+
+        start = min(startToInterval.keys())
+        end = max(startToInterval.keys())
+        result = []
+        lastPrice = None
+        for i in range(start, end + 1):
+            if i in startToInterval:
+                lastPrice = startToInterval[i].lastPrice[1]
+                result.append([i * 10, startToInterval[i].maxV, startToInterval[i].minV, startToInterval[i].firstPrice[1], startToInterval[i].lastPrice[1]])
+            else:
+                result.append([i * 10, lastPrice, lastPrice, lastPrice, lastPrice])
+
+        return result
+
+    def test_CandleStick(self):
+        print(self.calcCandleStick("1:2,3:4,9:8,5:10,13:18,34:32,55:44"))
+
         return
 
 if __name__ == '__main__':
