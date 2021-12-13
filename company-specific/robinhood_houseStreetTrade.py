e9e0cd06f8210b8efee8206123e9eba5e0d91383
Shijie Zhang <ericzhangworking@gmail.com>
2021-12-12 20:57:30 -0800

    Create robinhood_houseStreetTrade.py

---
+++ company-specific/robinhood_houseStreetTrade.py
@@ -0,0 +1,38 @@
+# Definition for a binary tree node.
+import bisect
+import collections
+import unittest
+from collections import defaultdict
+from typing import List
+
+class HouseStreetTrade(unittest.TestCase):
+
+    def findUnmatchedExactTrades(self, houseTrades: List[str], streetTrades: List[str]) -> List[str]:
+        houseHisto = collections.Counter(houseTrades)
+
+        result = []
+        for trade in streetTrades:
+            if trade in houseHisto:
+                houseHisto[trade] -= 1
+                if houseHisto[trade] == 0:
+                    del houseHisto[trade]
+            else:
+                result.append(trade)
+
+        for key, value in houseHisto.items():
+            result.append(key)
+
+        return sorted(result)
+
+    def test_FindExactMatch(self):
+        houseTrades = ["AAPL,B,0100,ABC123", "AAPL,B,0100,ABC123", "GOOG,S,0050,CDC333"]
+        streetTrades = ["  FB,B,0100,GBGGGG", "AAPL,B,0100,ABC123"]
+        print(self.findUnmatchedExactTrades(houseTrades, streetTrades))
+        return
+
+    @unittest.skip
+    def test_MultiMatchWithSameDis(self):
+        return
+
+if __name__ == '__main__':
+    unittest.main()
