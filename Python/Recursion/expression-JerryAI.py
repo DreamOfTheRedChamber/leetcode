0ce182810cc8f7b0c057385b0c03154a38963306
@EricSmile <ericzhangworking@gmail.com>
2024-02-20 14:03:48 +0800

    resolve the bug

--- Python/Recursion/expression-JerryAI.py
+++ Python/Recursion/expression-JerryAI.py
@@ -2,6 +2,14 @@
 import unittest
 from typing import List
 
+""""
+题目是给一串数字（0 - 9）每个数字之间可以加 + - 号或者不加，组成的表达式计算结果等于 给定的目标数，输出所有满足条件的表达式。
+例如： [1 2 3 4 5 6 7 8 9],目标 100
+可能的组合：
+1 + 23 - 4 + 56 + 7 + 8 + 9
+-1 - 2 + 34 - 5 - 6 + 78 + 9
+"""
+
 class ExpressionDfs(unittest.TestCase):
 
     def test_Normal(self):
@@ -18,29 +26,24 @@ class ExpressionDfs(unittest.TestCase):
                     result.append(currStr)
                 return
 
-            if source[pos] == "0":
-                dfs(result, pos+1, source, target, currStr + "+" + source[pos], currValue)
-                dfs(result, pos+1, source, target, currStr + "-" + source[pos], currValue)
-                return
-
             for i in range(pos, len(source)):
                 numStr = source[pos: i+1]
                 num = int(numStr)
 
+                if len(numStr) > 1 and numStr[0] == "0":
+                    continue
+
                 if len(currStr) == 0:
                     dfs(result, i + 1, source, target, numStr, num)
-                    dfs(result, i + 1, source, target, "-" + numStr, - num)
                 else:
                     dfs(result, i + 1, source, target, currStr + "+" + numStr, currValue + num)
                     dfs(result, i + 1, source, target, currStr + "-" + numStr, currValue - num)
 
             return
 
-        result = []
-        dfs(result, 0, input, target, "", 0)
-        return result
-
-
+        expressions = []
+        dfs(expressions, 0, input, target, "", 0)
+        return expressions
 
 if __name__ == '__main__':
     unittest.main()
