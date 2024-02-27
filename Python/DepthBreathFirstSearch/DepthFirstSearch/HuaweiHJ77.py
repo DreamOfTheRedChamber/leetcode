ba55adcdd923ba78fdba88743a6956ac904c2564
@EricSmile <ericzhangworking@gmail.com>
2024-02-27 11:59:41 +0800

    initial debugging

--- Python/DepthBreathFirstSearch/DepthFirstSearch/HuaweiHJ77.py
+++ Python/DepthBreathFirstSearch/DepthFirstSearch/HuaweiHJ77.py
@@ -1,3 +1,6 @@
+
+# https://www.nowcoder.com/practice/97ba57c35e9f4749826dc3befaeae109?tpId=37&tqId=21300&rp=1&ru=/exam/oj/ta&qru=/exam/oj/ta&sourceUrl=%2Fexam%2Foj%2Fta%3FtpId%3D37&difficulty=undefined&judgeStatus=undefined&tags=&title=hj77
+
 import sys
 from typing import List
 
@@ -17,6 +20,14 @@ def dfs(trains: List[int], pos: int, stack: List[int], oneResult: List[int], all
     dfs(trains, pos + 1, stack, oneResult, allResults)
     oneResult.pop()
 
+"""
+numTrains = int(input())
+trains = []
+for i in range(numTrains):
+    trains = list(map(int, input().split(" ")))
+"""
+
+"""
 count = 0
 numTrains = 0
 trains = []
@@ -25,8 +36,12 @@ for line in sys.stdin:
         numTrains = int(line.split()[0])
     if count == 1:
         trains = list(map(int, line.split(" ")))
+    if count == 2:
+        break
+    count += 1
+"""
 
 allResults = []
-dfs(trains, 0, [], [], allResults)
+dfs([1, 2, 3], 0, [], [], allResults)
 for item in allResults:
     print(item)
