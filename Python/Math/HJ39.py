cf83a33f9a150033558d3301e0c79a676cc35d4b
@EricSmile <ericzhangworking@gmail.com>
2024-03-08 17:12:12 +0800

    passed implementation

--- Python/Math/HJ39.py
+++ Python/Math/HJ39.py
@@ -1,8 +1,13 @@
 from typing import List
 
-def isMaskValid(input: str) -> bool:
+def isMaskValid(input: List[int]) -> bool:
+    for seg in input:
+        if not 0 <= seg < 256:
+            return False
+
+    maskBin = decToBin(input)
     flag = False
-    for char in input:
+    for char in maskBin:
         if char == '0':
             flag = True
         if char == '1' and flag:
@@ -46,7 +51,7 @@ def calculate(mask: str, ip1: str, ip2: str) -> int:
     ip1Bin = decToBin(ip1)
     ip2Bin = decToBin(ip2)
 
-    if not isMaskValid(maskBin) or not isIpValid(ip1) or not isIpValid(ip2):
+    if not isMaskValid(mask) or not isIpValid(ip1) or not isIpValid(ip2):
         return 1
     else:
         result1 = binAnd(maskBin, ip1Bin)
@@ -62,7 +67,6 @@ ip1 = input().strip()
 ip2 = input().strip()
 
 """
-
 
 mask = "255.255.255.0"
 ip1 = "192.168.224.256"
@@ -83,3 +87,13 @@ mask = "255.0.0.0"
 ip1 = "92.24.244.54"
 ip2 = "146.46.253.71"
 print(calculate(mask, ip1, ip2)) # 2
+
+mask = "-255.0.0.0"
+ip1 = "119.70.215.88"
+ip2 = "227.120.96.170"
+print(calculate(mask, ip1, ip2)) # 1
+
+mask = "255.256.0.0"
+ip1 = "130.32.67.107"
+ip2 = "143.32.132.184"
+print(calculate(mask, ip1, ip2)) # 1
