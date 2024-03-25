b1ac6f8f319cd68b3fabcf83f0c6f5b0776afc4b
@EricSmile <ericzhangworking@gmail.com>
2024-03-25 11:27:03 +0800

    initial debugging still wrong

--- Python/Hashmap/InvalidTransactions1169.py
+++ Python/Hashmap/InvalidTransactions1169.py
@@ -10,6 +10,7 @@ class InvalidTransactions(unittest.TestCase):
 
     def invalidTransactions(self, transactions: List[str]) -> List[str]:
         invalid = []
+        existingDup = set()
         mapping = defaultdict(list)
 
         for tran in transactions:
@@ -18,21 +19,45 @@ class InvalidTransactions(unittest.TestCase):
             if int(amount) > 1000:
                 invalid.append(tran)
 
-            if name in mapping:
+            elif name in mapping:
+                isInvalid = False
                 for existingTime, existingAmount, existingCity in mapping[name]:
                     if abs(existingTime - int(time)) <= 60 and existingCity != city:
                         existingEntry = name + ',' + str(existingTime) + ',' + str(existingAmount) + ',' + existingCity
-                        invalid.append(existingEntry)
-                        invalid.append(tran)
+                        existingDup.add(existingEntry)
+                        isInvalid = True
+
+                if isInvalid:
+                    invalid.append(tran)
 
             mapping[name].append((int(time), int(amount), city))
 
+        for entry in existingDup:
+            invalid.append(entry)
+
         return invalid
 
+    @unittest.skip
     def test_example1(self):
-        transactions = ["alice,20,800,mtv","alice,50,100,beijing"]
-        # ["alice,20,800,mtv","alice,50,100,beijing"]
+        transactions = ["alice,20,800,mtv", "alice,50,100,beijing"]
         result = self.invalidTransactions(transactions)
+
+        # ["alice,20,800,mtv","alice,50,100,beijing"]
+        print(result)
+
+    def test_example2(self):
+        transactions = ["alice,20,800,mtv", "alice,50,100,mtv", "alice,51,100,frankfurt"]
+        result = self.invalidTransactions(transactions)
+
+        # ["alice,20,800,mtv","alice,50,100,mtv","alice,51,100,frankfurt"]
+        print(result)
+
+    @unittest.skip
+    def test_example3(self):
+        transactions = ["alice,20,1220,mtv", "alice,20,1220,mtv"]
+        result = self.invalidTransactions(transactions)
+
+        # ["alice,20,1220,mtv", "alice,20,1220,mtv"]
         print(result)
 
 if __name__ == '__main__':
