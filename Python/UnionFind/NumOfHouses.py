e24e88f934e525689a9c8d9e30f81d3d10f027d7
@EricSmile <ericzhangworking@gmail.com>
2024-03-29 15:23:21 +0800

    implement the dfs solution

--- Python/UnionFind/NumOfHouses.py
+++ Python/UnionFind/NumOfHouses.py
@@ -28,6 +28,33 @@ class UnionFind(object):
 
 class NumOfHouses(unittest.TestCase):
 
+    def dfs(self, xStart: int, yStart: int, grid: List[List[int]], visited: List[List[bool]]):
+        if grid[xStart][yStart] == 0 or visited[xStart][yStart]:
+            return
+
+        visited[xStart][yStart] = True
+        numRows = len(grid)
+        numCols = len(grid[0])
+        dirs = [[0, 1], [1, 0], [-1, 0], [0, -1]]
+        for xDir, yDir in dirs:
+            xNeigh = xDir + xStart
+            yNeigh = yDir + yStart
+            if 0 <= xNeigh < numRows and 0 <= yNeigh < numCols and grid[xNeigh][yNeigh] == 1:
+                self.dfs(xNeigh, yNeigh, grid, visited)
+
+
+    def countHousesDFS(self, grid: List[List[int]]) -> int:
+        numRows = len(grid)
+        numCols = len(grid[0])
+        visited = [[False for j in range(numCols)] for i in range(numRows)]
+        numComp = 0
+        for i in range(numRows):
+            for j in range(numCols):
+                if grid[i][j] == 1 and not visited[i][j]:
+                    self.dfs(i, j, grid, visited)
+                    numComp += 1
+        return numComp
+
     def countHouses(self, grid: List[List[int]]) -> int:
         numRows = len(grid)
         numCols = len(grid[0])
@@ -36,14 +63,14 @@ class NumOfHouses(unittest.TestCase):
 
         for i in range(numRows):
             for j in range(numCols):
-                index = i * numRows + j
+                index = i * numCols + j
                 if grid[i][j] != 1:
                     continue
 
                 for xDir, yDir in dirs:
                     xNeigh = i + xDir
                     yNeigh = j + yDir
-                    neighIndex = xNeigh * numRows + yNeigh
+                    neighIndex = xNeigh * numCols + yNeigh
                     if 0 <= xNeigh < numRows and 0 <= yNeigh < numCols:
                         if grid[xNeigh][yNeigh] != 1:
                             continue
@@ -53,13 +80,16 @@ class NumOfHouses(unittest.TestCase):
         for i in range(numRows):
             for j in range(numCols):
                 index = i * numRows + j
-                numHouses.add(uf.find(index))
+                if grid[i][j] == 1:
+                    root = uf.find(index)
+                    print(root)
+                    numHouses.add(root)
 
         return len(numHouses)
 
     def test_example1(self):
         grid = [[0, 1, 0, 1, 1], [0, 1, 0, 0, 0], [1, 0, 0, 1, 0], [0, 0, 0, 1, 1]]
-        self.assertEqual(4, self.countHouses(grid))
+        self.assertEqual(4, self.countHousesDFS(grid))
 
 if __name__ == '__main__':
     unittest.main()
