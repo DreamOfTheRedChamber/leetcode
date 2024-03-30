eb76f1ea7c123689eed1a8c531221865de6f76bd
@EricSmile <ericzhangworking@gmail.com>
2024-03-30 10:51:50 +0800

    second impl with bugs

--- Python/DFS/CountSubIslands1905.py
+++ Python/DFS/CountSubIslands1905.py
@@ -8,16 +8,22 @@ from typing import List
 
 class CountSubIslands(unittest.TestCase):
 
-    def dfs(self, xCor: int, yCor: int, visited: List[List[bool]], grid1: List[List[int]], grid2: List[List[int]]):
+    def dfs(self, xCor: int, yCor: int, visited: List[List[bool]], grid1: List[List[int]], grid2: List[List[int]]) -> bool:
+        if grid1[xCor][yCor] == 0:
+            return False
+
         visited[xCor][yCor] = True
         height = len(grid2)
         width = len(grid2[0])
 
+        result = True
         dirs = [[0, 1], [1, 0], [-1, 0], [0, -1]]
         for xDir, yDir in dirs:
             xNeigh, yNeigh = xDir + xCor, yDir + yCor
-            if 0 <= xNeigh < height and 0 <= yNeigh < width and not visited[xNeigh][yNeigh] and grid2[xNeigh][yNeigh] == 1 and grid1[xNeigh][yNeigh] == 1:
-                self.dfs(xNeigh, yNeigh, visited, grid1, grid2)
+            if 0 <= xNeigh < height and 0 <= yNeigh < width and not visited[xNeigh][yNeigh] and grid2[xNeigh][yNeigh] == 1:
+                result = result and self.dfs(xNeigh, yNeigh, visited, grid1, grid2)
+
+        return result
 
     def countSubIslands(self, grid1: List[List[int]], grid2: List[List[int]]) -> int:
         height = len(grid2)
@@ -27,8 +33,8 @@ class CountSubIslands(unittest.TestCase):
         for i in range(height):
             for j in range(width):
                 if not visited[i][j] and grid2[i][j] == 1 and grid1[i][j] == 1:
-                    self.dfs(i, j, visited, grid1, grid2)
-                    numComp += 1
+                    if self.dfs(i, j, visited, grid1, grid2):
+                        numComp += 1
         return numComp
 
     def test_example1(self):
