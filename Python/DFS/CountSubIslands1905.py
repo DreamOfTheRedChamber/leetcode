7fd0016af748451dadff09059b67674665b7e64c
@EricSmile <ericzhangworking@gmail.com>
2024-03-30 11:12:47 +0800

    fix the bug to avoid compiler optimization

--- Python/DFS/CountSubIslands1905.py
+++ Python/DFS/CountSubIslands1905.py
@@ -9,19 +9,22 @@ from typing import List
 class CountSubIslands(unittest.TestCase):
 
     def dfs(self, xCor: int, yCor: int, visited: List[List[bool]], grid1: List[List[int]], grid2: List[List[int]]) -> bool:
+
+        result = True
+
         if grid1[xCor][yCor] == 0:
-            return False
+            result = False
 
         visited[xCor][yCor] = True
         height = len(grid2)
         width = len(grid2[0])
 
-        result = True
         dirs = [[0, 1], [1, 0], [-1, 0], [0, -1]]
         for xDir, yDir in dirs:
             xNeigh, yNeigh = xDir + xCor, yDir + yCor
             if 0 <= xNeigh < height and 0 <= yNeigh < width and not visited[xNeigh][yNeigh] and grid2[xNeigh][yNeigh] == 1:
-                result = result and self.dfs(xNeigh, yNeigh, visited, grid1, grid2)
+                if not self.dfs(xNeigh, yNeigh, visited, grid1, grid2):
+                    result = False
 
         return result
 
@@ -46,6 +49,15 @@ class CountSubIslands(unittest.TestCase):
         grid1 = [[1, 0, 1, 0, 1], [1, 1, 1, 1, 1], [0, 0, 0, 0, 0], [1, 1, 1, 1, 1], [1, 0, 1, 0, 1]]
         grid2 = [[0, 0, 0, 0, 0], [1, 1, 1, 1, 1], [0, 1, 0, 1, 0], [0, 1, 0, 1, 0], [1, 0, 0, 0, 1]]
         self.assertEqual(2, self.countSubIslands(grid1, grid2))
+
+    def test_example3(self):
+        grid1 = [[1, 0, 0, 0, 1, 1],
+                 [1, 0, 0, 0, 1, 0],
+                 [1, 1, 1, 1, 1, 0]]
+        grid2 = [[1, 0, 0, 1, 0, 1],
+                 [1, 1, 1, 1, 1, 1],
+                 [1, 0, 0, 1, 0, 0]]
+        self.assertEqual(0, self.countSubIslands(grid1, grid2))
 
 if __name__ == '__main__':
     unittest.main()
