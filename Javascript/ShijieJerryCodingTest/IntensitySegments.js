e60c7436d1f703e3fa9748fbcc6010d82140fd33
@EricSmile <ericzhangworking@gmail.com>
2024-04-20 19:33:35 +0800

    first impl

--- Javascript/ShijieJerryCodingTest/IntensitySegments.js
+++ Javascript/ShijieJerryCodingTest/IntensitySegments.js
@@ -4,75 +4,143 @@ class IntensitySegments
 {
     constructor()
     {
-        this.segments = new OrderedMap();
+        this.skyline = new OrderedMap();
     }
 
     add(from, to, amount)
     {
-        if (!this.segments.find(from))
+        let fromIterator = this.skyline.find(from);
+        if (fromIterator.equals(this.skyline.end()))
         {
-            this.segments.setElement(from, 0);
+            let lastSmaller = this.skyline.reverseUpperBound(from);
+            if (lastSmaller.equals(this.skyline.rEnd()))
+            {
+                this.skyline.setElement(from, amount, lastSmaller);
+            }
+            else
+            {
+                let [key, value] = lastSmaller.pointer;
+                this.skyline.setElement(from, amount + value);
+            }
         }
-        if (!this.segments.find(to))
+        else
         {
-            this.segments.setElement(to, 0);
+            let [key, value] = fromIterator.pointer;
+            this.skyline.setElement(key, value + amount);
         }
 
-        let currNode = this.segments.find(from);
-        let currKey = currNode.pointer[0];
-        let currValue = currNode.pointer[1];
-        while (currKey < to)
+        let it = this.skyline.find(from).next();
+        while (!it.equals(this.skyline.end()))
         {
-            this.segments.setElement(currKey, currValue + amount);
-            currNode = currNode.next();
-            currKey = currNode.pointer[0];
-            currValue = currNode.pointer[1];
+            let [key, value] = it.pointer;
+            if ( key >= to )
+            {
+                break;
+            }
+
+            this.skyline.setElement(key, value + amount);
+            it = it.next();
         }
 
-        if (currKey === to && currValue === 0 && amount === -1)
+        if (it.equals(this.skyline.end()))
         {
-            this.segments.eraseElementByKey(to);
+            this.skyline.setElement(to, 0);
         }
+        else
+        {
+            let [key, value] = it.pointer;
+            if (it.equals(this.skyline.rBegin()) && key === to)
+            {
+                this.skyline.setElement(to, 0);
+            }
+            else
+            {
+                // first element bigger than to
+                this.skyline.setElement(to, value);
+            }
+        }
+
+        this.cleanSkyline();
     }
 
     set(from, to, amount)
     {
-        if (!this.segments.find(from))
+        this.skyline.setElement(from, amount);
+        let it = this.skyline.find(from);
+        for (; !it.equals(this.skyline.end()); it.next())
         {
-            this.segments.setElement(from, amount);
-        }
-        if (!this.segments.find(to))
-        {
-            this.segments.setElement(to, amount);
+            let [key, value] = it.pointer;
+            if (key >= to)
+            {
+                break;
+            }
+
+            this.skyline.setElement(key, amount);
         }
 
-        let currNode= this.segments.find(from);
-        let currKey = currNode.pointer[0];
-        let currValue = currNode.pointer[1];
-        while (currKey < to)
+        if (it.equals(this.skyline.end()))
         {
-            this.segments.setElement(currKey, amount);
-            currNode = currNode.next();
-            currKey = currNode.pointer[0];
-            currValue = currNode.pointer[1];
+            this.skyline.setElement(to, 0);
         }
+        else
+        {
+            this.skyline.setElement(to, amount);
+        }
+
+        this.cleanSkyline();
     }
 
     toString()
     {
         let result = "["
-        for (let it = this.segments.begin(); !it.equals(this.segments.end()); it.next())
+        for (let it = this.skyline.begin(); !it.equals(this.skyline.end()); it.next())
         {
             result += "[" + it.pointer[0] + "," + it.pointer[1] + "],"
         }
 
         if (result.length > 1)
         {
-            result = result.slice(0, result.length-2)
+            result = result.slice(0, result.length - 1)
         }
 
         result += "]"
         return result
+    }
+
+    cleanSkyline()
+    {
+        // Remove the beginning 0 values
+        const keyToRemove = new Set();
+        for (let it = this.skyline.begin(); !it.equals(this.skyline.end()); it.next())
+        {
+            let [key, value] = it.pointer;
+            if (value === 0)
+            {
+                keyToRemove.add(key);
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        // For multiple neighboring entries with same value, only the 1st one is kept.
+        let [prevKey, prevValue] = this.skyline.begin().pointer;
+        for (let it = this.skyline.begin().next(); !it.equals(this.skyline.end()); it.next())
+        {
+            let [key, value] = it.pointer;
+            if (value === prevValue)
+            {
+                keyToRemove.add(key);
+            }
+
+            [prevKey, prevValue] = [key, value];
+        }
+
+        for (const key of keyToRemove)
+        {
+            this.skyline.eraseElementByKey(key);
+        }
     }
 }
 
--- Javascript/ShijieJerryCodingTest/IntensitySegments.test.js
+++ Javascript/ShijieJerryCodingTest/IntensitySegments.test.js
@@ -35,36 +35,23 @@ describe("add() operation", () =>
         segments.add(10, 40, -1);
         expect(segments.toString()).toBe("[[10,-1],[20,0],[30,-1],[40,0]]")
     });
+});
 
-    test('Third example"', () =>
+describe("set() operation", () =>
+{
+    test('"', () =>
     {
-        /*
-        let segments= new OrderedMap();
-        segments.setElement(1, 0);
-        segments.setElement(3, 2);
-        segments.setElement(6, 5);
-        segments.setElement(8, 7);
-        segments.setElement(11, 10);
-        segments.setElement(13, 12);
+        const segments = new IntensitySegments();
+        expect(segments.toString()).toBe("[]");
 
-        let elem = segments.find(6);
-        if (!segments.find(6).equals(segments.end()))
-        {
-            console.log(elem.pointer[0])
-            console.log(elem.pointer[1])
-        }
+        segments.set(10, 30, 1);
+        expect(segments.toString()).toBe("[[10,1],[30,0]]")
 
-        let elem1 = segments.find(5)
-        if (!segments.find(5).equals(segments.end()))
-        {
-            console.log(elem1.pointer[0])
-            console.log(elem1.pointer[1])
+        segments.set(20, 40, 2);
+        expect(segments.toString()).toBe("[[10,1],[20,2],[40,0]]")
 
-            console.log(elem1.next().pointer[0])
-        }
-
-        let elemNotExist = segments.find(8);
-        */
+        segments.set(30, 50, 0);
+        expect(segments.toString()).toBe("[[10,1],[20,2],[30,0]]")
     });
 });
 
